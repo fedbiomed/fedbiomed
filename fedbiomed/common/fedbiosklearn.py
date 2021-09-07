08ed1c2f4ca104f0f79ac5455d442fb99f14b530
jsaray <jose-francisco.saray-villamizar@inria.fr>
2021-09-07 15:10:16 +0200

    yannick 3rd review

--- fedbiomed/common/fedbiosklearn.py
+++ fedbiomed/common/fedbiosklearn.py
@@ -5,8 +5,12 @@ from sklearn.linear_model import SGDRegressor, SGDClassifier, Perceptron
 from sklearn.naive_bayes import BernoulliNB, GaussianNB
 
 class SGDSkLearnModel():
-    '''Initialize model parameters'''
+
     def set_init_params(self, kwargs):
+        """
+            Initialize model parameters
+            :param kwargs dictionnary containing model parameters
+        """
         if self.model_type in ['SGDRegressor']:
             self.param_list = ['intercept_','coef_']
             init_params = {'intercept_': np.array([0.]), 
@@ -22,26 +26,44 @@ class SGDSkLearnModel():
         for p in self.params_sgd:
             setattr(self.m, p, self.params_sgd[p])
 
-    ''' Provide partial fit method of scikit learning model here. '''
     def partial_fit(self,X,y):
+        """
+            Provide partial fit method of scikit learning model here.
+            :param X, ndarray
+            :param y, vector
+            :raise NotImplementedError if developer do not implement this method.
+        """
         raise NotImplementedError('Partial fit must be implemented')
 
-    '''Perform in this method all data reading and data transformations you need.
-    At the end you should provide a couple (X,y) as indicated in the partial_fit
-    method of the scikit learn class.'''
     def training_data(self, batch_size=None):
+        """
+            Perform in this method all data reading and data transformations you need.
+            At the end you should provide a couple (X,y) as indicated in the partial_fit
+            method of the scikit learn class.
+            :param batch_size
+            :raise NotImplementedError if developer do not implement this method.
+        """
         raise NotImplementedError('Training data must be implemented')
 
-    ''' Provide a dictionnary with the parameters you need to be fitted, refer to
-     scikit documentation for a detail of parameters '''
     def after_training_params(self):
+        """Provide a dictionnary with the federated parameters you need to aggregate, refer to
+            scikit documentation for a detail of parameters
+            :return the federated parameters
+        """
         return {key: getattr(self.m, key) for key in self.param_list}
 
-    '''
-    Method training_routine called in Round, to change only if you know what you are doing.
-    '''
     def training_routine(self, epochs=1, log_interval=10, lr=1e-3, batch_size=50, batch_maxnum=0, dry_run=False,
                          logger=None):
+        """
+            Method training_routine called in Round, to change only if you know what you are doing.
+            :param epochs
+            :param log_interval unused, provided in the declaration for backward compatibility.
+            :param lr unused, provided in the declaration for backward compatibility.
+            :param batch_size size of batch for partial_fit
+            :param batch_maxnum always 0 for the moment.
+            :param dry_run provided for backward compatibility only
+            :param logger provided for backward compatibility only
+        """
         print('SGD Regressor training batch size ', batch_size)
         #print('Init parameters', self.m.coef_, self.m.intercept_)
         (data, target) = self.training_data(batch_size=batch_size)
@@ -60,6 +82,10 @@ class SGDSkLearnModel():
         #print('MODEL PARAMS:',self.m.coef_, self.m.intercept_)
 
     def __init__(self,kwargs):
+        """
+           Class initializer.
+           :param kwargs dictionnary containing model parameters
+        """
         self.batch_size = 100
         self.model_map = {'MultinomialNB', 'BernoulliNB', 'Perceptron', 'SGDClassifier', 'PassiveAggressiveClassifier',
                           'SGDRegressor', 'PassiveAggressiveRegressor', 'MiniBatchKMeans',
@@ -85,20 +111,17 @@ class SGDSkLearnModel():
 
     # provided by fedbiomed // necessary to save the model code into a file
     def add_dependency(self, dep):
+        """
+           Add new dependency to this class.
+           :param dep dependency to add.
+        """
         self.dependencies.extend(dep)
         pass
 
     '''Save the code to send to nodes '''
     def save_code(self, filename: str):
         """Save the class code for this training plan to a file
-                Args:
-                    filename (string): path to the destination file
-
-                Returns:
-                    None
-
-                Exceptions:
-                    none
+           :param filename (string): path to the destination file
         """
         content = ""
         for s in self.dependencies:
@@ -112,10 +135,13 @@ class SGDSkLearnModel():
         file.write(content)
         file.close()
 
-    ''' Save method for parameter communication, internally is used
-    dump and load joblib library methods '''
     def save(self, filename, params: dict=None):
-        '''
+        """
+        Save method for parameter communication, internally is used
+        dump and load joblib library methods.
+        :param filename
+        :param params model parameters to save
+
         Save can be called from Job or Round.
             From round is always called with params.
             From job is called with no params in constructor and
@@ -124,7 +150,7 @@ class SGDSkLearnModel():
             Torch state_dict has a model_params object. model_params tag
             is used in the code. This is why this tag is
             used in sklearn case.
-        '''
+        """
         file = open(filename, "wb")
         if params is None:
             dump(self.m, file)
@@ -138,14 +164,16 @@ class SGDSkLearnModel():
             dump(self.m, file)
         file.close()
 
-    ''' Save method for parameter communication, internally is used
-        dump and load joblib library methods '''
     def load(self, filename, to_params: bool = False):
-        '''
+        """
+        Method to load the updated parameters of a scikit model
         Load can be called from Job or Round.
-            From round is called with no params
-            From job is called with  params
-        '''
+        From round is called with no params
+        From job is called with  params
+        :param filename
+        :param to_params boolean to differentiate a pytorch from a sklearn
+        :return a dictionnary with the loaded parameters.
+        """
         di_ret = {}
         file = open( filename , "rb")
         if not to_params:
@@ -157,14 +185,15 @@ class SGDSkLearnModel():
         file.close()
         return di_ret
 
-    # provided by the fedbiomed / can be overloaded // need WORK
-    def logger(self, msg, batch_index, log_interval = 10):
-        pass
-
-    # provided by the fedbiomed // should be moved in a DATA manipulation module
     def set_dataset(self, dataset_path):
+        """
+          :param dataset_path
+        """
         self.dataset_path = dataset_path
         print('Dataset_path',self.dataset_path)
 
     def get_model(self):
+        """
+            :return the scikit model
+        """
         return self.m
