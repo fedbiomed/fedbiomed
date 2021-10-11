6ccd68aa70f93a00f994c431a93eee8c33a517f5
Sergen Cansiz <sergen.cansiz@inria.fr>
2021-10-11 19:41:52 +0200

    copy oparation for notebooks in dockerfile

--- envs/development/docker/notebook/run_mounts/start-notebook.d/container-init.sh
+++ envs/development/docker/notebook/run_mounts/start-notebook.d/container-init.sh
@@ -9,35 +9,45 @@ set -x
 #     ln -s /home/jovyan "/home/$NB_USER"
 # fi
 
+notebooks='/home/notebooks/'
+
 if [ -n "$NB_UID" ] ; then
     #find /home/jovyan/ -mount -exec mountpoint {} >/dev/null \; -or -exec chown $NB_UID {} \;
     find /home/fed/ -mount -exec mountpoint {} >/dev/null \; -or -exec chown $NB_UID {} \;
 fi
-if [ -n "$NB_GID" ] ; then
-    find /home/jovyan/ -mount -exec mountpoint {} >/dev/null \; -or -exec chgrp $NB_UID {} \;
-fi
+
+if [ -d "$notebooks" ]; then
+    cp -r "$notebooks" /home/jovyan
+fi 
+
+
+# if [ -n "$NB_GID" ] ; then
+#     find /home/jovyan/ -mount -exec mountpoint {} >/dev/null \; -or -exec chgrp $NB_UID {} \;
+# fi
 
 
 # glitch: make a copy of sample notebooks - owned and writable by the container,
 # saved outside of container, not git'ed
 NOTEBOOKNAME='notebook'
-NOTEBOOKPATH="/home/$NOTEBOOKNAME/$NOTEBOOKNAME"
-if [ ! -d "$NOTEBOOKPATH" ] ; then
-    mkdir $NOTEBOOKPATH
-    [ -n "$NB_UID" ] && chown $NB_UID $NOTEBOOKPATH
-    [ -n "$NB_GID" ] && chgrp $NB_GID $NOTEBOOKPATH
-fi
+NOTEBOOKPATH="/home/$NOTEBOOKNAME"
+
+# if [ ! -d "$NOTEBOOKPATH" ] ; then
+#     mkdir $NOTEBOOKPATH
+#     [ -n "$NB_UID" ] && chown $NB_UID $NOTEBOOKPATH
+#     [ -n "$NB_GID" ] && chgrp $NB_GID $NOTEBOOKPATH
+# fi
+
 [ -e "/home/jovyan/$NOTEBOOKNAME" ] || ln -s $NOTEBOOKPATH /home/jovyan/$NOTEBOOKNAME
 
-for file in $(cd /home/samples ; ls -1) ; do
-    [ -e "$NOTEBOOKPATH/$file" ] || cp -p /home/samples/$file $NOTEBOOKPATH/$file
-done
+# for file in $(cd /home/samples ; ls -1) ; do
+#     [ -e "$NOTEBOOKPATH/$file" ] || cp -p /home/samples/$file $NOTEBOOKPATH/$file
+# done
 
-if [ -n "$NB_UID" ] ; then
-    find $NOTEBOOKPATH/ -mount -exec mountpoint {} >/dev/null \; -or -exec chown $NB_UID {} \;
-fi
-if [ -n "$NB_GID" ] ; then
-    find $NOTEBOOKPATH/ -mount -exec mountpoint {} >/dev/null \; -or -exec chgrp $NB_UID {} \;
-fi
+# if [ -n "$NB_UID" ] ; then
+#     find $NOTEBOOKPATH/ -mount -exec mountpoint {} >/dev/null \; -or -exec chown $NB_UID {} \;
+# fi
+# if [ -n "$NB_GID" ] ; then
+#     find $NOTEBOOKPATH/ -mount -exec mountpoint {} >/dev/null \; -or -exec chgrp $NB_UID {} \;
+# fi
 
 
