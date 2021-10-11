bc247f936fa548186d9ec6d9ac7a3c3252111f8d
Marc Vesin <marc.vesin@inria.fr>
2021-10-11 14:03:39 +0200

    typo pretty

--- envs/vpn/docker/restful/build_files/entrypoint.sh
+++ envs/vpn/docker/restful/build_files/entrypoint.sh
@@ -63,13 +63,16 @@ finish () {
 
 trap finish TERM INT QUIT
 
+# we want to start from a fresh config
+rm -rf /app/db.sqlite3
+#
 # glitch : could not have PATH properly set with su -c either via logins.def or .profile/etc.
 # and we need more than default PATH
-su -c " export PATH=${PATH} ; python manage.py migrate" $CONTAINER_USER
-su -c " export PATH=${PATH} ; python manage.py collectstatic --link --noinput" $CONTAINER_USER
-su -c " export PATH=${PATH} ; python manage.py createsuperuser --noinput" $CONTAINER_USER
+su -c "export PATH=${PATH} ; python manage.py migrate" $CONTAINER_USER
+su -c "export PATH=${PATH} ; python manage.py collectstatic --link --noinput" $CONTAINER_USER
+su -c "export PATH=${PATH} ; python manage.py createsuperuser --noinput" $CONTAINER_USER
 
-su -c " export PATH=${PATH} ; gunicorn -w 4 -b 0.0.0.0:8000 --log-level debug fedbiomed.wsgi" $CONTAINER_USER &
+su -c "export PATH=${PATH} ; gunicorn -w 4 -b 0.0.0.0:8000 --log-level debug fedbiomed.wsgi" $CONTAINER_USER &
 #sleep infinity &
 
 wait $!
