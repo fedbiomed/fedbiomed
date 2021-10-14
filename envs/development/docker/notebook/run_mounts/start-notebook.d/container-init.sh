3ad3c625dd7802e3b5b52142164f35ec683269ce
Sergen Cansiz <sergen.cansiz@inria.fr>
2021-10-14 17:07:06 +0200

    changing workspace variable naming

--- envs/development/docker/notebook/run_mounts/start-notebook.d/container-init.sh
+++ envs/development/docker/notebook/run_mounts/start-notebook.d/container-init.sh
@@ -7,19 +7,11 @@ if [ -n "$NB_UID" ] ; then
 fi
 
 
+# Configure workspace
+WORKSPACE='/home/workspace'
+[ -n "$NB_UID" ] && chown $NB_UID $WORKSPACE
+[ -n "$NB_GID" ] && chgrp $NB_GID $WORKSPACE
 
-NOTEBOOKPATH='/home/workspace'
+ln -s $WORKSPACE /home/jovyan/
 
-[ -n "$NB_UID" ] && chown $NB_UID $NOTEBOOKPATH
-[ -n "$NB_GID" ] && chgrp $NB_GID $NOTEBOOKPATH
-
-[ -e "$NOTEBOOKNAME" ] || ln -s $NOTEBOOKPATH /home/jovyan/$NOTEBOOKNAME
-
-# if [ -n "$NB_UID" ] ; then
-#     find /home/workspace -mount -exec mountpoint {} >/dev/null \; -or -exec chown $NB_UID {} \;
-# fi
-
-# if [ -n "$NB_GID" ] ; then
-#     find /home/workspace -mount -exec mountpoint {} >/dev/null \; -or -exec chgrp $NB_UID {} \;
-# fi
 
