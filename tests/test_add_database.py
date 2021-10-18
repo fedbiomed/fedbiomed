cf64aa052f985f11cb449c824d4caf82d3e1b562
Jean-Luc Szpyrka <Jean-Luc.Szpyrka@inria.fr>
2021-10-18 18:24:26 +0200

    fix data test path

--- tests/test_add_database.py
+++ tests/test_add_database.py
@@ -6,6 +6,10 @@ from unittest.mock import MagicMock, patch
 import os
 import warnings
 
+import inspect
+
+print(os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe()))))
+
 
 class TestLoadDataSets(unittest.TestCase):
     """
@@ -13,55 +17,77 @@ class TestLoadDataSets(unittest.TestCase):
     Args:
         unittest ([type]): [description]
     """
-    
+
     # Setup data manager
     def setUp(self):
-        
+
+        self.testdir = os.path.join(
+            os.path.dirname(
+                os.path.abspath(inspect.getfile(inspect.currentframe()))
+                ),
+            "test-data"
+            )
+
         # Ignore ResoruceWarning, this action does not change test results
         warnings.simplefilter('ignore', category=ResourceWarning)
-        self.data_manager = Data_manager() 
+        self.data_manager = Data_manager()
         pass
 
     # after the tests
-    def tearDown(self): 
+    def tearDown(self):
         os.remove(DB_PATH)
         pass
-    
+
     def test_load_csv_dataset(self):
-        
+
         """ Test function for loading csv datasets """
 
-        # Load data with header example 
+        # Load data with header example
         self.data_manager.add_database( name='test',
                                         tags=['titi'],
                                         data_type='csv',
                                         description='description',
-                                        path='tests/test-data/csv/tata-header.csv')
+                                        path=os.path.join( self.testdir,
+                                                           "csv",
+                                                           "tata-header.csv"
+                                                          )
+                                        )
 
         # Should raise error due to same tag
-        with self.assertRaises(Exception):                            
+        with self.assertRaises(Exception):
             self.data_manager.add_database( name='test',
-                                    tags=['titi'],
-                                    data_type='csv',
-                                    description='description',
-                                    path='tests/test-data/csv/tata-header.csv')
+                                            tags=['titi'],
+                                            data_type='csv',
+                                            description='description',
+                                            path=os.path.join( self.testdir,
+                                                               "csv",
+                                                               "tata-header.csv"
+                                                              )
+                                           )
 
         # Load data with normal different types
         self.data_manager.add_database( name='test',
                                         tags=['tata'],
                                         data_type='csv',
                                         description='description',
-                                        path='tests/test-data/csv/titi-normal.csv')
+                                        path=os.path.join( self.testdir,
+                                                           "csv",
+                                                           "titi-normal.csv"
+                                                          )
+                                       )
 
         # Should raise error due to broken csv
-        with self.assertRaises(Exception):                            
+        with self.assertRaises(Exception):
             self.data_manager.add_database( name='test',
-                                    tags=['tutu'],
-                                    data_type='csv',
-                                    description='description',
-                                    path= 'tests/test-data/csv/toto-error.csv')
-
-        pass
+                                            tags=['tutu'],
+                                            data_type='csv',
+                                            description='description',
+                                            path=os.path.join( self.testdir,
+                                                               "csv",
+                                                               "toto-error.csv"
+                                                              )
+                                           )
+            pass
 
     def test_load_image_dataset(self):
 
@@ -72,7 +98,10 @@ class TestLoadDataSets(unittest.TestCase):
                                         tags=['titi'],
                                         data_type='images',
                                         description='description',
-                                        path='tests/test-data/images')
+                                        path=os.path.join( self.testdir,
+                                                           "images"
+                                                          )
+                                       )
 
         # Should raise error due to same tag
         with self.assertRaises(Exception):
@@ -80,10 +109,11 @@ class TestLoadDataSets(unittest.TestCase):
                                             tags=['titi'],
                                             data_type='images',
                                             description='description',
-                                            path='tests/test-data/images')
-        pass
+                                            path=os.path.join( self.testdir,
+                                                               "images"
+                                                              )
+                                           )
+            pass
 
-if __name__ == '__main__':  # pragma: no cover    
-    unittest.main() 
-
-
+if __name__ == '__main__':  # pragma: no cover
+    unittest.main()
