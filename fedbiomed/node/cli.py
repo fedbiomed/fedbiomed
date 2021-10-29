c1bc4099ff407ae65bbf43a7cec03999c9a94c1e
Jean-Luc Szpyrka <Jean-Luc.Szpyrka@inria.fr>
2021-10-29 16:39:51 +0200

    most important than everything else.... the banner !

--- fedbiomed/node/cli.py
+++ fedbiomed/node/cli.py
@@ -19,17 +19,22 @@ from fedbiomed.node.node import Node
 from fedbiomed.common.logger import logger
 
 
+#
+# print(pyfiglet.Figlet("doom").renderText(' fedbiomed node'))
+#
 __intro__ = """
 
-   __         _ _     _                          _        _ _            _
-  / _|       | | |   (_)                        | |      | (_)          | |
- | |_ ___  __| | |__  _  ___  _ __ ___   ___  __| |   ___| |_  ___ _ __ | |_
- |  _/ _ \/ _` | '_ \| |/ _ \| '_ ` _ \ / _ \/ _` |  / __| | |/ _ \ '_ \| __|
- | ||  __/ (_| | |_) | | (_) | | | | | |  __/ (_| | | (__| | |  __/ | | | |_
- |_| \___|\__,_|_.__/|_|\___/|_| |_| |_|\___|\__,_|  \___|_|_|\___|_| |_|\__|
+   __         _ _     _                          _                   _
+  / _|       | | |   (_)                        | |                 | |
+ | |_ ___  __| | |__  _  ___  _ __ ___   ___  __| |  _ __   ___   __| | ___
+ |  _/ _ \/ _` | '_ \| |/ _ \| '_ ` _ \ / _ \/ _` | | '_ \ / _ \ / _` |/ _ \
+ | ||  __/ (_| | |_) | | (_) | | | | | |  __/ (_| | | | | | (_) | (_| |  __/
+ |_| \___|\__,_|_.__/|_|\___/|_| |_| |_|\___|\__,_| |_| |_|\___/ \__,_|\___|
+
+
 """
 
-# this may be changed on command line or in the config_client.ini
+# this may be changed on command line or in the config_node.ini
 logger.setLevel("DEBUG")
 
 data_manager = Data_manager()
@@ -304,7 +309,7 @@ def launch_cli():
         parser.print_help()
     else:
         print(__intro__)
-        print('\t- 🆔 Your client ID:', CLIENT_ID, '\n')
+        print('\t- 🆔 Your node ID:', CLIENT_ID, '\n')
 
     if args.add:
         add_database()
