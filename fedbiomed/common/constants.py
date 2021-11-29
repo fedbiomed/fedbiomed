907b043b90b20649101080db212a357b1bbe5ac3
Jean-Luc Szpyrka <Jean-Luc.Szpyrka@inria.fr>
2021-11-29 12:06:44 +0100

    added all errors as an enum in common/constants.py

--- fedbiomed/common/constants.py
+++ fedbiomed/common/constants.py
@@ -46,3 +46,47 @@ class ModelTypes(_BaseEnum):
 
     REGISTERED = 'registered'
     DEFAULT    = 'default'
+
+
+class ErrorHandling(_BaseEnum):
+
+    """
+    list of all error messages types
+    """
+
+    # MQTT errors
+    FB100 = "server does not answer in dedicated time"
+    FB101 = "mqqt call error"
+    FB102 = "message echange error"
+
+    # HTTP errors
+
+    FB200 = "server not reachable"
+    FB201 = "server return 404 error"
+    FB202 = "server return other 4xx or 500 error"
+
+    # application error on node
+
+    FB300 = "TrainingPlan class does not load"
+    FB301 = "TrainingPlan class does not contain expected methods"
+    FB302 = "a TrainingPlan method crashes"
+    FB303 = "a TrainingPlan loops indefinitely"
+    FB304 = "bad URL (.py) for TrainingPlan"
+    FB305 = "bad URL (.pt) for training params"
+    FB306 = "bad training request ().json"
+    FB307 = "bad model params (.pt)"
+    FB308 = "bad data format"
+    FB309 = "receiving a new computation request during a running computation"
+
+    # application error on researcher
+    FB400 = "fedaverage method crashes or returns an error"
+    FB401 = "strategy method creashes or sending an error"
+    FB402 = "bad URL (.pt) for model param"
+    FB403 = "bad model param (.pt) format for TrainingPlan"
+    FB404 = "received delayed answer for previous computation round"
+    FB405 = "list of nodes is empty at data lookup phase"
+    FB406 = "list of nodes became empty then training"
+
+    # node problem detected by researcher
+    FB500 = "receiving error message from node"  ### useless ???
+    FB501 = "node not reacheable"
