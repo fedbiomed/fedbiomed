2774adc5e68b656844b51aaa65a3ef9dcdced874
Jean-Luc Szpyrka <Jean-Luc.Szpyrka@inria.fr>
2021-11-29 11:47:04 +0100

    simplify logger level handling

--- fedbiomed/common/logger.py
+++ fedbiomed/common/logger.py
@@ -326,28 +326,6 @@ class _LoggerBase():
         pass
 
 
-    def _internalForceMqttHandlerLevel(self, level):
-        """
-        internal method
-
-        MQTT handler must send ERROR + CRITICAL messages do the
-        the researcher, in any case. So MQQT handler error level must
-        be at least ERROR.
-        """
-
-        # sanity check that MQTT handler is installed
-        if not "MQTT" in self._handlers:
-            return
-
-        level = self._internalLevelTranslator(level)
-
-        if level > logging.ERROR:
-            level = logging.error
-            logger.debug("setting minimal level to ERROR for MQTT handler")
-
-        self._handlers["MQTT"].setLevel(level)
-
-
     def addMqttHandler(self,
                        mqtt        = None,
                        node_id     = None,
@@ -410,21 +388,25 @@ class _LoggerBase():
         htype : if provided (non madatory), change the level of the given handler.
                 if not  provided (or None), change the level of all known handlers
 
+        Remark: level should not be lower than CRITICAL (meaning CRITICAL errors are
+                always displayed
+
         Ex:
             setLevel( logging.DEBUG, 'FILE')
         """
 
         level = self._internalLevelTranslator(level)
+
+        if level > logging.CRITICAL:
+            level = logging.critical
+            logger.debug("setting minimal level to CRITICAL")
+
+        # store this level (for future handler adding)
         self._logger.setLevel( level )
 
         if htype is None:
             for h in self._handlers:
                 self._handlers[h].setLevel(level)
-            self._internalForceMqttHandlerLevel(level)
-            return
-
-        if htype == "MQTT":
-            self._internalForceMqttHandlerLevel(level)
             return
 
         if htype in self._handlers:
@@ -433,6 +415,7 @@ class _LoggerBase():
 
         # htype provided but no handler for this type exists
         self._logger.warning(htype + " handler not initialized yet")
+
 
 
     def __getattr__(self,s):
