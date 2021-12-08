f374c5977630624815c915ec906fd86990066b66
Marc Vesin <marc.vesin@inria.fr>
2021-12-08 18:36:28 +0100

    WIP Job unit test update for breakpoint v2 - needs patch Responses for test_private_load_training_replies - needs additional tests for load/save staete

--- tests/test_job.py
+++ tests/test_job.py
@@ -5,7 +5,7 @@ import os
 
 # Managing NODE, RESEARCHER environ mock before running tests
 from testsupport.delete_environ import delete_environ
-# Detele environ. It is necessary to rebuild environ for required component
+# Delete environ. It is necessary to rebuild environ for required component
 delete_environ()
 import testsupport.mock_common_environ
 # Import environ for node, since tests will be runing for node component
@@ -30,28 +30,43 @@ class TestJob(unittest.TestCase):
     def tearDownClass(cls):
         pass
 
-
     def setUp(self):
 
         self.patcher = patch('fedbiomed.researcher.requests.Requests.__init__',
                              return_value=None)
-        self.patcher2 = patch('fedbiomed.researcher.requests.Requests.search',
-                              return_value=None)
-        self.patcher3 = patch('fedbiomed.common.repository.Repository.upload_file',
+        self.patcher2 = patch('fedbiomed.common.repository.Repository.upload_file',
                               return_value={"file": environ['UPLOADS_URL']})
         self.patcher.start()
         self.patcher2.start()
-        self.patcher3.start()
-
-
 
     def tearDown(self) -> None:
 
         self.patcher.stop()
         self.patcher2.stop()
-        self.patcher3.stop()
         #shutil.rmtree(os.path.join(VAR_DIR, "breakpoints"))
         # (above) remove files created during these unit tests
+
+
+    # common mock classes
+    class ResponsesMock(list):
+        """
+        This class mimicks `Responses` class. It
+        can be considered as a minimal implementation of
+        the forementioned class
+        """
+        def __init__(self, training_replies):
+            super().__init__(training_replies)
+            self._data = training_replies
+        @property
+        def data(self) -> list:
+            """setter
+            Returns:
+                list:  data of the class `Responses`
+            """
+            return(self._data)
+        def __getitem__(self, item):
+            return self._data[item]
+
 
     # tests
     def test_job(self):
@@ -63,34 +78,13 @@ class TestJob(unittest.TestCase):
 
         pass
 
+
     def test_save_private_training_replies(self):
         """
-        tests if `_save_training_replies` is converting
-        pytorch tensor and numpy arrays into path files (since
-        both are not JSON serializable). It uses a dummy class
+        tests if `_save_training_replies` is properly extracting
+        breakpoint info from `training_replies`. It uses a dummy class
         ResponsesMock, a weak implementation of `Responses` class
         """
-        class ResponsesMock(list):
-            """
-            This class mimicks `Responses` class. It
-            can be considered as a minimal implementation of
-            the forementioned class
-            """
-            def __init__(self, training_replies):
-                super().__init__(training_replies)
-                self._data = training_replies
-
-            @property
-            def data(self) -> list:
-                """setter
-
-                Returns:
-                    list:  data of the class `Responses`
-                """
-                return(self._data)
-
-            def __getitem__(self, item):
-                return self._data[item]
 
         # mock model object
         model_file = MagicMock(return_value=None)
@@ -99,82 +93,71 @@ class TestJob(unittest.TestCase):
         # mock FederatedDataSet
         fds = MagicMock()
         fds.data = MagicMock(return_value={})
-        #fds.keys = MagicMock(return_value={})
 
-        # instanciate job
+        # instantiate job
         test_job = Job(model=model_file,
                        data=fds)
-        # create dummy data mimicking 2 nodes
-        node_id1, node_id2 = '1', '2'
-        tmpfilename_client1 = str(os.path.join('client', '1', 'path'))
-        tmpfilename_client2 = str(os.path.join('client', '2', 'path'))
-
-        test_job._params_path = {node_id1: tmpfilename_client1,
-                                      node_id2: tmpfilename_client2}
-
 
         # first create a `_training_replies` variable
-        _training_replies = {0: ResponsesMock([]),
-                             1: ResponsesMock([
-                                {"node_id": node_id1,
-                                    'params': torch.Tensor([1, 3, 5]),
-                                    'dataset_id': 'id_node_1'
-                                },
-                                {"node_id": node_id2,
-                                    'params': np.array([1, 3, 5]),
-                                    'dataset_id': 'id_node_2'
-                                },
-                                ])
-                            }
+        training_replies = {
+            0: self.ResponsesMock([]),
+            1: self.ResponsesMock(
+                [
+                    {
+                        "node_id": '1234',
+                        'params': torch.Tensor([1, 3, 5]),
+                        'dataset_id': 'id_node_1'
+                    },
+                    {
+                        "node_id": '5678',
+                        'params': np.array([1, 3, 5]),
+                        'dataset_id': 'id_node_2'
+                    },
+                ])
+            }
 
-        test_job._training_replies = _training_replies
-
-        test_job._model_file = ""
-
-        # second, use `save_state` function
-        test_job.save_state(round=1)
-
-        # FIXME: not the best way to check if path names are valid
-        new_training_replies = test_job.state.get('training_replies')
+        new_training_replies = test_job._save_training_replies(training_replies)
 
         # check if `training_replies` is  saved accordingly
-        self.assertTrue(isinstance(new_training_replies[1][0].get('params'),
-                                   str))
-        self.assertEquals(new_training_replies[1][0].get('params'),
-                          tmpfilename_client1)
+        self.assertTrue(type(new_training_replies) is list)
+        self.assertTrue(len(new_training_replies) == 2)
+        self.assertTrue('params' not in new_training_replies[1][0])
+        self.assertEqual(new_training_replies[1][1].get('dataset_id'), 'id_node_2')
 
-        self.assertEquals(new_training_replies[1][1].get('dataset_id'),
-                          'id_node_2')
 
     def test_private_load_training_replies(self):
-        """tests if `_load_training_replies` is converting path files into
-        pytorch tensor or numpy arrays.
+        """tests if `_load_training_replies` is loading file content from path file.
         """
 
         # first test with a model done with pytorch
-        pytorch_params = torch.Tensor([1, 3, 5, 7])
-        sklearn_params = np.array([[1,2,3,4,5],
-                                   [2,8,7,5,5]])
+        pytorch_params = {
+            # dont need other fields
+            'model_params': torch.Tensor([1, 3, 5, 7])
+        }
+        sklearn_params = {
+            # dont need other fields
+            'model_params': np.array([[1,2,3,4,5], [2,8,7,5,5]])
+        }
         # mock FederatedDataSet
         fds = MagicMock()
         fds.data = MagicMock(return_value={})
 
-        # mock Pytroch model object
+        # mock Pytorch model object
         model_torch = MagicMock(return_value=None)
         model_torch.save = MagicMock(return_value=None)
-        model_torch.load = MagicMock(return_value=pytorch_params)
+        func_torch_loadparams = MagicMock(return_value=pytorch_params)
 
-        # instanciate job
+        # instantiate job
         test_job_torch = Job(model=model_torch,
                              data=fds)
-        # second create a `_training_replies` variable
-        loaded_training_replies = {"3": [
+        # second create a `training_replies` variable
+        loaded_training_replies =  [
+                                        [
                                         {"success": True,
                                          "msg": "",
                                          "dataset_id": "dataset_1234",
                                          "node_id": "node_1234",
                                          "params_path": "/path/to/file/param.pt",
-                                         "params": "/path/to/file/param.pt",
                                          "timing": {"time": 0}
                                          },
                                         {"success": True,
@@ -182,43 +165,53 @@ class TestJob(unittest.TestCase):
                                          "dataset_id": "dataset_4567",
                                          "node_id": "node_4567",
                                          "params_path": "/path/to/file/param2.pt",
-                                         "params": "/path/to/file/param2.pt",
                                          "timing": {"time": 0}
                                          }
                                         ]
-                                   }
+                                    ]
 
-        params_path = {"node_1234": "/path/to/file/param.pt",
-                       "node_4567": "/path/to/file/param2.pt"}
-        test_job_torch._load_training_replies(loaded_training_replies,
-                                              params_path)
+        torch_training_replies = test_job_torch._load_training_replies(
+                                    loaded_training_replies,
+                                    func_torch_loadparams
+                                    )
 
-        # check `self._training_replies` for pytorch models
+        self.assertTrue(type(torch_training_replies) is dict)
+        # heuristic check `training_replies` for existing field in input
+        self.assertEqual(
+            torch_training_replies[0][0]['node_id'],
+            loaded_training_replies[0][0]['node_id'])
+        # check `training_replies` for pytorch models
+        self.assertTrue(torch.isclose(torch_training_replies[0][1]['params'],
+                        pytorch_params['model_params']).all())
+        self.assertTrue(torch_training_replies[0][1]['params_path'],
+                        "/path/to/file/param2.pt")
+        self.assertTrue(isinstance(torch_training_replies[0], Responses))
 
-        self.assertTrue(torch.isclose(test_job_torch._training_replies[3][0]['params'],
-                        pytorch_params).all())
-        self.assertTrue(test_job_torch._training_replies[3][0]['params_path'],
-                        "/path/to/file/param.pt")
-        self.assertTrue(isinstance(test_job_torch._training_replies[3],
-                                   Responses))
 
         ##### REPRODUCE TESTS BUT FOR SKLEARN MODELS
         # mock sklearn model object
         model_sklearn = MagicMock(return_value=None)
         model_sklearn.save = MagicMock(return_value=None)
-        model_sklearn.load = MagicMock(return_value=sklearn_params)
-        # instanciate job
+        func_sklearn_loadparams = MagicMock(return_value=sklearn_params)
+        # instantiate job
         test_job_sklearn = Job(model=model_sklearn,
                                data=fds)
-        test_job_sklearn._load_training_replies(loaded_training_replies,
-                                                params_path)
+        
+        sklearn_training_replies = test_job_sklearn._load_training_replies(
+                                        loaded_training_replies,
+                                        func_sklearn_loadparams
+                                        )
 
-        # check `self._training_replies` for sklearn models
-        self.assertTrue(np.allclose(test_job_sklearn._training_replies[3][0]['params'],
-                        sklearn_params))
-        self.assertTrue(test_job_sklearn._training_replies[3][0]['params_path'],
-                        "/path/to/file/param.pt")
-        self.assertTrue(isinstance(test_job_sklearn._training_replies[3],
+        # heuristic check `training_replies` for existing field in input
+        self.assertEqual(
+            sklearn_training_replies[0][0]['node_id'],
+            loaded_training_replies[0][0]['node_id'])
+        # check `training_replies` for sklearn models
+        self.assertTrue(np.allclose(sklearn_training_replies[0][1]['params'],
+                        sklearn_params['model_params']))
+        self.assertTrue(sklearn_training_replies[0][1]['params_path'],
+                        "/path/to/file/param2.pt")
+        self.assertTrue(isinstance(sklearn_training_replies[0],
                                    Responses))
 
 if __name__ == '__main__':  # pragma: no cover
