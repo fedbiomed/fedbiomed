d63a23155c541f4a849aa96a3beae6350bd4a373
Yannick Bouillard <yannick.bouillard@inria.fr>
2021-12-16 17:23:18 +0100

    updates

--- notebooks/demo_poc_data_wrapper/data_format_ref_cli.py
+++ notebooks/demo_poc_data_wrapper/data_format_ref_cli.py
@@ -1,13 +1,13 @@
 from enum import Enum
 import os
-from typing import List, Tuple, Dict, List, Any
+from typing import List, Tuple, Dict, List, Any, Callable
 import pprint
 
 import pandas as pd
 
-from demo_poc_data_wrapper.data_amputation_methods import ImputationMethods
+from notebooks.demo_poc_data_wrapper.data_imputation_methods import ImputationMethods
 from demo_poc_data_wrapper import utils
-from demo_poc_data_wrapper.data_type import DataType
+from demo_poc_data_wrapper.data_type import DataType, CategoricalDataType, DataTypeProperties
 
 def get_yes_no_msg() -> str:
     msg_yes_or_no_question = '1) YES\n2) NO\n'   
@@ -57,7 +57,7 @@ def get_data_imputation_methods_msg(d_type: type = None) -> Tuple[str, Dict[str,
             is_d_type_in_sub_type = utils.check_data_type_consistancy(imput_method.data_type, 
                                                                d_type)
             if not is_d_type_in_sub_type:
-                continue # data type doesnot match method amputation requirments
+                continue # data type doesnot match method imputation requirments
         msg += '%d) %s\n' % (i, imput_method.name)
         select_action[str(i)] = imput_method
         i += 1
@@ -114,8 +114,8 @@ def get_from_user_dataframe_format_file(dataset: pd.DataFrame) -> Dict[str, Any]
         if data_format is DataType.KEY or data_format is DataType.DATETIME:  
             # for these data type, missing values are disabled by default
             is_missing_values_allowed = False
-            amputation_method = None
-            amputation_method_parameters = None
+            imputation_method = None
+            imputation_method_parameters = None
         else: 
             # ask user if missing values are allowed for this specific variable
             ## message definition
@@ -128,29 +128,29 @@ def get_from_user_dataframe_format_file(dataset: pd.DataFrame) -> Dict[str, Any]
                                                         n_answers=2)
             is_missing_values_allowed = parse_yes_no_msg(missing_values_user_selection)
             
-            amputation_method = None
-            amputation_method_parameters = None
+            imputation_method = None
+            imputation_method_parameters = None
             if is_missing_values_allowed:
                 # let user select amputation method if missing data are allowed
-                amputation_method_user_selection = get_user_input(msg_data_imputation_methods,
+                imputation_method_user_selection = get_user_input(msg_data_imputation_methods,
                                                                 n_answers=n_data_imputation_method)
                 
-                amputation_method_selected = data_imputation_methods.get(amputation_method_user_selection)
+                imputation_method_selected = data_imputation_methods.get(imputation_method_user_selection)
                 
-                if amputation_method_selected is not None:
-                    amputation_method = amputation_method_selected.name
-                    if amputation_method_selected.parameters_to_ask_user is not None:
-                        print(f'Selected: {amputation_method}\n')
-                        amputation_method_parameters = ask_for_data_imputation_parameters(amputation_method_selected.parameters_to_ask_user)
-                        print('amput param', amputation_method_parameters)
+                if imputation_method_selected is not None:
+                    imputation_method = imputation_method_selected.name
+                    if imputation_method_selected.parameters_to_ask_user is not None:
+                        print(f'Selected: {imputation_method}\n')
+                        imputation_method_parameters = ask_for_data_imputation_parameters(imputation_method_selected.parameters_to_ask_user)
+                        print('amput param', imputation_method_parameters)
             
                 
         data_format_file[feature] = {'data_format': data_format.name,
                                      'data_type': n_data_type.name,
                                      'values': str(d_type),
                                      'is_missing_values': is_missing_values_allowed,
-                                     'data_amputation_method': amputation_method,
-                                     'data_amputation_parameters': amputation_method_parameters
+                                     'data_imputation_method': imputation_method,
+                                     'data_imputation_parameters': imputation_method_parameters
                                     }
         
     return data_format_file
@@ -189,3 +189,312 @@ def get_from_user_multi_view_dataset_fromat_file(datasets: Dict[str, pd.DataFram
             data_format_files[_file_name] = data_format_file
         
     return data_format_files
+
+
+# edition of format_file_ref
+
+def edit_format_file_ref(format_file_ref: Dict[str, Dict[str, Any]]) -> Dict[str, Dict[str, Any]]:
+    
+    # CLI for editing `format_file_ref`, a file containing information about each variable
+    # in a tabular dataset
+    print(f'Now editing format file ref')
+    
+    ## variables initialization
+    available_categorical_data_types = [t for t in CategoricalDataType]
+    _file_names = list(format_file_ref.keys())
+    _n_tot_files = len(_file_names)
+    
+    ## messages definition
+    _data_type_selection_msg, ign_key = get_data_type_selection_msg(available_categorical_data_types)
+    
+    _messages = {
+        'yes_or_no': get_yes_no_msg(),
+        'edit': 'Which field should be modified?\n',
+    }
+
+    
+    
+    # iterate over name of files (ie views)
+    for i_file in range(_n_tot_files):
+        # ask for each file if user wants to edt it
+        _answer = get_user_input(f"Edit file: {_file_names[i_file]}?\n" + _messages['yes_or_no'], 2)
+        _answer = parse_yes_no_msg(_answer)
+        
+        if _answer:
+            # case where user wants to modify current view scheme
+            _file_content = format_file_ref[_file_names[i_file]]  # get file (ie view) content
+            
+            ## variables initialization for parsing current view
+            _features_names = list(_file_content.keys())
+            _n_tot_feature = len(_features_names)
+            
+            # iterate over features found in view
+            for i_feature in range(_n_tot_feature):
+                feature_name = _features_names[i_feature]
+                feature_content = _file_content[feature_name]
+                feature_content = edit_feature_format_file_ref(feature_content,
+                                                               feature_name,
+                                                               available_categorical_data_types,
+                                                               _messages,
+                                                               ign_key)
+            format_file_ref[_file_names[i_file]].update({feature_name: feature_content})
+            
+    return format_file_ref
+
+import dateutil
+from dateutil.parser._parser import ParserError
+
+
+def create_msg_action_selection(data_type_propreties: Enum) -> Tuple[str, int, Dict[int, Callable]]:
+    # create edit selection message for user given data type
+    # number of possible action depend of data type properties
+    msg = ""
+    action_counter = 1
+    actions = {}
+    
+    # data type change command
+    msg += "%d) data_type\n" % action_counter
+    actions[str(action_counter)] = ask_for_data_type
+    action_counter += 1
+    
+    if data_type_propreties.lower_bound:
+        # lower bound edit command
+        msg += "%d) lower bound\n" % action_counter
+        actions[str(action_counter)] = ask_for_lower_bound
+        action_counter += 1
+        
+    if data_type_propreties.upper_bound:
+        # upper bound edit command
+        msg += "%d)upper bound\n" % action_counter
+        actions[str(action_counter)] = ask_for_upper_bound
+        action_counter += 1
+        
+    if data_type_propreties.set_of_values:
+        # value taken edit command
+        msg += "%d) Values taken\n" % action_counter
+        actions[str(action_counter)] = ask_for_categorical_values
+        action_counter += 1
+        
+    if data_type_propreties.date_format:
+        # date formatter edit command
+        msg += "%d) Date format\n" % action_counter
+        actions[str(action_counter)] = ask_for_date_format
+        action_counter += 1
+        
+    if data_type_propreties.allow_missing_values:
+        # change data method for data imputation
+        msg += "%d) Data Value amputation method\n" % action_counter
+        actions[str(action_counter)] = ask_for_data_amputation_method
+        action_counter += 1
+        
+    msg += "%d) Cancel Operation\n" % action_counter
+    actions[str(action_counter)] = cancel_operation
+    return msg, action_counter, actions
+
+def select_action(
+                  action: str,
+                  possible_actions: Dict[str, Callable],
+                  ) -> Tuple[Dict[str, Any], bool]:
+    
+    # variable initialization
+    is_cancelled = False
+    new_field = None
+    _action_counter = 1  # integer for dynamic cli management
+    print('action', action, str(len(possible_actions.keys())),possible_actions)
+    if action == str(len(possible_actions.keys())):
+        is_cancelled = True
+        
+    else:
+        # define action among the pool of possible actions
+        _action_func = possible_actions[action]
+        new_field = _action_func()
+    
+    return new_field, is_cancelled
+
+
+def isfloat(value:str) ->bool:
+    """checks if string represents a float or int"""
+    is_float = True
+    try:
+        float(value)
+    except ValueError as e:
+        is_float = False
+    return is_float
+
+
+
+def is_datetime(date: str) -> bool:
+    """checks if date is a date"""
+    is_date_parsable = True
+    try:
+        dateutil.parser.parse(date)
+    except (ParserError, ValueError) as err:
+        is_date_parsable = False
+        
+    return is_date_parsable
+
+def cancel_operation():
+    print("operation cancelled")
+        
+def ask_for_lower_bound() -> Dict[str, float]:
+    _is_entered_value_correct = False
+    while not _is_entered_value_correct:
+        lower_bound = input('enter lower bound')
+        if isfloat(lower_bound) or is_datetime(lower_bound):
+            # check if entered value is correct (is a numerical value)
+            _is_entered_value_correct = True
+        else:
+            print('Value not a Number! please retry')
+    return {'lower_bound': float(lower_bound)}
+
+def ask_for_upper_bound() -> Dict[str, float]:
+    
+    _is_entered_value_correct = False
+    while not _is_entered_value_correct:
+        upper_bound = input('enter upper bound')
+        if isfloat(upper_bound) or is_datetime(upper_bound):
+            # check if entered value is correct (is a numerical value)
+            _is_entered_value_correct = True
+        else:
+            print('Value not a Number! please retry')
+    return {'upper_bound': float(upper_bound)}
+
+
+def _ask_for_data_type(data_type: Enum) -> Enum:
+    """asks user for datatype contains in `data_type`
+    If user selects `cancel`, it will return None
+    """
+    
+    _available_data_type = [t for t in data_type]  # get all keys contain in data_type
+    n_avail_data_type = len(_available_data_type)
+    msg, _n_answer = get_data_type_selection_msg(data_type, ign_msg="cancel operation")
+    data_type_selection = get_user_input(msg, _n_answer)
+    
+    if str(_n_answer) != data_type_selection:
+        return _available_data_type[int(data_type_selection) - 1]
+    
+    
+def ask_for_data_type() -> Dict[str, Any]:
+
+    updates = None
+    
+    new_data_format = _ask_for_data_type(DataType)
+    
+    if new_data_format is not None: 
+        # case where 'cancel operation' hasnot been selected
+                
+        updates = {'data_format': new_data_format.name}
+        if  isinstance(next(iter(new_data_format.value)), Enum):
+            # if subtypes are available
+            new_data_type= _ask_for_data_type(new_data_format.value)
+            new_values = list(map(lambda x: str(x), new_data_type.value))
+            updates.update({'data_type': new_data_type, 'values': new_values})
+        else:
+            new_values = list(map(lambda x: str(x), new_data_format.value))
+            updates.update({'values': new_values})
+    return updates
+
+
+def ask_for_data_amputation_method(d_type: type=None) -> Dict[str, str]:
+    
+    # variable initialisation
+    _amputation_method = None
+    _amputation_method_parameters = None
+    
+    # get user message + dictionary mapping user responses to data amputation methods
+    _msg_data_imputation_methods, _data_imputation_methods = get_data_imputation_methods_msg(d_type)
+    
+    # ask for user selection
+    _amputation_method_user_selection = get_user_input(_msg_data_imputation_methods,
+                                                      n_answers=len(_data_imputation_methods))
+    
+    # select user data amputation method given user command
+    _amputation_method_selected = _data_imputation_methods.get(_amputation_method_user_selection)
+
+    if _amputation_method_selected is not None:
+        _amputation_method = _amputation_method_selected.name
+        
+        if _amputation_method_selected.parameters_to_ask_user is not None:
+            print(f'Method amputation selected: {_amputation_method}\n')
+            _amputation_method_parameters = ask_for_data_imputation_parameters(_amputation_method_selected.parameters_to_ask_user)
+            print('amput param', _amputation_method_parameters)
+    updates = {'data_amputation_method': _amputation_method,
+               'data_amputation_parameters': _amputation_method_parameters}
+    return updates
+
+def ask_for_categorical_values() -> Dict[str, Any]:
+    possible_values = input('enter possible values (separated by ",")')
+    possible_values = possible_values.split(",")  # separate values passed by user into a list
+    return {'categorical_values': possible_values}
+
+
+def ask_for_date_format() -> Dict[str, Any]:
+    # TODO : ask for date format (UTC, ....)
+    msg = 'please enter date format:\n1)timetsamp\n2)ISO date format (YYYY-MM-DD)\n3)custom date format\n'
+    user_selection = input(msg)
+    # default date format
+    msg = {'1': 'timestamp',
+          '2': '(American default date format) mm/dd/yy',
+          '3': '(Europeen default date format) dd/mm/yy',
+           '4': 'ISO date format (YYYY-MM-DD)',
+          '5': 'custom date format',
+          '6': 'select timezone'}
+    pass
+
+def edit_feature_format_file_ref(feature_content: Dict[str, Any],
+                                  feature_name: str,
+                                  available_categorical_data_type: List[Enum],
+                                  messages: Dict[str, str],
+                                  ignore_keystroke: int) -> Dict[str, Any]:
+    """Edits a specific feature that belongs to a specific view within a format file"""
+    
+
+    _is_feature_unparsed = True  
+    _is_cancelled = False  # whether parsing of current column has been cancelled or not
+    _is_first_edit = True
+    _avail_data_type_properties = [dtype for dtype in DataTypeProperties]
+    
+    # iterate over number of feature contained in view, and ask for each feature if changes are needed
+    while _is_feature_unparsed:
+        if _is_cancelled or not _is_first_edit:
+            _f_answer = True
+        else:
+            _f_answer = get_user_input(f"Edit variable: {feature_name}?\n" + messages['yes_or_no'], 2)
+            # ask if user wants to edit feature variables
+            _f_answer = parse_yes_no_msg(_f_answer)
+            _is_operation_cancelled = False  # for cancelling feature edition
+            _is_first_edit = False
+        if _f_answer:
+            # case where user wants to edit the current feature
+            
+            _msg = messages['edit']
+            
+            data_format = feature_content.get('data_format')
+            for data_type_properties in _avail_data_type_properties:
+                if data_format == data_type_properties.name:
+                    # get data property from data_format
+                    select_msg, n_actions, possible_actions = create_msg_action_selection(data_type_properties)
+                    _msg += select_msg
+                    _edit_selection = get_user_input(_msg, n_actions)
+
+                    _edited_field, _is_cancelled = select_action(
+                                                                  _edit_selection,
+                                                                  possible_actions
+                                                                  #available_categorical_data_type,
+                                                                  #messages['data_type_select']
+                                                                )
+
+            if not _is_cancelled:
+                # if user has not cancelled field edition
+                if _edited_field is not None:
+                    feature_content.update(_edited_field)
+             
+                _c_answer = get_user_input(f"Continue Editing variable: {feature_name}?\n" + messages['yes_or_no'], 2)
+                _is_feature_unparsed = parse_yes_no_msg(_c_answer)
+            else:
+                _is_feature_unparsed = False
+                
+        else:
+            _is_feature_unparsed = False
+            
+    return feature_content
