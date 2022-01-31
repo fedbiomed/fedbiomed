5a5c10ec1a584cd0793b07534e6228d7081562b2
Yannick Bouillard <yannick.bouillard@inria.fr>
2022-01-31 14:44:22 +0100

    WIP: second test for Rounds class

--- tests/test_round.py
+++ tests/test_round.py
@@ -11,12 +11,29 @@ from fedbiomed.node.environ import environ
 import time
 
 import unittest
-from unittest.mock import patch
+from unittest.mock import MagicMock, patch
 
 from fedbiomed.node.round import Round
 
 
 class TestRound(unittest.TestCase):
+    SLEEPING_TIME = 1  # time that simulate training (in seconds)
+    class FakeModel:
+        # Fake model that mimics a Training Plan model
+        def __init__(self, **kwargs):
+            pass
+        def load(self, path:str, to_params:bool):
+            pass
+        def save(self, filename:str, results: Dict[str, Any]):
+            pass
+        def set_dataset(self, path:str):
+            pass
+        def training_routine(self, **kwargs):
+            time.sleep(TestRound.SLEEPING_TIME)
+        def after_training_params(self)-> List[int]:
+            return [1, 2, 3, 4]
+        
+            
     @patch('fedbiomed.common.repository.Repository.__init__')
     @patch('fedbiomed.node.model_manager.ModelManager.__init__')
     def setUp(self,
@@ -31,6 +48,9 @@ class TestRound(unittest.TestCase):
         
     
     def tearDown(self) -> None:
+        pass
+    
+    def _init_normal_case_scenario(self):
         pass
     
     @patch('fedbiomed.common.message.NodeMessages.reply_create')
@@ -49,32 +69,19 @@ class TestRound(unittest.TestCase):
                                    repository_upload_patch,
                                    node_msg_patch
                                    ):
-        """tests correct execution and message parameter. 
-        Besides tests the training time.
+        """tests correct execution and message parameters. 
+        Besides test tests the training time.
         """
-        SLEEPING_TIME = 1  # time that simulate training (in seconds)
+        TestRound.SLEEPING_TIME = 1
         URL_MSG = 'http://url/where/my/file?is=True'
         # initalisation of dummy classes
         class FakeUuid:
+            # Fake uuid class
             def __init__(self):
                 self.hex = 1234
+                
             def __str__(self):
                 return '1234'
-            
-        class FakeModel:
-            def __init__(self, **kwargs):
-                pass
-            def load(self, path:str, to_params:bool):
-                pass
-            def save(self, filename:str, results: Dict[str, Any]):
-                pass
-            def set_dataset(self, path:str):
-                pass
-            def training_routine(self, **kwargs):
-                time.sleep(SLEEPING_TIME)
-                pass
-            def after_training_params(self)-> List[int]:
-                return [1, 2, 3, 4]
             
         class FakeMonitor:
             def history(self) -> Dict[str, float]:
@@ -99,7 +106,7 @@ class TestRound(unittest.TestCase):
         repository_download_patch.side_effect = repository_side_effect
         model_manager_patch.return_value = (True, {'name': "model_name"})
         builtin_exec_patch.return_value = None
-        builtin_eval_patch.return_value = FakeModel
+        builtin_eval_patch.return_value = self.FakeModel
         repository_upload_patch.return_value = {'file': URL_MSG}
         node_msg_patch.side_effect = node_msg_side_effect
         
@@ -114,18 +121,93 @@ class TestRound(unittest.TestCase):
         # check results
         self.assertTrue(msg.get('success', False))
         self.assertEqual(URL_MSG, msg.get('params_url'))
+        self.assertEqual('train', msg.get('command'))
         
         # timing test
         if not isinstance(msg, dict) or not hasattr(msg, 'get'):
             self.skipTest('timing tests skipped because of incorrect data type returned')
         self.assertAlmostEqual(
             msg.get('timing', {'rtime_training': 0}).get('rtime_training'),
-            SLEEPING_TIME, places=2
+            TestRound.SLEEPING_TIME, places=2
             )
-        
-    def test_run_model_training_02(self):
+    
+    @patch('fedbiomed.common.message.NodeMessages.reply_create')
+    @patch('fedbiomed.common.repository.Repository.upload_file')
+    @patch('builtins.eval')
+    @patch('builtins.exec')
+    @patch('fedbiomed.node.model_manager.ModelManager.check_is_model_approved')
+    @patch('fedbiomed.common.repository.Repository.download_file')
+    @patch('uuid.uuid4')    
+    def test_run_model_training_02(self,
+                                   uuid_patch,
+                                   repository_download_patch,
+                                   model_manager_patch,
+                                   builtin_exec_patch,
+                                   builtin_eval_patch,
+                                   repository_upload_patch,
+                                   node_msg_patch):
         # test if all methods of `model` have been called
-        pass
+        TestRound.SLEEPING_TIME = 0
+        URL_MSG = 'http://url/where/my/file?is=True'
+        # initalisation of dummy classes
+        class FakeUuid:
+            # Fake uuid class
+            def __init__(self):
+                self.hex = 1234
+                
+            def __str__(self):
+                return '1234'
+            
+        class FakeMonitor:
+            def history(self) -> Dict[str, float]:
+                return {'Loss': 0.1}
+            
+        class FakeNodeMessages:
+            def __init__(self, msg: Dict[str, Any]):
+                self.msg = msg
+
+            def get_dict(self) -> Dict[str, Any]:
+                return self.msg
+
+        def repository_side_effect(model_url: str, model_name: str):
+            return 200, 'my_python_model'
+        
+        def node_msg_side_effect(msg: Dict[str, Any]) -> Dict[str, Any]:
+            fake_node_msg = FakeNodeMessages(msg)
+            return fake_node_msg
+
+        # initialisation of patchers 
+        # dummy model
+        class FakeModel:
+            pass
+        # Mocking model
+        dummy_model = FakeModel
+        #dummy_model.load = MagicMock()
+        dummy_model.save = MagicMock()
+        dummy_model.set_dataset = MagicMock()
+        dummy_model.training_routine = MagicMock()
+        dummy_model.after_training_params = MagicMock()
+        #print(dummy_model.ll)
+        
+        uuid_patch.return_value = FakeUuid()
+        repository_download_patch.side_effect = repository_side_effect
+        model_manager_patch.return_value = (True, {'name': "model_name"})
+        builtin_exec_patch.return_value = None
+        builtin_eval_patch.return_value = self.FakeModel
+        repository_upload_patch.return_value = {'file': URL_MSG}
+        node_msg_patch.side_effect = node_msg_side_effect
+        
+        self.r1.training_kwargs = {}
+        self.r1.dataset = {'path': 'my/dataset/path',
+                           'dataset_id': 'id_1234'}
+        dummy_monitor = MagicMock()
+        self.r1.monitor = dummy_monitor        
+        # action!
+        with patch.object(self.FakeModel, 'training_routine') as mock1:
+            msg = self.r1.run_model_training()
+        # tests
+        mock1.assert_called_once_with( monitor=dummy_monitor,
+                                        node_args=None)
     
     def test_run_model_training_03(self):
         # test exceptions
