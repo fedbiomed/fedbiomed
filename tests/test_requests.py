87394b790db94b6bf789d1cf83d6ee674ae3cf1f
Sergen Cansiz <sergen.cansiz@inria.fr>
2022-02-08 10:39:51 +0100

    adding test for Request constructor

--- tests/test_requests.py
+++ tests/test_requests.py
@@ -1,14 +1,19 @@
 # Managing NODE, RESEARCHER environ mock before running tests
+import fedbiomed.researcher.requests
 from testsupport.delete_environ import delete_environ
+
 # Delete environ. It is necessary to rebuild environ for required component
 delete_environ()
 # overload with fake environ for tests
 import testsupport.mock_common_environ
-
+from importlib import reload
 from typing import Callable
 from fedbiomed.researcher.requests import Requests
 from fedbiomed.researcher.responses import Responses
 from fedbiomed.researcher.monitor import Monitor
+from fedbiomed.common.messaging import Messaging
+from fedbiomed.researcher.environ import environ
+from fedbiomed.common.constants import ComponentType
 import unittest
 from unittest.mock import patch
 
@@ -23,40 +28,62 @@ class TestRequest(unittest.TestCase):
         self.req_pathcer2 = patch('fedbiomed.common.messaging.Messaging.start')
         self.req_pathcer3 = patch('fedbiomed.common.messaging.Messaging.send_message')
 
-        self.message_init           = self.req_pathcer1.start()
-        self.message_start          = self.req_pathcer2.start()
-        self.message_send           = self.req_pathcer3.start()
+        self.message_init = self.req_pathcer1.start()
+        self.message_start = self.req_pathcer2.start()
+        self.message_send = self.req_pathcer3.start()
 
         self.message_init.return_value = None
         self.message_start.return_value = None
         self.message_send.return_value = None
 
-
     def tearDown(self):
 
         self.req_pathcer1.stop()
-        self.req_pathcer1.stop()
-        self.req_pathcer1.stop()
+        self.req_pathcer2.stop()
+        self.req_pathcer3.stop()
 
         pass
 
+    def test_constructor(self):
+        """ Testing Request constructor """
+
+        # Remove previous singleton instance
+        if Requests in Requests._objects:
+            del Requests._objects[Requests]
+
+        # Build brand new reqeust by providing Messaging in advance
+        messaging = Messaging()
+        req_1 = Requests(mess=messaging)
+        self.assertEqual(0, req_1._sequence)
+        self.assertEqual(None, req_1._monitor_message_callback)
+        self.assertEqual(messaging, req_1.messaging)
+
+        # Remove previous singleton instance
+        if Requests in Requests._objects:
+            del Requests._objects[Requests]
+
+        # Build new fresh reqeust
+        req_2 = Requests(mess=None)
+        self.assertEqual(0, req_1._sequence)
+        self.assertEqual(None, req_1._monitor_message_callback)
+        self.assertIsInstance(req_2.messaging, Messaging)
 
     @patch('fedbiomed.researcher.requests.Requests.get_responses')
     def test_list_function(self, request_get_response):
 
         # Test with single response database
         res = [
-                {'node_id' : 'node-1',
-                'researcher_id': 'r-xxx',
-                'databases' : [
-                    {'data_type' : 'csv', 'tags': ['ss' , 'ss'], 'shape' : [1,2], 'name' : 'data'},
-                    {'data_type' : 'csv', 'tags': ['ss' , 'ss'], 'shape' : [1,2], 'name' : 'data'}
-                ],
-                'success' : True,
-                'count' : 2,
-                'command': 'list'
-                }
-            ]
+            {'node_id': 'node-1',
+             'researcher_id': 'r-xxx',
+             'databases': [
+                 {'data_type': 'csv', 'tags': ['ss', 'ss'], 'shape': [1, 2], 'name': 'data'},
+                 {'data_type': 'csv', 'tags': ['ss', 'ss'], 'shape': [1, 2], 'name': 'data'}
+             ],
+             'success': True,
+             'count': 2,
+             'command': 'list'
+             }
+        ]
 
         responses = Responses(res)
         request_get_response.return_value = responses
@@ -68,29 +95,29 @@ class TestRequest(unittest.TestCase):
 
         self.assertIsInstance(result, object)
 
-        # Test with multople database response
+        # Test with multiple database response
         res = [
-                {'node_id' : 'node-1',
-                'researcher_id': 'r-xxx',
-                'databases' : [
-                    {'data_type' : 'csv', 'tags': ['ss' , 'ss'], 'shape' : [1,2], 'name' : 'data'},
-                    {'data_type' : 'csv', 'tags': ['ss' , 'ss'], 'shape' : [1,2], 'name' : 'data'}
-                ],
-                'success' : True,
-                'count' : 2,
-                'command': 'list'
-                },
-                {'node_id' : 'node-2',
-                'researcher_id': 'r-xxx',
-                'databases' : [
-                    {'data_type' : 'csv', 'tags': ['ss' , 'ss'], 'shape' : [1,2], 'name' : 'data'},
-                    {'data_type' : 'csv', 'tags': ['ss' , 'ss'], 'shape' : [1,2], 'name' : 'data'}
-                ],
-                'success' : True,
-                'count' : 2,
-                'command': 'list'
-                }
-            ]
+            {'node_id': 'node-1',
+             'researcher_id': 'r-xxx',
+             'databases': [
+                 {'data_type': 'csv', 'tags': ['ss', 'ss'], 'shape': [1, 2], 'name': 'data'},
+                 {'data_type': 'csv', 'tags': ['ss', 'ss'], 'shape': [1, 2], 'name': 'data'}
+             ],
+             'success': True,
+             'count': 2,
+             'command': 'list'
+             },
+            {'node_id': 'node-2',
+             'researcher_id': 'r-xxx',
+             'databases': [
+                 {'data_type': 'csv', 'tags': ['ss', 'ss'], 'shape': [1, 2], 'name': 'data'},
+                 {'data_type': 'csv', 'tags': ['ss', 'ss'], 'shape': [1, 2], 'name': 'data'}
+             ],
+             'success': True,
+             'count': 2,
+             'command': 'list'
+             }
+        ]
 
         responses = Responses(res)
         request_get_response.return_value = responses
@@ -105,17 +132,17 @@ class TestRequest(unittest.TestCase):
 
         # Test with verbose mode
         res = [
-                {'node_id' : 'node',
-                'researcher_id': 'r-xxx',
-                'databases' : [
-                    {'data_type' : 'csv', 'tags': ['ss' , 'ss'], 'shape' : [1,2], 'name' : 'data'},
-                    {'data_type' : 'csv', 'tags': ['ss' , 'ss'], 'shape' : [1,2], 'name' : 'data'}
-                ],
-                'success' : True,
-                'count' : 2,
-                'command': 'list'
-                },
-            ]
+            {'node_id': 'node',
+             'researcher_id': 'r-xxx',
+             'databases': [
+                 {'data_type': 'csv', 'tags': ['ss', 'ss'], 'shape': [1, 2], 'name': 'data'},
+                 {'data_type': 'csv', 'tags': ['ss', 'ss'], 'shape': [1, 2], 'name': 'data'}
+             ],
+             'success': True,
+             'count': 2,
+             'command': 'list'
+             },
+        ]
 
         responses = Responses(res)
         request_get_response.return_value = responses
@@ -129,44 +156,43 @@ class TestRequest(unittest.TestCase):
         self.assertIsInstance(result, object)
         # Test with node ids
         res = [
-                {'toto' : 'node',
-                'researcher_id': 'r-xxx',
-                'databases' : [
-                    {'data_type' : 'csv', 'tags': ['ss' , 'ss'], 'shape' : [1,2], 'name' : 'data'},
-                    {'data_type' : 'csv', 'tags': ['ss' , 'ss'], 'shape' : [1,2], 'name' : 'data'}
-                ],
-                'success' : True,
-                'count' : 2,
-                'command': 'list'
-                },
-                {'node_id' : 'node-2',
-                'researcher_id': 'r-xxx',
-                'databases' : [
-                    {'data_type' : 'csv', 'tags': ['ss' , 'ss'], 'shape' : [1,2], 'name' : 'data'},
-                    {'data_type' : 'csv', 'tags': ['ss' , 'ss'], 'shape' : [1,2], 'name' : 'data'}
-                ],
-                'success' : True,
-                'count' : 2,
-                'command': 'list'
-                }
-            ]
+            {'toto': 'node',
+             'researcher_id': 'r-xxx',
+             'databases': [
+                 {'data_type': 'csv', 'tags': ['ss', 'ss'], 'shape': [1, 2], 'name': 'data'},
+                 {'data_type': 'csv', 'tags': ['ss', 'ss'], 'shape': [1, 2], 'name': 'data'}
+             ],
+             'success': True,
+             'count': 2,
+             'command': 'list'
+             },
+            {'node_id': 'node-2',
+             'researcher_id': 'r-xxx',
+             'databases': [
+                 {'data_type': 'csv', 'tags': ['ss', 'ss'], 'shape': [1, 2], 'name': 'data'},
+                 {'data_type': 'csv', 'tags': ['ss', 'ss'], 'shape': [1, 2], 'name': 'data'}
+             ],
+             'success': True,
+             'count': 2,
+             'command': 'list'
+             }
+        ]
 
         responses = Responses(res)
         request_get_response.return_value = responses
 
         try:
-            req = Requests(nodes= ['node-1' , 'node-2'])
+            req = Requests(nodes=['node-1', 'node-2'])
             result = req.list()
         except:
             self.assertTrue(False, 'List method failed even data is okay')
 
         self.assertIsInstance(result, object)
 
-
     def test_add_remove_monitor_callback(self):
 
         """ Test adding and removing monitor message callbacks """
-        
+
         req = Requests()
         monitor = Monitor()
 
@@ -177,6 +203,7 @@ class TestRequest(unittest.TestCase):
         # Test removing monitor callback
         req.remove_monitor_callback()
         self.assertIsNone(req._monitor_message_callback, "Monitor callback han't been removed")
-        
+
+
 if __name__ == '__main__':  # pragma: no cover
     unittest.main()
