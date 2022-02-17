214ff95ceee01782d5e948014cc6aebbbdd78031
Jean-Luc Szpyrka <Jean-Luc.Szpyrka@inria.fr>
2022-02-17 15:44:26 +0100

    added FedbiomedTasQueueError

--- fedbiomed/common/tasks_queue.py
+++ fedbiomed/common/tasks_queue.py
@@ -2,15 +2,16 @@ from typing import Optional
 
 import persistqueue
 
-
-exceptionsEmpty = persistqueue.exceptions.Empty
+from fedbiomed.common.constants  import ErrorNumbers
+from fedbiomed.common.exceptions import FedbiomedTaskQueueError
+from fedbiomed.common.logger     import logger
 
 
 class TasksQueue:
     """
     A disk-persistant Queue object, ensuring queue will remain on
     disk even if program crashes. Relies on `persistqueue` package.
-    
+
     """
     def __init__(self, messages_queue_dir: str, tmp_dir: str):
         """Instantiates a disk-persistant Queue.
@@ -20,7 +21,12 @@ class TasksQueue:
             persisted.
             tmp_dir (str): indicates where temporary files should be stored.
         """
-        self.queue = persistqueue.Queue(messages_queue_dir, tempdir=tmp_dir)
+        try:
+            self.queue = persistqueue.Queue(messages_queue_dir, tempdir=tmp_dir)
+        except ValueError as e:
+            msg = ErrorNumbers.FB603.value + ": cannot create queue (" + str(e) + ")"
+            logger.critical(msg)
+            raise FedbiomedTaskQueueError(msg)
 
     def add(self, task: dict):
         """this method adds a task to the queue
@@ -28,7 +34,15 @@ class TasksQueue:
         Args:
             task (dict): a dict describing the task to be added
         """
-        self.queue.put(task)
+        try:
+            self.queue.put(task)
+        except persistqueue.exceptions.Full:
+            msg = ErrorNumbers.FB603.value + ": queue is full"
+            logger.critical(msg)
+            raise FedbiomedTaskQueueError(msg)
+        # persistequeue does also raise ValueError if timeout is < 0
+        # but we do not provide a timeout value
+
 
     def get(self, block: Optional[bool] = True) -> dict:
         """this method gets the current task in the queue
@@ -40,7 +54,18 @@ class TasksQueue:
         Returns:
             [dict]: dictionary object stored in queue
         """
-        return self.queue.get(block)
+        try:
+            return self.queue.get(block)
+        except persistqueue.exceptions.Empty:
+            msg = ErrorNumbers.FB603.value + ": queue is empty"
+            logger.debug(msg)
+            raise FedbiomedTaskQueueError(msg)
+        #
+        # - this ignore may be ignored by the caller
+        #
+        # - persistequeue does also raise ValueError then timeout < 0
+        #   but we do not use timeout yet
+        #
 
     def qsize(self):
         """this method returns the size of the queue
@@ -56,4 +81,9 @@ class TasksQueue:
         Returns:
             'bool': True if task is complete
         """
-        return self.queue.task_done()
+        try:
+            return self.queue.task_done()
+        except ValueError:
+            # persistqueue raises it if task_done called too many time
+            # we can ignore it
+            return
