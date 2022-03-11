ea02b31c7c63d5ba84bedbf0bb973073bf2b9acd
Marc Vesin <marc.vesin@inria.fr>
2022-03-11 17:30:53 +0100

    first draft version of gui container launching

--- envs/vpn/docker/gui/build_files/entrypoint.sh
+++ envs/vpn/docker/gui/build_files/entrypoint.sh
@@ -3,10 +3,10 @@
 # Fed-BioMed - gui container launch script
 # - can be launched as unprivileged user account (when it exists)
 
-#export PYTHONPATH=/fedbiomed
-#eval "$(conda shell.bash hook)"
-#conda activate fedbiomed-gui
-#./scripts/fedbiomed_run gui data-folder /data config /fedbiomed/etc start
+export PYTHONPATH=/fedbiomed
+eval "$(conda shell.bash hook)"
+conda activate fedbiomed-gui
+./scripts/fedbiomed_run gui host 0.0.0.0 data-folder /data config config_node.ini start &
 
 sleep infinity &
 
