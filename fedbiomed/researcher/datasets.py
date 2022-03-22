a52a266deb2d7f35cd70264b109c3e659d137780
Marc Vesin <marc.vesin@inria.fr>
2022-03-22 10:54:46 +0100

    dataset : revert to version without test ratio

--- fedbiomed/researcher/datasets.py
+++ fedbiomed/researcher/datasets.py
@@ -16,17 +16,12 @@ class FederatedDataSet:
     such as client ids, data size that can be useful for
     aggregating or sampling strategies on researcher's side
     """
-    def __init__(self,
-                 data: Dict[str, List[Dict[str, Any]]],
-                 test_ratio: float = .0
-                ):
+    def __init__(self, data: Dict):
+
         """
         Constructor
         """
         self._data = data
-
-        # testing facility attributes
-        self.set_test_ratio(test_ratio, False)
 
     def data(self) -> Dict:
         """
@@ -36,29 +31,6 @@ class FederatedDataSet:
             Dict: Dict of federated datasets, keys as node ids
         """
         return self._data
-
-    def test_ratio(self) -> float:
-        return self._test_ratio
-
-    def set_test_ratio(self, ratio: float, overwrite: bool = False) -> float:
-        """
-        Sets testing ratio. 
-
-        Args:
-            - ratio (float): testing ratio, that MUST be within interval [0, 1]
-            - overwrite (bool, optional): modify ratio for the datasets that already
-                have a ratio value
-
-        Returns:
-            float: set testing ratio
-        """
-        self._test_ratio = ratio
-        for _, node_datasets in self._data.items():
-            for ds in node_datasets:
-                if overwrite is True or 'test_ratio' not in ds:
-                    ds.update({'test_ratio': self._test_ratio})
-
-        return self._test_ratio
 
     def node_ids(self) -> List[uuid.UUID]:
         """
