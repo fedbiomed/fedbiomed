1a1de3d4d99284f57dace517a97c60383f067c76
Marc Vesin <marc.vesin@inria.fr>
2022-05-16 09:08:42 +0200

    rename datasets.py to _datasets.py

--- fedbiomed/common/data/__init__.py
+++ fedbiomed/common/data/__init__.py
@@ -7,7 +7,7 @@ from ._data_manager import DataManager
 from ._torch_data_manager import TorchDataManager
 from ._sklearn_data_manager import SkLearnDataManager
 from ._torch_tabular_dataset import TorchTabularDataset
-from .datasets import NIFTIFolderDataset
+from ._datasets import NIFTIFolderDataset
 
 __all__ = [
     "DataManager",
