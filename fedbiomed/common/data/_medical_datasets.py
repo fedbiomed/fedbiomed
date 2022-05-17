b0a0e8197bafae6287de54f991b41a9b3e70fce6
Marc Vesin <marc.vesin@inria.fr>
2022-05-17 13:43:35 +0200

    - add getters - rename `class_names` to `class_labels` - some docstrings

--- fedbiomed/common/data/_medical_datasets.py
+++ fedbiomed/common/data/_medical_datasets.py
@@ -6,7 +6,7 @@ Provides classes managing dataset for common cases of use in healthcare:
 """
 from os import PathLike
 from pathlib import Path
-from typing import Union, Tuple
+from typing import Union, Tuple, List
 
 import torch
 from monai.data import ITKReader
@@ -26,7 +26,7 @@ class NIFTIFolderDataset(Dataset):
 
     This is a Dataset useful in classification tasks. Its usage is quite simple.
     Images must be contained by folders that describe the group/class they belong to.
-    Behaves much like torchvision.datasets.ImageFolder
+    Behaves much like `torchvision.datasets.ImageFolder`
 
     ```
     nifti_dataset_root_folder
@@ -55,7 +55,7 @@ class NIFTIFolderDataset(Dataset):
             target_transform: transforms to be applied on target.
         """
         self._files = []
-        self._class_names = []
+        self._class_labels = []
         self._targets = []
 
         self._root_dir = Path(root).expanduser()
@@ -69,18 +69,18 @@ class NIFTIFolderDataset(Dataset):
         self._explore_root_folder()
 
     def _explore_root_folder(self) -> None:
-        """Lists all files found in folder"""
+        """Scans all files found in folder structure to populate dataset"""
 
         # Search files that correspond to the following criteria:
         # 1. Extension in ALLOWED extensions
         # 2. File folder's parent must be root (inspects folder only one level of depth)
         self._files = [p.resolve() for p in self._root_dir.glob("**/*") if ''.join(p.suffixes) in self._ALLOWED_EXTENSIONS and p.parent.parent == self._root_dir]
 
-        # Create class names dictionary
-        self._class_names = tuple(set([p.parent.name for p in self._files]))
+        # Create class labels dictionary
+        self._class_labels = tuple(set([p.parent.name for p in self._files]))
 
         # Assign numerical value to target 0...n_classes
-        self._targets = torch.tensor([self._class_names.index(p.parent.name) for p in self._files]).long()
+        self._targets = torch.tensor([self._class_labels.index(p.parent.name) for p in self._files]).long()
 
         # Raise error if empty dataset
         if len(self._files) == 0 or len(self._targets) == 0:
@@ -88,15 +88,40 @@ class NIFTIFolderDataset(Dataset):
                 f"{ErrorNumbers.FB612.value}: Cannot create dataset because no compatible files found"
                 f" in the {self._root_dir}.")
 
-    def __getitem__(self, item: int) -> Tuple[Tensor, int]:
-        """ Gets item from dataset
+    def labels(self) -> List[str]:
+        """Retrieves the labels of the target classes.
 
-        Args:
-            item: Key/index to select single sample from dataset
+        Target label index is the index in this list.
 
         Returns:
-            input: Input sample
-            target: Target sample index
+            List of the labels of the target classes.
+        """
+        return self._class_labels
+
+    def files(self) -> List[Path]:
+        """Retrieves the paths to the sample images.
+
+        Gives sames order as when retrieving the sample images (eg `self.files[0]`
+        is the path to `self)
+
+        Returns:
+            List of the absolute paths to the sample images
+        """
+        return self._files
+
+    def __getitem__(self, item: int) -> Tuple[Tensor, int]:
+        """Gets item from dataset
+
+        Applies `transform` to the image if it is not `None`.
+        Applies `target_transform` to the sample index if it is not `None`
+
+        Args:
+            item: Index to select single sample from dataset
+
+        Raises:
+
+        Returns:
+            A tuple composed of the input sample (an image) and a target sample index (label index).
         """
         img = self._reader(self._files[item])
         target = int(self._targets[item])
@@ -108,5 +133,7 @@ class NIFTIFolderDataset(Dataset):
 
         return img, target
 
-    def __len__(self):
+    def __len__(self) -> int:
+        """Gets number of samples in dataset
+        """
         return len(self._files)
