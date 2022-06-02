673c12b7bd06700142dff4e0d50ea1f1afbe3567
Santiago SILVA <16252054+sssilvar@users.noreply.github.com>
2022-06-02 11:23:55 +0200

    Debug

--- notebooks/download_and_split_ixi.py
+++ notebooks/download_and_split_ixi.py
@@ -12,7 +12,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from tinydb import TinyDB, Query
 
-FEDBIOMED_ROOT = os.path.dirname(os.path.dirname(__file__))
+FEDBIOMED_ROOT = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
 config_file = """
 [default]
 node_id = CENTER_ID
