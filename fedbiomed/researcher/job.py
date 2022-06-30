0e5ca66af2a9e29cec19879f1688df827624078c
Francesco Cremonesi <francesco.cremonesi@inria.fr>
2022-06-30 14:15:01 +0200

    Do not instantiate the dp_args in the TrainingPlan constructor
    This allows us to avoid having to specify the dp_args in the model
    parameters, but we only specify them in the training_parameters.

--- fedbiomed/researcher/job.py
+++ fedbiomed/researcher/job.py
@@ -135,7 +135,7 @@ class Job:
             # also handle case where model is an instance of a class
             self.model_instance = model
 
-        if 'dp_args' in self.training_args:
+        if 'dp_args' in self.training_args.dict():
             self.model_instance.validate_and_fix_model()
 
         # find the name of the class in any case
