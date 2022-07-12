d60877a84a5829a8c284aed10b03ae2375130875
Yannick Bouillard <yannick.bouillard@inria.fr>
2022-07-12 17:21:01 +0200

    WIP: adding logging page

--- gui/server/app.py
+++ gui/server/app.py
@@ -1,8 +1,9 @@
 import os
 from datetime import timedelta
 
-from flask import Flask, render_template, send_from_directory, request, redirect
+from flask import Flask, render_template, send_from_directory, request, redirect, url_for
 from flask_jwt_extended import JWTManager
+from http import HTTPStatus
 
 from config import Config
 # Import api route blueprint before importing routes
@@ -40,6 +41,8 @@ jwt = JWTManager(app)
 app.register_blueprint(api)
 
 
+
+
 # Routes for react build directory
 @app.route('/', defaults={'path': ''}, methods=['GET'])
 @app.route('/<path:path>')
@@ -47,6 +50,7 @@ def index(path):
     """ The index route. This route should render the
         React build files. Which is located at the front-end folder.
     """
+    before_request(request)
     if path != "" and os.path.exists(app.static_folder + '/' + path):
         return send_from_directory(app.static_folder, path)
     else:
@@ -55,24 +59,55 @@ def index(path):
     return render_template('index.html')
 
 
+# functions for HTTP to HTTPS redirections
 @app.before_request
-def before_request():
-    print(request)
-    # redirect connections from HTTP to HTTPS
-    if not request.is_secure:
-        print("REDIRECTION")
-        url = request.url.replace('http://', 'https://', 1)
-        return redirect(url, code=301)
+def before_request(req=None):
+    if request.scheme == 'http':
+            return redirect(url_for(request.endpoint,
+                                    _scheme='https',
+                                    _external=True),
+                            HTTPStatus.PERMANENT_REDIRECT)
+# def before_request(req=None):
+#     print(request)
+#     # redirect connections from HTTP to HTTPS
+#     print("REDIRECTION STUFF")
+#     if request.url.startswith('http://'):
+#         print("REDIRECTION")
+#         url = request.url.replace('http://', 'https://', 1).replace('080', '443', 1)
+#         return redirect(url, code=302)
+
+@app.after_request
+def set_hsts_header(response):
+        """Adds HSTS header to each response."""
+        # Should we add STS header?
+        if request.is_secure:
+            response.headers.setdefault(
+                'Strict-Transport-Security', hsts_header)
+        return response
+
+def hsts_header():
+    """Returns the proper HSTS policy."""
+    hsts_age = 100000
+    hsts_policy = 'max-age={0}'.format(hsts_age)
+
+    if self.hsts_include_subdomains:
+        hsts_policy += '; includeSubDomains'
+
+    return hsts_policy
 
 
+#def context()
 # Run the application
 if __name__ == '__main__':
+
     # Start Flask
     print(CERTIFICATE_NAME, PRIVATE_KEY_NAME)
     if CERTIFICATE_NAME and PRIVATE_KEY_NAME:
         context = ('../../etc/' + CERTIFICATE_NAME, '../../etc/' + PRIVATE_KEY_NAME)
     else:
         context = "adhoc"
+    # Process(target=before_request,
+    #         daemon=True).start()
     app.run(host=app.config['HOST'],
             port=app.config['PORT'],
             debug=app.config['DEBUG'],
