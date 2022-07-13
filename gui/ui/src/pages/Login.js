0afb101cf1876c6057b998bcfcf02ef7c72a9030
Yannick Bouillard <yannick.bouillard@inria.fr>
2022-07-13 18:42:27 +0200

    adding basic login connection

--- gui/ui/src/pages/Login.js
+++ gui/ui/src/pages/Login.js
@@ -1,19 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 
 
-// const [errorMessages, setErrorMessages] = useState({});
+
+
 // const [isSubmitted, setIsSubmitted] = useState(false);
 
-// const renderErrorMessage = (name) =>
-//   name === errorMessages.name && (
-//     <div className="error">{errorMessages.message}</div>
-//   );
-
-//   const handleSubmit = (event) => {
-//     // Prevent page reload
-//     event.preventDefault();
-//   };
 
 /*
 const Login = (props) => {
@@ -49,17 +41,53 @@ const Login = (props) => {
 */
 const Login = (props) => {
 
+    const [errorMessages, setErrorMessages] = useState({});
+    const renderErrorMessage = (name) =>
+        { name === errorMessages.name && (
+            <div className="error">{errorMessages.message}</div>)
+        };
+    console.log("is active")
+    const computedClassName = props.active ? 'active' : 'muted';
+    console.log(computedClassName)
+    const handleSubmit = (event) => {
+        // Prevent page reload
+        event.preventDefault();
+    };
 
-return (
-    <React.Fragment>
-        <div>
-            <h2>Fed-BioMed Node GUI</h2>
-            <p>Welcome to Fed-BioMed Node application. In this application, you can manage your data
-                files that are deployed in the node or load new datasets into the node.  </p>
-        </div>
-        
-    </React.Fragment>
-)
+    const hideMainSideBar = () => {
+        return <div className="main-side-bar" style={{"display": "none"}}/>
+
+
+    }
+    return (
+        <React.Fragment>
+            <div>
+                <h2>Fed-BioMed Node GUI</h2>
+                <p>Welcome to Fed-BioMed Node application. In this application, you can manage your data
+                    files that are deployed in the node or load new datasets into the node.  </p>
+            </div>
+            {hideMainSideBar}
+            <div className="form">
+                    <form onSubmit={handleSubmit}>
+                        <div className="input-container">
+                        <label>Username </label>
+                        <input type="text" name="uname" required />
+                        {renderErrorMessage("uname")}
+                        </div>
+                        <div className="input-container">
+                        <label>Password </label>
+                        <input type="password" name="pass" required />
+                        {renderErrorMessage("pass")}
+                        </div>
+                        <div className="button-container">
+                        <input type="submit" />
+                        </div>
+                    </form>
+            </div>
+
+            
+        </React.Fragment>
+    )
 }
 
 
