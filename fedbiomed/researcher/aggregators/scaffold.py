54464eafd41264d1a64e0d7fa8250ccf0ba4d55d
Ayed <ayedsamy25@gmail.com>
2022-08-29 21:01:24 +0200

    Scaffold strategy class

---
+++ fedbiomed/researcher/aggregators/scaffold.py
@@ -0,0 +1,34 @@
+"""
+"""
+
+from typing import Dict
+
+from fedbiomed.researcher.aggregators.aggregator import Aggregator
+from fedbiomed.researcher.aggregators.functional import federated_averaging
+
+
+class Scaffold(Aggregator):
+    """
+    Defines the Scaffold strategy
+    """
+
+    def __init__(self):
+        """Construct `Scaffold` object as an instance of [`Aggregator`]
+        [fedbiomed.researcher.aggregators.Aggregator].
+        """
+        super(Scaffold, self).__init__()
+        self.aggregator_name = "Scaffold"
+
+    def aggregate(self, model_params: list, weights: list) -> Dict:
+        """ Aggregates local models sent by participating nodes into a global model, using Federated Averaging
+        also present in Scaffold for the aggregation step.
+
+        Args:
+            model_params: contains each model layers
+            weights: contains all weights of a given layer.
+
+        Returns:
+            Aggregated parameters
+        """
+        weights = self.normalize_weights(weights)
+        return federated_averaging(model_params, weights)
