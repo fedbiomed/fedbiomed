b34b97465e8f1da51b5653c61e2140b2cb6b4720
Francesco Cremonesi <francesco.cremonesi@inria.fr>
2022-09-08 17:01:00 +0200

    Remove useless router code from ModalitiesToFolders

--- gui/ui/src/pages/datasets/medical.folder.dataset/ModalitiesToFolders.js
+++ gui/ui/src/pages/datasets/medical.folder.dataset/ModalitiesToFolders.js
@@ -12,22 +12,6 @@ import {
     clearModalityMapping,
     } from "../../../store/actions/medicalFolderDatasetActions"
 
-const withRouter = (Component) =>  {
-    function ComponentWithRouterProp(props) {
-
-      let location = useLocation();
-      let navigate = useNavigate();
-      let params = useParams();
-      return (
-        <Component
-          {...props}
-          router={{location, navigate, params}}
-        />
-      );
-    }
-    return ComponentWithRouterProp;
-}
-
 
 export class ModalitiesToFolders extends React.Component {
     componentDidMount(){
