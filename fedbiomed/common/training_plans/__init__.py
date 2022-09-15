957e508645b7325d8a359df78b7aa7e6898344ad
Francesco Cremonesi <francesco.cremonesi@inria.fr>
2022-09-15 16:54:57 +0200

    Introduce FlambyTrainingPlan

--- fedbiomed/common/training_plans/__init__.py
+++ fedbiomed/common/training_plans/__init__.py
@@ -8,8 +8,10 @@ from ._torchnn import TorchTrainingPlan
 from ._sklearn_training_plan import SKLearnTrainingPlan
 from ._sklearn_models import FedPerceptron, FedSGDRegressor, FedSGDClassifier
 from ._base_training_plan import BaseTrainingPlan
+from ._flamby_training_plan import FlambyTrainingPlan
 
 __all__ = [
+    "FlambyTrainingPlan",
     "TorchTrainingPlan",
     "SKLearnTrainingPlan",
     "FedPerceptron",
---
+++ fedbiomed/common/training_plans/_flamby_training_plan.py
@@ -0,0 +1,27 @@
+from fedbiomed.common.data import DataManager, FlambyDataset
+from fedbiomed.common.training_plans import TorchTrainingPlan
+
+
+class FlambyTrainingPlan(TorchTrainingPlan):
+    def __init__(self, model_args: dict = {}):
+        """ Construct training plan
+
+        Args:
+            model_args: model arguments. Items used in this class build time
+        """
+        super().__init__(model_args)
+        self.add_dependency(["from fedbiomed.common.training_plans import FlambyTrainingPlan"])
+
+    def training_data(self, batch_size=2):
+        """
+
+        If researcher wants to set a transform, they can override the function as follows:
+        def training_data(self, batch_size=2):
+            data_manager = super().training_data(batch_size)
+            transform = myComposedTransform()
+            data_manager.dataset.set_transform(myComposedTransform)
+            return data_manager
+        """
+        dataset = FlambyDataset()
+        train_kwargs = {'batch_size': batch_size, 'shuffle': True}
+        return DataManager(dataset, **train_kwargs)
