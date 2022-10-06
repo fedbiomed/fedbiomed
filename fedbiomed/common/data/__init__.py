11dda4bf574bf46ace771a5ac974ef1116ea670a
Francesco Cremonesi <francesco.cremonesi@inria.fr>
2022-10-06 15:24:30 +0200

    Scikit-learn DataManager now returns a NPDataLoader

--- fedbiomed/common/data/__init__.py
+++ fedbiomed/common/data/__init__.py
@@ -16,6 +16,7 @@ from ._data_loading_plan import (DataLoadingBlock,
                                  SerializationValidation  # keep it for documentation
                                  )
 from ._np_data_loader import NPDataLoader
+
 __all__ = [
     "MedicalFolderBase",
     "MedicalFolderController",
