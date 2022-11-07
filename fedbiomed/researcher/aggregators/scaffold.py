54b7f12314684d3d029c2dcf81b2f1acd6963f95
Paul Andrey <paul.andrey@inria.fr>
2022-11-07 10:40:41 +0100

    Correct `Scaffold` computations.

--- fedbiomed/researcher/aggregators/scaffold.py
+++ fedbiomed/researcher/aggregators/scaffold.py
@@ -56,6 +56,7 @@ class Scaffold(Aggregator):
             raise FedbiomedAggregatorError("SCAFFOLD Error: Server learning rate cannot be equal to 0")
         self.server_lr: float = server_lr
         self.nodes_correction_states: Dict[str, Mapping[str, Union[torch.Tensor, np.ndarray]]] = {}
+        self.global_state: Mapping[str, Union[torch.Tensor, np.ndarray]] = {}
 
         self.nodes_lr: Dict[str, List[float]] = {}
         if fds is not None:
@@ -81,12 +82,12 @@ class Scaffold(Aggregator):
         Performed computations:
         -----------------------
 
-        c_i(+) <- c_i - c + 1/(K*eta_l*eta_g)(x - y_i)
+        c_i(+) <- c_i - c + 1/(K*eta_l)(x - y_i)
         c <- c + 1/N * sum_S(c_i(+) - c_i)
 
         x <- x + eta_g/S * sum_S(y_i - x)
 
-        where, accroding to paper notations
+        where, according to paper notations
             c_i: correction state for node `i`;
             c: correction state at the beginning of round
             eta_g: server's learning rate
@@ -99,7 +100,7 @@ class Scaffold(Aggregator):
 
         Args:
             model_params (list): list of models parameters recieved from nodes
-            weights (List[Dict[str, float]]): weitghs depciting sample proportions available
+            weights (List[Dict[str, float]]): weights depciting sample proportions available
                 on each node. Unused for Scaffold.
             global_model (Mapping[str, Union[torch.Tensor, np.ndarray]]): global model,
                 ie aggregated model
@@ -113,21 +114,22 @@ class Scaffold(Aggregator):
             Dict: aggregated parameters, ie mapping of layer names and layer values.
         """
 
-        #weights_processed = [list(weight.values())[0] for weight in weights] # same retrieving
-
-        model_params_processed = self.scaling(copy.deepcopy(model_params), copy.deepcopy(global_model))
-        # model params are contained in a dictionary with node_id as key, we just retrieve the params
-        model_params_processed = [list(model_param.values())[0] for model_param in model_params_processed]
-
-        aggregated_parameters = weighted_sum(model_params_processed, [1 / len(node_ids)] * len(node_ids))
-
+        # Unpack input local model parameters to {node_id: {name: value, ...}, ...} format.
+        model_params = {list(node_content.keys())[0]: list(node_content.values())[0] for node_content in model_params}
+        # Compute the new aggregated model parameters.
+        aggregated_parameters = {}
+        for key, val in global_model.items():
+            update = sum(params[key] for params in model_params.values()) / len(model_params)
+            newval = (1 - self.server_lr) * val + self.server_lr * update
+            aggregated_parameters[key] = newval
+        # Gather the learning rates used by nodes, updating `self.nodes_lr`.
         self.set_nodes_learning_rate_after_training(training_plan, training_replies, n_round)
+        # At round 0, initialize correction states. FIXME-PAUL: this does not make sense.
         if n_round <= 0:
             self.init_correction_states(global_model, node_ids)
-        model_params = {list(node_content.keys())[0]: list(node_content.values())[0] for node_content in model_params}
-
-        self.update_correction_states(model_params, global_model, node_ids, n_updates)
-
+        # Update correction states.
+        self.update_correction_states(model_params, global_model, n_updates)
+        # Return aggregated parameters.
         return aggregated_parameters
 
     def create_aggregator_args(self,
@@ -241,48 +243,14 @@ class Scaffold(Aggregator):
             node_ids (Iterable[str]): iterable containing node_ids
         """
         # initialize nodes states with zeros tensors
-
-        init_params = {key:initialize(tensor)[1]for key, tensor in global_model.items()}
+        init_params = {key: initialize(tensor)[1] for key, tensor in global_model.items()}
         self.nodes_correction_states = {node_id: copy.deepcopy(init_params) for node_id in node_ids}
+        self.global_state = init_params
 
-    def scaling(self,
-                model_params: List[Dict[str, Mapping[str, Union[torch.Tensor, np.ndarray]]]],
-                global_model: Mapping[str, Union[torch.Tensor, np.ndarray]]) -> List[Dict[str, Mapping[str, Union[torch.Tensor, np.ndarray]]]]:
-        """
-        Computes quantity `x (1 - eta_g) + eta_g * y_i`
-        Proof:
-            x <- x + eta_g * grad(x)
-            x <- x + eta_g / S * sum_i(y_i - x)
-            x <- x (1 - eta_g) + eta_g / S * sum_i(y_i)
-            x <- sum_i(x (1 - eta_g) + eta_g * y_i) / S
-            x <- avg(x (1 - eta_g) + eta_g * y_i) ... averaging is done afterwards, in aggregate method
-
-        where (notations are the same as in the paper):
-        x: global model parameters
-        y_i: local node models updates
-        eta_g: server (researcher)'s learning rate
-        S: number of nodes particpating in the current round
-
-        Args:
-            model_params (list): model parameters to be scaled. model params are a list
-                of Dictionary mapping nodes id with model parameters.
-            global_model (OrderedDict): global model mapping layers names to parameter values
-
-        Returns:
-            list: List of Dict that maps node ids to scaled model parameters (same format as model_params)
-        """
-        # refers as line 13 and 17 in pseudo code
-        # should scale regading option
-        for idx, model_param in enumerate(model_params):
-            node_id = list(model_param.keys())[0] # retrieve node_id
-            for layer in model_param[node_id]:
-                model_params[idx][node_id][layer] = model_param[node_id][layer] * self.server_lr + (1 - self.server_lr) * global_model[layer]
-
-        return copy.deepcopy(model_params)
-
-    def update_correction_states(self, updated_model_params: Dict[str, Mapping[str, Union[torch.Tensor, np.ndarray]]],
+    def update_correction_states(self,
+                                 local_models: Dict[str, Mapping[str, Union[torch.Tensor, np.ndarray]]],
                                  global_model: Mapping[str, Union[torch.Tensor, np.ndarray]],
-                                 node_ids: Iterable[str], n_updates: int = 1,):
+                                 n_updates: int = 1,) -> None:
         """Updates correction states
 
         Proof:
@@ -301,55 +269,62 @@ class Scaffold(Aggregator):
         y_i: local model updates
 
         Args:
-            updated_model_params (Mapping[str, Union[torch.Tensor, np.ndarray]]): node local model after updates,
-                maps layer name to layer values
-            global_model (Mapping[str, Union[torch.Tensor, np.ndarray]]): global model (before updates)
-            node_ids (Iterator[str]): Iterable of all node ids taking part in the round
-            n_updates (int, optional): number of batches (or updates) performed during one round. Refers to `K` in
-            Scaffold paper. Defaults to 1.
+            local_models: Node-wise local model parameters after updates, as
+                as {name: value} parameters mappings indexed by node id.
+            global_model: Global model parameters (before updates), as a single
+                {name: value} parameters mapping.
+            n_updates: number of batches (or updates) performed during one round
+                Referred to as `K` in the Scaffold paper. Defaults to 1.
 
         Raises:
-            FedbiomedAggregatorError: raised if no FederatedDataset has been found
+            FedbiomedAggregatorError: if no FederatedDataset has been found.
         """
-        # refers as line 12, 13 and 17 in pseudo code
+        # Gather the total number of nodes (not just participating ones).
         if self._fds is None:
             raise FedbiomedAggregatorError("Cannot run SCAFFOLD aggregator: No Federated Dataset set")
-        total_nb_nodes = len(self._fds.node_ids())  # get the total number of nodes
-
-        weights = [1/total_nb_nodes] * len(node_ids)
-
-        present_nodes_idx = list(range(len(self._fds.node_ids())))
-
-        for idx, node_id in enumerate(self._fds.node_ids()):
-            if node_id not in node_ids:
-                present_nodes_idx.remove(idx)
-
-        assert len(present_nodes_idx) == len(node_ids)
-        # get weights for weighted summation
-        _tmp_correction_update = []
-
-        for idx, node_id in enumerate( node_ids):
-
-            _tmp_correction_update.append({})
-            #_tmp_correction_update[idx][node_id] = {}
-            lrs: List[float] = self.nodes_lr[node_id]
-
-            for idx_layer, (layer_name, node_layer) in enumerate(updated_model_params[node_id].items()): # iterate params of each client
-
-                # `_tmp_correction_update`` is an intermediate variable equals to 1/ (K * eta_l)(x - y_i) - c
-
-                _tmp_correction_update[idx][layer_name] = (global_model[layer_name] - node_layer) / (self.server_lr * lrs[idx_layer] * n_updates)
-                # FIXME: check why we need server learning_rate in above formulae
-                _tmp_correction_update[idx][layer_name] = _tmp_correction_update[idx][layer_name] - self.nodes_correction_states[node_id][layer_name]
-
-        _aggregated_tmp_correction_update = weighted_sum(_tmp_correction_update, weights)
-
-        # finally, perform `c <- c + S/N \Delta{c}`
-
+        total_nb_nodes = len(self._fds.node_ids())
+        # Compute the node-wise average of corrected gradients (ACG_i).
+        # i.e. (theta^t - theta_i^{t+1}) / (K * eta_l)
+        local_state_updates = {}  # type: Dict[str, Mapping[str, Union[torch.Tensor, np.ndarray]]]
+        for node_id, params in local_models.items():
+            local_state_updates[node_id] = {
+                key: (global_model[key] - val) / (self.nodes_lr[node_id][idx] * n_updates)
+                for idx, (key, val) in enumerate(params.items())
+            }
+        # Compute the shared state variable's update by averaging the former.
+        global_state_update = {
+            key: sum(state[key] for state in local_state_updates.values()) / total_nb_nodes
+            for key in global_model
+        }
+        # Compute the updated shared state variable.
+        # c^{t+1} = (1 - I/N)c^t + (1/N) sum_{i=1}^I ACG_i
+        share = 1 - len(local_models) / total_nb_nodes
+        global_state_new = {
+            key: share * self.global_state[key] + val
+            for key, val in global_state_update.items()
+        }
+        # Compute the difference between past and new shared state variables.
+        global_state_diff = {
+            key: self.global_state[key] - val
+            for key, val in global_state_new.items()
+        }
+        # Compute the updated node-wise correction terms.
         for node_id in self._fds.node_ids():
-            for layer_name, node_layer in global_model.items():
-                self.nodes_correction_states[node_id][layer_name] += _aggregated_tmp_correction_update[layer_name]
-
+            acg = local_state_updates.get(node_id, None)
+            # Case when the node did not participate in the round.
+            # d_i^{t+1} = d_i^t + c^t - c^{t+1}
+            if acg is None:
+                for key, val in self.nodes_correction_states.items():
+                    self.nodes_correction_states[node_id][key] += global_state_diff[key]
+            # Case when the node participated in the round
+            # d_i^{t+1} = c_i^{t+1} - c^{t+1} = ACG_i - d_i^{t} - c^{t+1}
+            else:
+                for key, val in self.nodes_correction_states.items():
+                    self.nodes_correction_states[node_id][key] = (
+                        local_state_updates[node_id][key] - val - global_state_new[key]
+                    )
+        # Assign the updated shared state.
+        self.global_state = global_state_new
 
     def set_training_plan_type(self, training_plan_type: TrainingPlans) -> TrainingPlans:
         """
