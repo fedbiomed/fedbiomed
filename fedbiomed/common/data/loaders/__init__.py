c1582396a95a877167805daf897d759d47e9c054
Francesco Cremonesi <francesco.cremonesi@inria.fr>
2022-11-22 13:15:10 +0100

    Improve docs for loaders

--- fedbiomed/common/data/loaders/__init__.py
+++ fedbiomed/common/data/loaders/__init__.py
@@ -34,6 +34,20 @@ much sense since each node may have a different number of samples in their datas
 need DataLoaders to be able to cycle through the data as many times as needed to achieve the total number of updates
 requested by the researcher.
 
+Calling `__next__` on a DataLoader returns one batch of data. It is important to note that this batch will *always
+contain `batch_size` elements*, regardless of whether some samples in the batch already belong to a new epoch.
+Consider the following example:
+
+- `dataset = [0, 1, 2, 3, 4]`
+- `batch_size = 3`
+
+then the first iteration yields `[0, 1, 2]`, and the second iteration yields `[3, 4, 0]`.
+If shuffling was activated, then the second batch would be assembled as follows:
+
+1. take the last two elements that haven't been used yet for this epoch
+2. shuffle the data
+3. complete the batch with the final missing element
+
 At the moment Fed-BioMed provides a custom implementation for numpy-based datasets
 (the [`NPDataLoader`][fedbiomed.common.data.loaders.NPDataLoader]) while torch-based datasets should rely on the
 [`torch.utils.data.DataLoader`](https://pytorch.org/docs/stable/data.html#torch.utils.data.DataLoader) class.
