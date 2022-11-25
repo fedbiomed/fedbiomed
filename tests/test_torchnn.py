Fix tests due to changes in computation of correction terms for scaffold

tests/test_torchnn.py
@@ -1,5 +1,6 @@
 import copy
 import itertools
+import types
 import unittest
 import os
 import logging
@@ -498,7 +499,7 @@ class TestTorchnn(unittest.TestCase):
         
     def test_torchnn_05_num_updates(self):
         """Test that num_updates parameter is respected correctly.
-
+e
         In the following test, we make sure that no matter the dataset size, nor the batch size, we always perform the
         number of updates requested by the researcher. Remember each update corresponds to one optimizer step, i.e.
         one batch.
@@ -591,15 +592,19 @@ class TestTorchnn(unittest.TestCase):
             tp._dry_run = False
             
             tp.aggregator_name = aggregator_name
-            # mocked_loss_result = MagicMock()
-            # mocked_loss_result.item.return_value = loss_value
-            tp.training_step = lambda x, y: Variable(loss_value + torch.sum(torch.Tensor([torch.dot(y_i, torch.zeros(y_i.shape)) for y_i in y])),
-                                                     requires_grad=True)
-            #tp.init_model = lambda x: 
+            if aggregator_name == 'scaffold':
+                for name, param in tp._model.named_parameters():
+                    tp.correction_state[name] = torch.zeros_like(param)
+
+            def training_step(instance, data, target):
+                return torch.sum(instance.model().forward(data['modality1']))
+
+            tp.training_step = types.MethodType(training_step, tp)
+
             custom_dataset = self.CustomDataset()
             x_train = torch.Tensor(custom_dataset.X_train)
             y_train = torch.Tensor(custom_dataset.Y_train)
-            num_batches = 10
+            num_batches = 1
             batch_size = 5
             dataset_size = num_batches * batch_size
             fake_data = {'modality1': x_train}
@@ -615,7 +620,7 @@ class TestTorchnn(unittest.TestCase):
             tp._dp_controller = FakeDPController()
             return tp
         
-        model = torch.nn.Linear(10, 3)
+        model = torch.nn.Linear(3, 1)
         tp_fedavg = set_training_plan(model, "fedavg", .1)
         tp_fedavg.training_routine(None, None)
         
@@ -628,52 +633,7 @@ class TestTorchnn(unittest.TestCase):
                                                                 tp_scaffold._model.state_dict().items()):
             self.assertTrue(torch.isclose(layer_fedavg, layer_scaffold).all())
 
-        model = torch.nn.Linear(10, 3)
-        correction_state = copy.deepcopy(model)
-        
-        for p in correction_state.parameters():
-            p.data.fill_(1)
-        tp_scaffold = set_training_plan(model, "scaffold", 0.)
-        tp_scaffold.correction_state = correction_state.state_dict()
-
-        tp_scaffold.training_routine(None, None)
-
-        # for (name, layer_scaffold), (name, correction_state) in zip(tp_scaffold._model.state_dict().items(),
-        #                                                             tp_scaffold.correction_state.items()):
-        #     self.assertTrue(torch.isclose(layer_scaffold, correction_state).all())
-      
-    def test_torch_nn_07_compute_corrected_loss_2(self):
-        """test_torch_nn_07_compute_corrected_loss_2: 
-        Tests consistancy of loss values returned by method
-        """
-        tp = TorchTrainingPlan()
-        n_layer = 10
-        dim = 3
-        model = torch.nn.Linear(n_layer, dim)
-        
-        correction_state = copy.deepcopy(model)
-    
-        for p in correction_state.parameters():
-            p.data.fill_(1)
-        
-        # test for Scaffold
-        tp._model = correction_state
-        tp.correction_state = correction_state.state_dict()
-        tp.aggregator_name = 'scaffold'
-
-        loss = torch.tensor(0.)
-        val = tp.compute_corrected_loss(loss)
-        self.assertEqual(val, loss - (n_layer + 1) * dim, "loss should be equal to loss minus number of layers + 1 time input dimension ")
-
-
-        # test for fedavg
-        tp.aggregator_name = 'fedavg'
-        val = tp.compute_corrected_loss(loss)
-        self.assertEqual(loss, val)
-
-        # TODO: test fedprox
-        
-    def test_torch_nn_08_get_learning_rate(self):
+    def test_torch_nn_07_get_learning_rate(self):
         """test_torch_nn_08_get_learning_rate: test we retrieve the appropriate 
         learning rate
         """