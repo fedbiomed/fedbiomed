030b080b3f0a2182030abc49375240400e09615d
Yannick Bouillard <yannick.bouillard@inria.fr>
2022-12-05 15:57:28 +0100

    fixing way parameters are stored

--- fedbiomed/common/training_plans/_torchnn.py
+++ fedbiomed/common/training_plans/_torchnn.py
@@ -101,7 +101,8 @@ class TorchTrainingPlan(BaseTrainingPlan, ABC):
                              ])
 
         # Aggregated model parameters
-        self._init_params = None
+        self._init_params: List[torch.Tensor] = None
+        self._init_torch_model = None
 
     def post_init(
             self,
@@ -154,7 +155,7 @@ class TorchTrainingPlan(BaseTrainingPlan, ABC):
         self._configure_model_and_optimizer()
 
         # Initial aggregated model parameters
-        self._init_params = deepcopy(self._model.state_dict())
+        #self._init_params = deepcopy(list(self._model.parameters()))
 
     @abstractmethod
     def init_model(self):
@@ -431,7 +432,8 @@ class TorchTrainingPlan(BaseTrainingPlan, ABC):
         # self.data = data_loader
 
         # initial aggregated model parameters
-        self._init_params = deepcopy(self._model.state_dict())
+        self._init_params = deepcopy(list(self._model.parameters()))
+
 
         if self._num_updates is not None:
             # compute num epochs and batches from num_updates
@@ -697,6 +699,7 @@ class TorchTrainingPlan(BaseTrainingPlan, ABC):
             L2 norm of model parameters (before local training)
         """
         norm = 0
-        for key, val in self._model.state_dict().items():
-            norm += ((val - self._init_params[key]) ** 2).sum()
+        
+        for current_model, init_model in zip(self._model.parameters(), self._init_params):
+            norm += ((current_model - init_model) ** 2).sum()
         return norm
