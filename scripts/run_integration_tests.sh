adff5117f8039d0fa85ed5fe353ff32801f39ae5
Erwan Demairy <Erwan.Demairy@inria.fr>
2022-12-12 20:53:15 +0100

    addition of a bats tester.

---
+++ scripts/run_integration_tests.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bats  -p
+#  /usr/bin/env bats --formatter junit  --report-formatter junit  --verbose-run
+
+@test "notebook 101_getting-started.py" {
+  ./scripts/run_integration_test -s ./notebooks/101_getting-started.py  \
+                                 -d ./tests/datasets/mnist.json
+}
+# @test "flamby-integration-into-fedbiomed.py" {
+#   run ./scripts/run_integration_test -s ./notebooks/flamby-integration-into-fedbiomed.ipynb  \
+#                                 -d ./tests/datasets/mnist.json >&2
+# }
+
