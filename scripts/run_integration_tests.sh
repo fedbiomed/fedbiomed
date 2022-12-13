0733582f2a3f05ca7485f84fb423407ea52470a0
Erwan Demairy <Erwan.Demairy@inria.fr>
2022-12-13 12:06:29 +0100

    Corrected misuse of mktemp. Attempt to use the TAP format.

--- scripts/run_integration_tests.sh
+++ scripts/run_integration_tests.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-bats_file=$(mktemp /tmp/run_XXXXXX.bats)
+bats_file=$(mktemp /tmp/run.bats.XXXXXX)
+echo ${bats_file}
 exec 3>"$bats_file" # file descriptor 3 
-for notebook in ./notebooks/*.py; do
+for notebook in ./notebooks/*101*.py; do
     echo "adding ${notebook}"
 cat <<EOF >>${bats_file}
 @test "${notebook}" {
@@ -11,10 +12,8 @@ cat <<EOF >>${bats_file}
 }
 EOF
 done
-echo "---- content of bats_file, begin -------"
-cat ${bats_file}
-echo "---- content of bats_file, end -------"
-bats --formatter junit  --report-formatter junit  --verbose-run --show-output-of-passing-tests ${bats_file} 
+
+bats --formatter tap --report-formatter tap --verbose-run --show-output-of-passing-tests ${bats_file} 
 
 # @test "flamby-integration-into-fedbiomed.py" {
 #   run ./scripts/run_integration_test -s ./notebooks/flamby-integration-into-fedbiomed.ipynb  \
