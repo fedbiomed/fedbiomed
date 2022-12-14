62063d28e2385db77d6d6203e20acd4b63d916d8
Erwan Demairy <Erwan.Demairy@inria.fr>
2022-12-14 14:14:04 +0100

    define one directory for outputs of tests.

--- scripts/run_integration_tests.sh
+++ scripts/run_integration_tests.sh
@@ -3,7 +3,7 @@
 bats_file=$(mktemp /tmp/run.bats.XXXXXX)
 echo ${bats_file}
 exec 3>"$bats_file" # associating a file descriptor with the temp file, so that is removed whatever the reason the script ends.
-for notebook in ./notebooks/*101*.py; do
+for notebook in ./notebooks/*.py; do
     echo "adding ${notebook}"
 cat <<EOF >>${bats_file}
 @test "$(basename ${notebook})" {
@@ -13,8 +13,10 @@ cat <<EOF >>${bats_file}
 EOF
 done
 
-rm -fr integration_tests_outputs/*
-bats --formatter tap --report-formatter tap --show-output-of-passing-tests -T -x --verbose-run --gather-test-outputs-in integration_tests_outputs ${bats_file}
+TEST_OUTPUT="integration_tests_outputs${BUILD_NUMBER}
+
+rm -fr ${TEST_OUTPUT}/*
+bats --formatter tap --report-formatter tap --show-output-of-passing-tests -T -x --verbose-run --gather-test-outputs-in ${TEST_OUTPUT} ${bats_file}
 
 # @test "flamby-integration-into-fedbiomed.py" {
 #   run ./scripts/run_integration_test -s ./notebooks/flamby-integration-into-fedbiomed.ipynb  \
