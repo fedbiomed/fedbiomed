a3a12d4c1d7e188198196a076e506bd9f3741df1
Erwan Demairy <Erwan.Demairy@inria.fr>
2022-12-14 11:44:55 +0100

    store the logs for each job.

--- scripts/run_integration_tests.sh
+++ scripts/run_integration_tests.sh
@@ -1,19 +1,21 @@
-#!/bin/bash
+#!/bin/bash -x
 
 bats_file=$(mktemp /tmp/run.bats.XXXXXX)
 echo ${bats_file}
-exec 3>"$bats_file" # file descriptor 3 
+exec 3>"$bats_file" # associating a file descriptor with the temp file, so that is removed whatever the reason the script ends.
 for notebook in ./notebooks/*101*.py; do
     echo "adding ${notebook}"
 cat <<EOF >>${bats_file}
-@test "${notebook}" {
-    run ./scripts/run_integration_test -s ${notebook}  \
+@test "$(basename ${notebook})" {
+# run ./scripts/run_integration_test -s ${notebook}  
+    ./scripts/run_integration_test -s ${notebook}  \
         -d ./tests/datasets/mnist.json
 }
 EOF
 done
 
-bats --formatter tap13 --report-formatter tap13 --verbose-run --show-output-of-passing-tests ${bats_file} 
+rm -fr integration_tests_outputs/*
+bats --formatter tap --report-formatter tap --show-output-of-passing-tests ${bats_file} --verbose-run --gather-test-outputs-in integration_tests_outputs 
 
 # @test "flamby-integration-into-fedbiomed.py" {
 #   run ./scripts/run_integration_test -s ./notebooks/flamby-integration-into-fedbiomed.ipynb  \
