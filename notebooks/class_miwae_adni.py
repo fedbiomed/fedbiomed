d74b97da19a394072ba538140a437d4264918385
ibalelli <irene.balelli@inria.fr>
2023-01-12 17:02:28 +0100

    scripts for miwae with adni

---
+++ notebooks/class_miwae_adni.py
@@ -0,0 +1,222 @@
+import pandas as pd
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.distributions as td
+
+from fedbiomed.common.training_plans import TorchTrainingPlan
+from fedbiomed.common.data import DataManager
+from fedbiomed.common.constants import ProcessTypes
+
+###########################################################
+# Federated standardization                               #
+###########################################################
+
+class FedMeanStdTrainingPlan(TorchTrainingPlan):
+    
+    def init_dependencies(self):
+        deps = ["import pandas as pd",
+            "import numpy as np"]
+        return deps
+        
+    def init_model(self,model_args):
+        
+        model = self.MeanStd(model_args)
+        
+        return model
+    
+    class MeanStd(nn.Module):
+        def __init__(self, model_args):
+            super().__init__()
+            self.n_features=model_args['n_features']
+            
+            self.mean = nn.Parameter(torch.zeros(self.n_features,dtype=torch.float64),requires_grad=False)
+            self.std = nn.Parameter(torch.zeros(self.n_features,dtype=torch.float64),requires_grad=False)
+            self.size = nn.Parameter(torch.zeros(self.n_features,dtype=torch.float64),requires_grad=False)
+            self.fake = nn.Parameter(torch.randn(1),requires_grad=True)
+
+        def forward(self, data):
+            data_np = data.numpy()
+            N = data.shape[0]
+            
+            ### Implementing with np.nanmean, np.nanstd
+            self.size += torch.Tensor([N - np.count_nonzero(np.isnan(data_np[:,dim]))\
+                                    for dim in range(self.n_features)])
+            self.mean += torch.from_numpy(np.nanmean(data_np,0))
+            self.std += torch.from_numpy(np.nanstd(data_np,0))
+            
+            return self.fake
+    
+        
+    def training_data(self):
+        
+        df = pd.read_csv(self.dataset_path, sep=',', index_col=False)
+        
+        ### NOTE: batch_size should be == dataset size ###
+        batch_size = df.shape[0]
+        x_train = df.values.astype(np.float64)
+        #print(x_train.dtype)
+        x_mask = np.isfinite(x_train)
+        xhat_0 = np.copy(x_train)
+        ### NOTE: we keep nan when data is missing
+        #xhat_0[np.isnan(x_train)] = 0
+        train_kwargs = {'batch_size': batch_size, 'shuffle': True}
+        
+        data_manager = DataManager(dataset=xhat_0 , target=x_mask , **train_kwargs)
+        
+        return data_manager
+    
+    def training_step(self, data, mask):
+        
+        return self.model().forward(data)
+
+
+###########################################################
+# Federated miwae                                         #
+###########################################################
+
+class MIWAETrainingPlan(TorchTrainingPlan):
+    
+    def init_dependencies(self):
+        deps = ["import torch.distributions as td",
+            "import pandas as pd",
+            "import numpy as np"]
+        return deps
+        
+    def init_model(self,model_args):
+        
+        if 'standardization' in model_args:
+            self.standardization = True
+            if (('fed_mean' in model_args['standardization']) and ('fed_std' in model_args['standardization'])):
+                self.fed_mean = np.array(model_args['standardization']['fed_mean'])
+                self.fed_std = np.array(model_args['standardization']['fed_std'])
+            else:
+                self.fed_mean = None
+                self.fed_std = None
+                
+        self.n_features=model_args['n_features']
+        self.n_latent=model_args['n_latent']
+        self.n_hidden=model_args['n_hidden']
+        self.n_samples=model_args['n_samples']
+        
+        model = self.MIWAE(model_args)
+        
+        return model
+    
+    class MIWAE(nn.Module):
+        def __init__(self, model_args):
+            super().__init__()
+
+            n_features=model_args['n_features']
+            n_latent=model_args['n_latent']
+            n_hidden=model_args['n_hidden']
+            n_samples=model_args['n_samples']
+
+            # the encoder will output both the mean and the diagonal covariance
+            self.encoder=nn.Sequential(
+                            torch.nn.Linear(n_features, n_hidden),
+                            torch.nn.ReLU(),
+                            torch.nn.Linear(n_hidden, n_hidden),
+                            torch.nn.ReLU(),
+                            torch.nn.Linear(n_hidden, 2*n_latent),  
+                            )
+            # the decoder will output both the mean, the scale, 
+            # and the number of degrees of freedoms (hence the 3*p)
+            self.decoder = nn.Sequential(
+                            torch.nn.Linear(n_latent, n_hidden),
+                            torch.nn.ReLU(),
+                            torch.nn.Linear(n_hidden, n_hidden),
+                            torch.nn.ReLU(),
+                            torch.nn.Linear(n_hidden, 3*n_features),  
+                            )
+
+            self.encoder.apply(self.weights_init)
+            self.decoder.apply(self.weights_init)
+    
+        def weights_init(self,layer):
+            if type(layer) == nn.Linear: torch.nn.init.orthogonal_(layer.weight)
+    
+    def init_optimizer(self,optimizer_args):
+        
+        #SGD optimizer
+        optimizer = torch.optim.SGD(list(self.model().encoder.parameters()) \
+                                    + list(self.model().decoder.parameters()),lr = optimizer_args['lr'])
+        
+        return optimizer
+        
+        
+    def miwae_loss(self,iota_x,mask):
+        # prior
+        self.p_z = td.Independent(td.Normal(loc=torch.zeros(self.n_latent).to(self._device)\
+                                    ,scale=torch.ones(self.n_latent).to(self._device)),1)
+        
+        batch_size = iota_x.shape[0]
+        out_encoder = self.model().encoder(iota_x)
+        
+        q_zgivenxobs = td.Independent(td.Normal(loc=out_encoder[..., :self.n_latent],\
+                                                scale=torch.nn.Softplus()\
+                                                (out_encoder[..., self.n_latent:\
+                                                            (2*self.n_latent)])),1)
+
+        zgivenx = q_zgivenxobs.rsample([self.n_samples])
+        zgivenx_flat = zgivenx.reshape([self.n_samples*batch_size,self.n_latent])
+
+        out_decoder = self.model().decoder(zgivenx_flat)
+        all_means_obs_model = out_decoder[..., :self.n_features]
+        all_scales_obs_model = torch.nn.Softplus()(out_decoder[..., self.n_features:\
+                                                            (2*self.n_features)]) + 0.001
+        all_degfreedom_obs_model = torch.nn.Softplus()\
+        (out_decoder[..., (2*self.n_features):(3*self.n_features)]) + 3
+
+        data_flat = torch.Tensor.repeat(iota_x,[self.n_samples,1]).reshape([-1,1])
+        tiledmask = torch.Tensor.repeat(mask,[self.n_samples,1])
+
+        all_log_pxgivenz_flat = torch.distributions.StudentT\
+        (loc=all_means_obs_model.reshape([-1,1]),\
+        scale=all_scales_obs_model.reshape([-1,1]),\
+        df=all_degfreedom_obs_model.reshape([-1,1])).log_prob(data_flat)
+        all_log_pxgivenz = all_log_pxgivenz_flat.reshape([self.n_samples*batch_size,self.n_features])
+
+        logpxobsgivenz = torch.sum(all_log_pxgivenz*tiledmask,1).reshape([self.n_samples,batch_size])
+        logpz = self.p_z.log_prob(zgivenx)
+        logq = q_zgivenxobs.log_prob(zgivenx)
+
+        neg_bound = -torch.mean(torch.logsumexp(logpxobsgivenz + logpz - logq,0))
+
+        return neg_bound
+
+    def training_data(self,  batch_size = 48):
+        
+        df = pd.read_csv(self.dataset_path, sep=',', index_col=False)
+        x_train = df.values.astype(np.float64)
+        x_mask = np.isfinite(x_train)
+        # xhat_0: missing values are replaced by zeros. 
+        #This x_hat0 is what will be fed to our encoder.
+        xhat_0 = np.copy(x_train)
+        
+        # Data standardization
+        if self.standardization:
+            xhat_0 = self.standardize_data(xhat_0)
+            
+        xhat_0[np.isnan(x_train)] = 0
+        train_kwargs = {'batch_size': batch_size, 'shuffle': True}
+        
+        data_manager = DataManager(dataset=xhat_0 , target=x_mask , **train_kwargs)
+        
+        return data_manager
+    
+    def standardize_data(self,data):
+        data_norm = np.copy(data)
+        if ((self.fed_mean is not None) and (self.fed_std is not None)):
+            print('FEDERATED STANDARDIZATION')
+            data_norm = (data_norm - self.fed_mean)/self.fed_std
+        else:
+            print('LOCAL STANDARDIZATION')
+            data_norm = (data_norm - np.nanmean(data_norm,0))/np.nanstd(data_norm,0)
+        return data_norm
+    
+    def training_step(self, data, mask):
+        self.model().encoder.zero_grad()
+        self.model().decoder.zero_grad()
+        loss = self.miwae_loss(iota_x = data,mask = mask)
+        return loss
---
+++ notebooks/func_miwae_adni.py
@@ -0,0 +1,186 @@
+import pandas as pd
+import numpy as np
+import torch
+import csv, os
+import torch.distributions as td
+
+###########################################################
+#Define the imputation and the MSE functions              #
+###########################################################
+
+def miwae_impute(encoder,decoder,iota_x,mask,d,L):
+
+    p_z = td.Independent(td.Normal(loc=torch.zeros(d),scale=torch.ones(d)),1)
+
+    batch_size = iota_x.shape[0]
+    out_encoder = encoder(iota_x)
+    q_zgivenxobs = td.Independent(td.Normal(loc=out_encoder[..., :d],scale=torch.nn.Softplus()(out_encoder[..., d:(2*d)])),1)
+
+    zgivenx = q_zgivenxobs.rsample([L])
+    zgivenx_flat = zgivenx.reshape([L*batch_size,d])
+
+    out_decoder = decoder(zgivenx_flat)
+    all_means_obs_model = out_decoder[..., :p]
+    all_scales_obs_model = torch.nn.Softplus()(out_decoder[..., p:(2*p)]) + 0.001
+    all_degfreedom_obs_model = torch.nn.Softplus()(out_decoder[..., (2*p):(3*p)]) + 3
+
+    data_flat = torch.Tensor.repeat(iota_x,[L,1]).reshape([-1,1])
+    tiledmask = torch.Tensor.repeat(mask,[L,1])
+
+    all_log_pxgivenz_flat = torch.distributions.StudentT(loc=all_means_obs_model.reshape([-1,1]),scale=all_scales_obs_model.reshape([-1,1]),df=all_degfreedom_obs_model.reshape([-1,1])).log_prob(data_flat)
+    all_log_pxgivenz = all_log_pxgivenz_flat.reshape([L*batch_size,p])
+
+    logpxobsgivenz = torch.sum(all_log_pxgivenz*tiledmask,1).reshape([L,batch_size])
+    logpz = p_z.log_prob(zgivenx)
+    logq = q_zgivenxobs.log_prob(zgivenx)
+
+    xgivenz = td.Independent(td.StudentT(loc=all_means_obs_model, scale=all_scales_obs_model, df=all_degfreedom_obs_model),1)
+
+    imp_weights = torch.nn.functional.softmax(logpxobsgivenz + logpz - logq,0) # these are w_1,....,w_L for all observations in the batch
+    xms = xgivenz.mean.reshape([L,batch_size,p])  # that's the only line that changed!
+    xm=torch.einsum('ki,kij->ij', imp_weights, xms) 
+
+    return xm
+
+def mse(xhat,xtrue,mask): # MSE function for imputations
+    xhat = np.array(xhat)
+    xtrue = np.array(xtrue)
+    return np.mean(np.power(xhat-xtrue,2)[~mask])
+
+def miwae_loss(encoder, decoder, iota_x, mask, d, p, K, batch_size):
+    p_z = td.Independent(td.Normal(loc=torch.zeros(d),scale=torch.ones(d)),1)
+    batch_size = iota_x.shape[0]
+    out_encoder = encoder(iota_x)
+
+    q_zgivenxobs = td.Independent(td.Normal(loc=out_encoder[..., :d],scale=torch.nn.Softplus()(out_encoder[..., d:(2*d)])),1)
+
+    zgivenx = q_zgivenxobs.rsample([K])
+    zgivenx_flat = zgivenx.reshape([K*batch_size,d])
+
+    out_decoder = decoder(zgivenx_flat)
+    all_means_obs_model = out_decoder[..., :p]
+    all_scales_obs_model = torch.nn.Softplus()(out_decoder[..., p:(2*p)]) + 0.001
+    all_degfreedom_obs_model = torch.nn.Softplus()(out_decoder[..., (2*p):(3*p)]) + 3
+
+    data_flat = torch.Tensor.repeat(iota_x,[K,1]).reshape([-1,1])
+    tiledmask = torch.Tensor.repeat(mask,[K,1])
+
+    all_log_pxgivenz_flat = torch.distributions.StudentT(loc=all_means_obs_model.reshape([-1,1]),scale=all_scales_obs_model.reshape([-1,1]),df=all_degfreedom_obs_model.reshape([-1,1])).log_prob(data_flat)
+    all_log_pxgivenz = all_log_pxgivenz_flat.reshape([K*batch_size,p])
+
+    logpxobsgivenz = torch.sum(all_log_pxgivenz*tiledmask,1).reshape([K,batch_size])
+    logpz = p_z.log_prob(zgivenx)
+    logq = q_zgivenxobs.log_prob(zgivenx)
+
+    neg_bound = -torch.mean(torch.logsumexp(logpxobsgivenz + logpz - logq,0))
+
+    return neg_bound
+
+def recover_data(data_missing, data_full, fed_mean, fed_std):
+
+    # TEST DATA: data_missing = data_test_missing; data_full = data_test
+    # CLIENT i, i=0,1,2: data_missing = Clients_missing[i]; data_full = Clients_data[i]
+
+    xmiss = np.copy(data_missing)
+    mask = np.isfinite(xmiss) # binary mask that indicates which values are missing
+
+    # Evaluate local mean and std of test dataset
+    mean = np.nanmean(xmiss,0)
+    std = np.nanstd(xmiss,0)
+
+    # standardization with respect to the federated dataset
+    xmiss_global_std = np.copy(data_missing)
+    xmiss_global_std = (xmiss_global_std - fed_mean.numpy())/fed_std.numpy()
+    xhat_0_global_std = np.copy(xmiss_global_std)
+    xhat_0_global_std[np.isnan(xmiss_global_std)] = 0
+    xhat_global_std = np.copy(xhat_0_global_std) # This will be out imputed data matrix
+    xfull_global_std = np.copy(data_full)
+    xfull_global_std = (xfull_global_std - fed_mean.numpy())/fed_std.numpy()
+
+    # local standardization
+    xmiss_local_std = np.copy(data_missing)
+    xmiss_local_std = (xmiss_local_std - mean)/std
+    xhat_0_local_std = np.copy(xmiss_local_std)
+    xhat_0_local_std[np.isnan(xmiss_local_std)] = 0
+    xhat_local_std = np.copy(xhat_0_local_std) # This will be out imputed data matrix
+    xfull_local_std = np.copy(data_full)
+    xfull_local_std = (xfull_local_std - mean)/std
+
+    return xmiss, mask, xhat_global_std, xfull_global_std, xhat_local_std, xfull_local_std
+
+def testing_func(data_missing, data_full, mask, encoder,decoder,d,L):
+
+    xhat = np.copy(data_missing)
+    xhat_0 = np.copy(data_missing)
+    xfull = np.copy(data_full)
+
+    xhat[~mask] = miwae_impute(encoder = encoder,decoder = decoder,iota_x = torch.from_numpy(xhat_0).float(),mask = torch.from_numpy(mask).float(),d = d,L= L).cpu().data.numpy()[~mask]
+    err = np.array([mse(xhat,xfull,mask)])
+
+    return float(err)
+
+def save_results(exp_id,Split_type,Train_data,Test_data,
+                perc_missing_train,perc_missing_test,model,
+                N_train_centers,Size,N_rounds,N_epochs,
+                std_training,std_testing,MSE):
+    os.makedirs('results', exist_ok=True) 
+    output_file_name = 'output_'+str(exp_id)+'_'+str(np.random.randint(9999, dtype=int))+'.csv'
+    fieldnames=['Split_type', 'Train_data', 'Test_data', 'perc_missing_train', 
+                'perc_missing_test', 'model', 
+                'N_train_centers', 'Size', 'N_rounds', 'N_epochs',
+                'std_training', 'std_testing', 'MSE']
+    if not os.path.exists('results/'+output_file_name):
+        output = open("results/"+output_file_name, "w")
+        writer = csv.DictWriter(output, fieldnames=fieldnames, delimiter = ';')
+        writer.writeheader()
+        output.close()
+
+    # Dictionary to be added
+    dict_out={'Split_type': Split_type, 'Train_data': Train_data, 'Test_data': Test_data, 
+                'perc_missing_train': perc_missing_train, 'perc_missing_test': perc_missing_test, 
+                'model': model, 'N_train_centers': N_train_centers, 'Size': Size, 
+                'N_rounds': N_rounds, 'N_epochs': N_epochs,
+                'std_training': std_training, 'std_testing': std_testing, 'MSE': MSE}
+
+    with open('results/'+output_file_name, 'a') as output_file:
+        dictwriter_object = csv.DictWriter(output_file, fieldnames=fieldnames, delimiter = ';')
+        dictwriter_object.writerow(dict_out)
+        dictwriter_object.writerow(dict_out)
+        output_file.close()
+
+def databases(Split_type,idx_clients,idx_Test_data,N_cl):
+    cwd = os.getcwd()
+
+    data_folder = 'Data/'
+    if Split_type == 'notIID':
+        data_folder += 'ADNI_notiid'
+    elif Split_type == 'site_1':
+        data_folder += 'ADNI_site_1'
+    elif Split_type == 'site_2':
+        data_folder += 'ADNI_site_2'
+     
+    if Split_type == 'site_2':
+        Perc_missing = [0.3,0.2,0.4,0.1]
+        Perc_missing_test = Perc_missing[idx_Test_data-1]
+        Perc_missing.remove(Perc_missing_test)
+    else:
+        Perc_missing = [0.3 for _ in range(N_cl)]
+        Perc_missing_test = 0.3
+
+    Clients_data=[]
+    Clients_missing=[]
+    for i in idx_clients:
+        data_full_file = os.path.join(cwd, data_folder+"/dataset_full_"+str(i)+".csv")
+        data_full = pd.read_csv(data_full_file, sep=",",index_col=False)
+        Clients_data.append(data_full)
+        data_file = os.path.join(cwd, data_folder+"/dataset_"+str(i)+".csv")
+        data = pd.read_csv(data_file, sep=",",index_col=False)
+        Clients_missing.append(data)
+
+    test_file = os.path.join(cwd, data_folder+"/dataset_full_"+str(idx_Test_data)+".csv")
+    data_test = pd.read_csv(test_file, sep=",",index_col=False)
+    test_missing_file = os.path.join(cwd, data_folder+"/dataset_"+str(idx_Test_data)+".csv")
+    data_test_missing = pd.read_csv(test_missing_file, sep=",",index_col=False)
+
+    return Clients_data, Clients_missing, data_test, data_test_missing, Perc_missing, Perc_missing_test
+    
