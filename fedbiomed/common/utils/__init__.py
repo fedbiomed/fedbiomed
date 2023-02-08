18206eaee79cb22f0a964f801ac1bbcc31c678d1
Marc Vesin <marc.vesin@inria.fr>
2023-02-08 12:39:37 +0100

    misc better imports for common.utils modules: replace * by explicit

--- fedbiomed/common/utils/__init__.py
+++ fedbiomed/common/utils/__init__.py
@@ -1,5 +1,40 @@
 # This file is originally part of Fed-BioMed
 # SPDX-License-Identifier: Apache-2.0
 
-from ._utils import *
-from ._config_utils import *
+from ._utils import (
+    read_file,
+    get_class_source,
+    is_ipython,
+    get_ipython_class_file,
+    get_method_spec,
+    convert_to_python_float,
+    convert_iterator_to_list_of_python_floats,
+    compute_dot_product,
+)
+from ._config_utils import (
+    get_fedbiomed_root,
+    get_component_config,
+    get_component_certificate_from_config,
+    get_all_existing_config_files,
+    get_all_existing_certificates,
+    get_existing_component_db_names,
+)
+
+__all__ = [
+    # _utils
+    "read_file",
+    "get_class_source",
+    "is_ipython",
+    "get_ipython_class_file",
+    "get_method_spec",
+    "convert_to_python_float",
+    "convert_iterator_to_list_of_python_floats",
+    "compute_dot_product",
+    # _config_utils
+    "get_fedbiomed_root",
+    "get_component_config",
+    "get_component_certificate_from_config",
+    "get_all_existing_config_files",
+    "get_all_existing_certificates",
+    "get_existing_component_db_names",
+]
