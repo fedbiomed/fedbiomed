325a7a440fd3c819e1e1b5dac2bbb309c499a064
Yannick Bouillard <yannick.bouillard@inria.fr>
2023-03-02 14:26:07 +0100

    forgot to add model that has been split into several files

---
+++ fedbiomed/common/models/__init__.py
@@ -0,0 +1,18 @@
+# This file is originally part of Fed-BioMed
+# SPDX-License-Identifier: Apache-2.0
+
+"""
+The `fedbiomed.common.models` module includes model abstraction classes
+that can be used either with plain framework specific models or with declearn model
+
+declearn repository: https://gitlab.inria.fr/magnet/declearn/declearn2/-/tree/r2.1
+"""
+from ._model import Model
+from ._sklearn import SkLearnModel
+from ._torch import TorchModel
+
+__all__ = [
+    "SkLearnModel",
+    "TorchModel",
+    "Model"
+]
