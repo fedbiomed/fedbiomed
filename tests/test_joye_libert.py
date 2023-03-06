d1c98d68a473b46117c9a1861e1a0676cc77d29e
Sergen Cansiz <sergen.cansiz@inria.fr>
2023-03-06 14:47:17 +0100

    add unittest file for JLS implementation

---
+++ tests/test_joye_libert.py
@@ -0,0 +1,54 @@
+import unittest
+
+
+class TestJoyeLibert(unittest.TestCase):
+
+    def setUp(self) -> None:
+        pass
+
+    def tearDown(self) -> None:
+        pass
+
+
+class TestPublicParam(unittest.TestCase):
+
+    def setUp(self) -> None:
+        pass
+
+    def tearDown(self) -> None:
+        pass
+
+class TestFDH(unittest.TestCase):
+
+    def setUp(self) -> None:
+        pass
+
+    def tearDown(self) -> None:
+        pass
+
+
+class TestEncryptedNumber(unittest.TestCase):
+
+    def setUp(self) -> None:
+        pass
+
+    def tearDown(self) -> None:
+        pass
+
+
+class TestUserKey(unittest.TestCase):
+
+    def setUp(self) -> None:
+        pass
+
+    def tearDown(self) -> None:
+        pass
+
+
+class TestServerKey(unittest.TestCase):
+
+    def setUp(self) -> None:
+        pass
+
+    def tearDown(self) -> None:
+        pass
