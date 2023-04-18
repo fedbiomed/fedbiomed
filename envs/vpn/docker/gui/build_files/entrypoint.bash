f979516db95d3f1d6b340acd048705bec0a4a917
Sergen Cansiz <sergen.cansiz@inria.fr>
2023-04-18 10:45:55 +0200

    launch Fed-BioMed GUI as production in docker

--- envs/vpn/docker/gui/build_files/entrypoint.bash
+++ envs/vpn/docker/gui/build_files/entrypoint.bash
@@ -11,7 +11,7 @@ change_path_owner "/fedbiomed" "/home/$CONTAINER_BUILD_USER"
 
 # caveat: expect `data-folder` to be mounted under same path as in `node` container
 # to avoid inconsistencies in dataset declaration
-$SETUSER ./scripts/fedbiomed_run gui host 0.0.0.0 data-folder /data config config_node.ini start &
+$SETUSER ./scripts/fedbiomed_run gui --host 0.0.0.0 --production --data-folder /data config config_node.ini start &
 
 # allow to stop/restart the gui without terminating the container
 sleep infinity &
