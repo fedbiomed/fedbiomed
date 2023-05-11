19764c8d098a18cf2c4b1333282477ac2129e6d1
Francesco Cremonesi <francesco.cremonesi@inria.fr>
2023-05-11 15:22:33 +0200

    Add version number for breakpoints

--- fedbiomed/common/constants.py
+++ fedbiomed/common/constants.py
@@ -36,6 +36,7 @@ MPSPDZ_certificate_prefix = "MPSPDZ_certificate"
 __version__ = FBM_Component_Version('4.3.1')  # Fed-BioMed software version
 __researcher_config_version__ = FBM_Component_Version('1')  # researcher config file version
 __node_config_version__ = FBM_Component_Version('1')  # node config file version
+__breakpoints_version__ = FBM_Component_Version('1')  # breakpoints format version
 
 
 class _BaseEnum(Enum):
