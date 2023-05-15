64427b116938b6800b5ae4d85953bc85b532a863
Marc Vesin <marc.vesin@inria.fr>
2023-05-15 10:03:19 +0200

    WIP protocol versioning review - need to harden version type check in message handling - misc message format

--- fedbiomed/common/constants.py
+++ fedbiomed/common/constants.py
@@ -295,7 +295,8 @@ class ErrorNumbers(_BaseEnum):
     FB621 = "FB621: declearn optimizer error"
     FB622 = "FB622: Model error"
     FB623 = "FB623: Secure aggregation database error"
-    FB624 = "FB620: Secure aggregation crypter error"
+    FB624 = "FB624: Secure aggregation crypter error"
+    FB625 = "FB625: Component version error"
     # oops
     FB999 = "FB999: unknown error code sent by the node"
 
