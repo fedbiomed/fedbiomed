bd811b088e06d89e70ae42ee38aee3df11a106b7
Erwan Demairy <Erwan.Demairy@inria.fr>
2023-09-11 15:32:40 +0200

    non-blocking notebooks tests.
    This test does not block when testing the notebooks. Must be still tested on ci.

--- scripts/run_integration_tests.sh
+++ scripts/run_integration_tests.sh
@@ -4,7 +4,7 @@
 
 bats_file=$(mktemp /tmp/run.bats.XXXXXX)
 echo ${bats_file}
-exec 3>"$bats_file" # associating a file descriptor with the temp file, so that is removed whatever the reason the script ends.
+exec 5>"$bats_file" # associating a file descriptor with the temp file, so that is removed whatever the reason the script ends.
 list_notebooks=( notebooks/101_getting-started.py notebooks/general-breakpoint-save-resume.py notebooks/general-tensorboard.py notebooks/general-use-gpu.py notebooks/pytorch-celeba-dataset.py notebooks/pytorch-csv-data.py notebooks/pytorch-local-training.py notebooks/pytorch-variational-autoencoder.py notebooks/test_nbconvert.py )
 #for notebook in ./notebooks/*.py; do
 test_counter=1
@@ -21,11 +21,6 @@ cat <<EOF >>${bats_file}
 EOF
 let test_counter+=1
 done
-#@test "$(basename ${notebook})" {
-#    run -0 ./scripts/run_integration_test -s ${notebook}  \
-#        -d ./tests/datasets/mnist.json \
-#        -d ./tests/datasets/celeba.json >&3
-#}
 
 TEST_OUTPUT="integration_tests_outputs${BUILD_NUMBER}"
 
