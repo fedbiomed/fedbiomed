d0a0fe669bc8f31ea37c0117030bdeeb23c87f68
Erwan Demairy <Erwan.Demairy@inria.fr>
2023-09-12 15:55:09 +0200

    Changing "integration" to "end_to_end"

--- scripts/run_end_to_end_tests.sh
+++ scripts/run_end_to_end_tests.sh
@@ -14,7 +14,7 @@ for notebook in ${list_notebooks[@]}; do
     echo "adding ${notebook}"
 cat <<EOF >>${bats_file}
 @test "${test_counter} - $(basename ${notebook})" {
-    ./scripts/run_integration_test -s ${notebook}  \
+    ./scripts/run_end_to_end_test -s ${notebook}  \
         -d ./tests/datasets/mnist.json  \
         -d ./tests/datasets/celeba.json 3>&- \
 #        3>&-   # indispensable to prevent the tests to hang.
@@ -23,13 +23,7 @@ EOF
 let test_counter+=1
 done
 
-TEST_OUTPUT="integration_tests_outputs${BUILD_NUMBER}"
+TEST_OUTPUT="end_to_end_tests_outputs${BUILD_NUMBER}"
 
 rm -fr ${TEST_OUTPUT}/*
 bats --formatter tap --report-formatter tap --show-output-of-passing-tests -T -x --verbose-run --gather-test-outputs-in ${TEST_OUTPUT} ${bats_file}
-
-# @test "flamby-integration-into-fedbiomed.py" {
-#   run ./scripts/run_end_to_end_test -s ./notebooks/flamby-integration-into-fedbiomed.ipynb  \
-#                                 -d ./tests/datasets/mnist.json >&2
-# }
-
