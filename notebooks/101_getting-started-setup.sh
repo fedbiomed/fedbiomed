d0684877045acbff42c879cffd6e127d6d23667b
Erwan Demairy <Erwan.Demairy@inria.fr>
2023-09-13 10:50:31 +0200

    Debugging dataset setup.

--- notebooks/101_getting-started-setup.sh
+++ notebooks/101_getting-started-setup.sh
@@ -1,3 +1,12 @@
-conda activate fedbiomed-node
-./scripts/fedbiomed_run node -adff ./tests/datasets/mnist.json
+eval "$(conda shell.$(basename $SHELL) activate fedbiomed-node)"
+
+./scripts/fedbiomed_run node list | grep MNIST
+if [ $? -eq 0 ]
+then
+    echo "MNIST already installed, not doing anything"
+else
+    echo "MNIST seems not to be installed, installing it"
+    ./scripts/fedbiomed_run node -adff ./tests/datasets/mnist.json
+fi
+
 
