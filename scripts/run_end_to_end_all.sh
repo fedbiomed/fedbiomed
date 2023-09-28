3b7fbab8ed672db7d381f19c25dfd7432e5ef8dc
Erwan Demairy <Erwan.Demairy@inria.fr>
2023-09-28 16:32:47 +0200

    Refactoring: - conda environment moved to ci envs; - some cleaning of bash expressions.

--- scripts/run_end_to_end_all.sh
+++ scripts/run_end_to_end_all.sh
@@ -3,10 +3,10 @@
 # SPDX-License-Identifier: Apache-2.0
 
 bats_file=$(mktemp /tmp/run.bats.XXXXXX)
-echo ${bats_file}
+echo "${bats_file}"
 exec 5>"$bats_file" # associating a file descriptor with the temp file, so that is removed whatever the reason the script ends.
 
-if [ "`uname`" == "Darwin" ]; then
+if [ "$(uname)" == "Darwin" ]; then
   conda env update -f ./envs/development/conda/fedbiomed-researcher-macosx.yaml
   conda env update -f ./envs/development/conda/fedbiomed-node-macosx.yaml
 else
@@ -14,7 +14,7 @@ else
   conda env update -f ./envs/development/conda/fedbiomed-node.yaml
 fi
 conda env update -f ./envs/development/conda/fedbiomed-network.yaml
-conda env update -f ./envs/development/conda/fedbiomed-researcher-end-to-end.yaml
+conda env update -f ./envs/ci/conda/fedbiomed-researcher-end-to-end.yaml
 
 rmdir ./data
 ln -s ~/Data/fedbiomed ./data
@@ -39,9 +39,9 @@ pytorch-variational-autoencoder\
 #declearn-with-sklearn,\
 
 test_counter=1
-for notebook in ${list_notebooks[@]}; do # \todo remove the split :..:.. when all notebooks are working.
+for notebook in "${list_notebooks[@]}"; do
     echo "adding ${notebook}"
-cat <<EOF >>${bats_file}
+cat <<EOF >>"${bats_file}"
 @test "${test_counter} - $(basename ${notebook})" {
     ./scripts/run_end_to_end_one.sh -s ${notebook}  \
         -d ./tests/datasets/mnist.json  \
@@ -50,11 +50,11 @@ cat <<EOF >>${bats_file}
 #        3>&-   # indispensable to prevent the tests to hang.
 }
 EOF
-let test_counter+=1
+(( test_counter+=1 ))
 done
 
 TEST_OUTPUT="end_to_end_tests_outputs"
 echo "cleaning directory ${TEST_OUTPUT}"
 rm -fr ./${TEST_OUTPUT}/*
 
-bats --formatter junit --report-formatter junit --show-output-of-passing-tests -T -x --verbose-run --gather-test-outputs-in ${TEST_OUTPUT} ${bats_file}
+bats --formatter junit --report-formatter junit --show-output-of-passing-tests -T -x --verbose-run --gather-test-outputs-in ${TEST_OUTPUT} "${bats_file}"
