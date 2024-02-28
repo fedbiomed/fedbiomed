28b1b6fe098d8bf4fc33b74bdecfa23dc390342f
Paul Andrey <paul.andrey@inria.fr>
2024-02-28 10:49:10 +0100

    Extend 'Serializer' support to 'set' objects.

--- fedbiomed/common/serializer.py
+++ fedbiomed/common/serializer.py
@@ -112,6 +112,8 @@ class Serializer:
             }
         if isinstance(obj, tuple):
             return {"__type__": "tuple", "value": list(obj)}
+        if isinstance(obj, set):
+            return {"__type__": "set", "value": list(obj)}
         if isinstance(obj, np.ndarray):
             spec = [obj.tobytes(), obj.dtype.name, list(obj.shape)]
             return {"__type__": "np.ndarray", "value": spec}
@@ -145,6 +147,8 @@ class Serializer:
             return tuple(obj["value"])
         if objtype == "int":
             return int.from_bytes(obj["value"], byteorder="big", signed=True)
+        if objtype == "set":
+            return set(obj["value"])
         if objtype == "np.ndarray":
             data, dtype, shape = obj["value"]
             return np.frombuffer(data, dtype=dtype).reshape(shape).copy()
