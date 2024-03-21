ca806b6dcf4a1fbd3aa0a3ab4bc79f3f6e237a5d
Marc Vesin <marc.vesin@inria.fr>
2024-03-21 13:04:30 +0100

    WIP version increment for components

--- fedbiomed/common/constants.py
+++ fedbiomed/common/constants.py
@@ -61,9 +61,9 @@ SERVER_certificate_prefix = "server_certificate"
 __version__ = FBM_Component_Version('5.1.0')  # Fed-BioMed software version
 __researcher_config_version__ = FBM_Component_Version('2')  # researcher config file version
 __node_config_version__ = FBM_Component_Version('2')  # node config file version
-__node_state_version__ = FBM_Component_Version('1')  # node state version
-__breakpoints_version__ = FBM_Component_Version('3')  # breakpoints format version
-__messaging_protocol_version__ = FBM_Component_Version('2')  # format of gRPC messages.
+__node_state_version__ = FBM_Component_Version('2')  # node state version
+__breakpoints_version__ = FBM_Component_Version('4')  # breakpoints format version
+__messaging_protocol_version__ = FBM_Component_Version('3')  # format of gRPC messages.
 # Nota: for messaging protocol version, all changes should be a major version upgrade
 
 # Max message length as bytes
