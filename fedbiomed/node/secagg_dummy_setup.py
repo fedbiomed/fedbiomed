9359061853b4a6931c213f1b5dfed4abe6d81498
Marc Vesin <marc.vesin@inria.fr>
2024-06-03 16:15:55 +0200

    WIP PoC for noe2node application layer - add sketch API in SecaggSetup

--- fedbiomed/node/secagg_dummy_setup.py
+++ fedbiomed/node/secagg_dummy_setup.py
@@ -6,13 +6,13 @@ import uuid
 
 from fedbiomed.common.constants import REQUEST_PREFIX, \
     TIMEOUT_NODE_TO_NODE_REQUEST
-from fedbiomed.common.message import NodeToNodeMessages, NodeMessages
 from fedbiomed.common.logger import logger
 
 from fedbiomed.transport.controller import GrpcController
 
+from fedbiomed.common.message import NodeToNodeMessages
 from fedbiomed.node.environ import environ
-from fedbiomed.node.overlay import format_outgoing_overlay
+from fedbiomed.node.overlay import send_overlay_message
 from fedbiomed.node.pending_requests import PendingRequests
 
 
@@ -36,34 +36,27 @@ class SecaggDummySetup:
 
     def setup(self):
         other_nodes = [ e for e in self._parties[1:] if e != environ['NODE_ID'] ]
-        request_ids = []
+        other_nodes_messages = []
 
         for node in other_nodes:
             # For real use: catch FedbiomedNodeToNodeError when calling `format_outgoing_overlay`
-            message_inner = NodeToNodeMessages.format_outgoing_message(
-                {
+            other_nodes_messages += [
+                NodeToNodeMessages.format_outgoing_message({
                     'request_id': REQUEST_PREFIX + str(uuid.uuid4()),
                     'node_id': environ['NODE_ID'],
                     'dest_node_id': node,
                     'dummy': f"DUMMY INNER from {environ['NODE_ID']}",
                     'command': 'key-request'
                 })
+            ]
+        listener_id = send_overlay_message(
+            self._grpc_client,
+            self._pending_requests,
+            self._researcher_id,
+            other_nodes,
+            other_nodes_messages,
+        )
 
-            message_overlay = NodeMessages.format_outgoing_message(
-                {
-                    'researcher_id': self._researcher_id,
-                    'node_id': environ['NODE_ID'],
-                    'dest_node_id': node,
-                    'overlay': format_outgoing_overlay(message_inner),
-                    'command': 'overlay-send'
-                })
-
-            logger.debug(f"SECAGG DUMMY: SENDING OVERLAY message to {node}: {message_overlay}")
-            request_ids += [message_inner.get_param('request_id')]
-            self._grpc_client.send(message_overlay)
-
-        print(f"PENDING REQUESTS {self._pending_requests}")
-        listener_id = self._pending_requests.add_listener(request_ids)
         all_received, messages = self._pending_requests.wait(listener_id, TIMEOUT_NODE_TO_NODE_REQUEST)
         logger.debug(f"SECAGG DUMMY: ALL RECEIVED ? {all_received}")
         logger.debug(f"SECAGG DUMMY: RECEIVED MESSAGES {messages}")
@@ -72,7 +65,7 @@ class SecaggDummySetup:
         return {
             'researcher_id': self._researcher_id,
             'secagg_id': self._secagg_id,
-            'success': True,
+            'success': all_received,
             'msg': 'DUMMY DH SECAGG COMPLETED',
             'command': 'secagg'
         }
