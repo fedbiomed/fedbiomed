f366af2c5ddedc09d963fd823a6567335487208c
Marc Vesin <marc.vesin@inria.fr>
2024-06-06 17:03:16 +0200

    WIP PoC for node2node application layer - introduce ProtocolHandler with one method per message type to process

--- fedbiomed/node/requests/__init__.py
+++ fedbiomed/node/requests/__init__.py
@@ -7,6 +7,7 @@ to simplify imports from fedbiomed.node.requests
 
 from ._overlay import format_outgoing_overlay, format_incoming_overlay, send_overlay_message
 from ._pending_requests import PendingRequests
+from ._protocol_handler import ProtocolHandler
 from ._protocol_manager import ProtocolManager
 
 __all__ = [
@@ -14,5 +15,6 @@ __all__ = [
     'format_incoming_overlay',
     'send_overlay_message',
     'PendingRequests',
+    'ProtocolHandler',
     'ProtocolManager',
 ]
