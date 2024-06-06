72fd9d5b4e54e96c17260f5ed5843b7f623fbe3f
rtaiello <taiello.riccardo@gmail.com>
2024-06-06 15:21:18 +0200

    fix exceptions
    Refactor DHKey class and switch from pycryptodome to cryptography
    
    - Renamed `DHKeysGeneration` to `DHKey`.
    - Modified `DHKey` to handle both key generation and key importing.
    - Added constructor parameters to accept optional private and public keys in PEM format.
    - Added error handling for invalid key formats.
    - Updated export methods to use `@property` notation.
    - Updated docstrings to remove type specifications in parentheses.
    - Switched from `pycryptodome` to `cryptography` library for cryptographic operations.
    - Fixed dependencies in the envs folder to reflect the use of `cryptography` instead of `pycryptodome`.
    
    Update test cases to reflect the changes:
    - Updated test cases to test both key generation and key importing.
    - Added tests for invalid key imports.
    - Updated calls to export methods to use `@property` notation.
    
    fixed cryptography and pyopessl wrong versions
    
    Implements suggestions from PR review
    
    Update fedbiomed/common/secagg/_dh.py
    
    Co-authored-by: srcansiz <sergen.cansiz@inria.fr>
    
    Update fedbiomed/common/secagg/_dh.py
    
    Co-authored-by: srcansiz <sergen.cansiz@inria.fr>
    
    Update fedbiomed/common/secagg/_dh.py
    
    Co-authored-by: srcansiz <sergen.cansiz@inria.fr>
    
    Update fedbiomed/common/secagg/_dh.py
    
    Co-authored-by: srcansiz <sergen.cansiz@inria.fr>
    
    updated code with PR suggestions
    
    implements some improvements

--- fedbiomed/common/secagg/_dh.py
+++ fedbiomed/common/secagg/_dh.py
@@ -1,46 +1,105 @@
-from Crypto.PublicKey import ECC
-from Crypto.Hash import TupleHash128
-from Crypto.Protocol.DH import key_agreement
+from cryptography.hazmat.primitives.asymmetric import ec
+from cryptography.hazmat.primitives import serialization, hashes
+from cryptography.hazmat.primitives.kdf.concatkdf import ConcatKDFHash
+from cryptography.hazmat.backends import default_backend
+
+from typing import Callable
+
 from fedbiomed.common.exceptions import FedbiomedSecaggCrypterError
 from fedbiomed.common.constants import ErrorNumbers
 
 
-class DHKeysGeneration:
+class DHKey:
     """
-    Key Generation with ephemeral-ephemeral ECDH, a session is defined for each experiment.
+    Key handling for ephemeral-ephemeral ECDH, a session is defined for each experiment.
 
-    This class handles the generation of ECC keys using the P-256 curve. It provides methods
+    This class handles the generation and importing of ECC keys using the P-256 curve. It provides methods
     to export the private and public keys in PEM format as bytes.
 
     Attributes:
-        private_key (ECC.EccKey): The user's private ECC key.
-        public_key (ECC.EccKey): The user's public ECC key.
+        private_key: The user's private ECC key.
+        public_key: The user's public ECC key.
     """
 
-    def __init__(self):
+    def __init__(
+        self,
+        private_key_pem: bytes | None = None,
+        public_key_pem: bytes | None = None
+    ) -> None:
         """
-        Initializes the DHKeysGeneration instance by generating a private key and deriving the public key.
-        """
-        self._private_key: ECC.EccKey = ECC.generate(curve="P-256")
-        self._public_key: ECC.EccKey = self._private_key.public_key()
+        Initializes the DHKey instance by generating a new key pair or importing the provided keys.
 
-    def export_private_key(self) -> bytes:
+        Args:
+            private_key_pem: Optional. The private key in PEM format.
+            public_key_pem: Optional. The public key in PEM format.
+        """
+        if private_key_pem:
+            self.private_key = self._import_key(
+                serialization.load_pem_private_key,
+                data=private_key_pem,
+                password=None,
+                backend=default_backend()
+            )
+        elif not public_key_pem:
+            self.private_key = ec.generate_private_key(
+                ec.SECP256R1(), default_backend()
+            )
+        else:
+            # Means that only public key is loaded
+            self.private_key = None
+
+        if public_key_pem:
+            self.public_key = self._import_key(serialization.load_pem_public_key,
+                data=public_key_pem, backend=default_backend()
+            )
+        else:
+            self.public_key = self.private_key.public_key()
+
+
+    def export_private_key(self):
         """
         Exports the private key to PEM format.
 
         Returns:
-            bytes: The private key in PEM format.
+            The private key in PEM format. Returns `None` if private key
+                is not loaded.
         """
-        return self._private_key.export_key(format="PEM").encode("utf-8")
+        if not self.private_key:
+            return None
 
-    def export_public_key(self) -> bytes:
+        return self.private_key.private_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PrivateFormat.PKCS8,
+            encryption_algorithm=serialization.NoEncryption(),
+        )
+
+    def export_public_key(self):
         """
         Exports the public key to PEM format.
 
         Returns:
-            bytes: The public key in PEM format.
+            The public key in PEM format.
         """
-        return self._public_key.export_key(format="PEM").encode("utf-8")
+        return self.public_key.public_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PublicFormat.SubjectPublicKeyInfo,
+        )
+
+    @staticmethod
+    def _import_key(func: Callable, **kwargs):
+        """Executes import function
+
+        Args:
+            func: private key import function or public key import function
+            kwargs: Argument to pass to the `func`
+        """
+        try:
+            return func(**kwargs)
+        except ValueError as exp:
+            raise FedbiomedSecaggCrypterError(
+                f"{ErrorNumbers.FB629.value}: Invalid key format, {kwargs}"
+            ) from exp
+
 
 
 class DHKeyAgreement:
@@ -51,72 +110,58 @@ class DHKeyAgreement:
     the private key, performs the key exchange, and derives the shared secret using a KDF.
 
     Attributes:
-        private_key (ECC.EccKey): The user's private ECC key.
+        private_key: The user's private ECC key.
     """
 
-    def __init__(self, node_u_id: str, node_u_private_key: bytes, session_salt: bytes):
+    def __init__(self, node_u_id, node_u_private_key, session_salt):
         """
         Initializes the DHKeyAgreement instance.
 
         Args:
-            node_u_id (str): The ID of the node.
-            node_u_private_key (bytes): The private key in PEM format as bytes.
-            session_salt (bytes): A session-specific salt.
+            node_u_id: The ID of the node.
+            node_u_private_key: The private key in PEM format as bytes.
+            session_salt: A session-specific salt.
         """
-        self._node_u_id: str = node_u_id
-        self._private_key: ECC.EccKey = self._import_key_pem(node_u_private_key, key_type="PRIVATE")
+        self._node_u_id = node_u_id
+        self._dh_key = DHKey(private_key_pem=node_u_private_key)
         self.session_salt = session_salt
 
-    @staticmethod
-    def _import_key_pem(key: bytes, key_type: str) -> ECC.EccKey:
+    def _kdf(self, key, node_v_id):
         """
-        Imports the ECC key from PEM format bytes.
+        Key Derivation Function (KDF) that derives a 32-byte key using ConcatKDFHash.
 
         Args:
-            key (bytes): The key in PEM format as bytes.
-            key_type (str): The type of key ("PUBLIC" or "PRIVATE").
+            key: The shared secret key.
+            node_v_id: The ID of the other node (node_v).
 
         Returns:
-            ECC.EccKey: The imported ECC key.
+            The derived key of 32 bytes.
         """
-        key_pem = key.decode("utf-8")
-        if key_type in key_pem:
-            return ECC.import_key(key_pem)
-        else:
-            raise FedbiomedSecaggCrypterError(f"{ErrorNumbers.FB629.value}: Invalid key type, expected '{key_type}'")
+        node_ids = (
+            self._node_u_id + node_v_id
+            if self._node_u_id < node_v_id
+            else node_v_id + self._node_u_id
+        )
+        ckdf = ConcatKDFHash(
+            algorithm=hashes.SHA256(),
+            length=32,
+            otherinfo=self.session_salt + node_ids.encode("utf-8"),
+            backend=default_backend(),
+        )
+        return ckdf.derive(key)
 
-    def _kdf(self, key: bytes, node_v_id: str) -> bytes:
-        """
-        Key Derivation Function (KDF) that derives a 32-byte key using TupleHash128.
-
-        Args:
-            key (bytes): The shared secret key.
-            node_v_id (str): The ID of the other node (node_v).
-
-        Returns:
-            bytes: The derived key of 32 bytes.
-        """
-        h = TupleHash128.new(digest_bytes=32)
-        # h.update is not commutative, the order of the arguments is important so we combine the node ids w.r.t the order
-        node_ids = self._node_u_id + node_v_id if self._node_u_id < node_v_id else node_v_id + self._node_u_id
-        h.update(key + self.session_salt + node_ids.encode("utf-8"))
-        return h.digest()
-
-    def agree(self, node_v_id: str, public_key_pem: bytes) -> bytes:
+    def agree(self, node_v_id, public_key_pem):
         """
         Performs the key agreement and derives the shared secret.
 
         Args:
-            node_v_id (str): The ID of the other node (node_v).
-            public_key_pem (bytes): The public key of the other node in PEM format as bytes.
+            node_v_id: The ID of the other node (node_v).
+            public_key_pem: The public key of the other node in PEM format as bytes.
 
         Returns:
-            bytes: The derived shared secret.
+            The derived shared secret.
         """
-        public_key = self._import_key_pem(public_key_pem, key_type="PUBLIC")
-        shared_secret = key_agreement(
-            static_priv=self._private_key,
-            static_pub=public_key,
-            kdf=lambda key: self._kdf(key, node_v_id)
-        )
-        return shared_secret
+        dh_v_key = DHKey(public_key_pem=public_key_pem)
+        shared_secret = self._dh_key.private_key.exchange(ec.ECDH(), dh_v_key.public_key)
+        derived_key = self._kdf(shared_secret, node_v_id)
+        return derived_key
