ce35ff680e215bb238aae6f8e593d446338ea32c
rtaiello <taiello.riccardo@gmail.com>
2024-09-05 16:24:25 +0200

    fixes various typos

--- tests/test_additive_ss.py
+++ tests/test_additive_ss.py
@@ -1,11 +1,6 @@
 import unittest
-from fedbiomed.common.secagg import Secret, Share
-from fedbiomed.common.exceptions import FedbiomedSecaggCrypterError
-from fedbiomed.common.constants import ErrorNumbers
-
-# Assuming the classes Secret and Share are defined in secret_module
-# from secret_module import Secret, Share
-
+from fedbiomed.common.exceptions import FedbiomedValueError, FedbiomedTypeError
+from fedbiomed.common.secagg._additive_ss import Secret, Share
 
 class TestSecret(unittest.TestCase):
     
@@ -21,9 +16,27 @@ class TestSecret(unittest.TestCase):
 
     def test_secret_initialization_invalid_type(self):
         """Test initializing Secret with an invalid type."""
-        with self.assertRaises(FedbiomedSecaggCrypterError) as context:
+        with self.assertRaises(FedbiomedValueError) as context:
             Secret("invalid_secret")
+        self.assertIn("Secret must be an int or a list of int", str(context.exception))
+    
+    def test_split_invalid_num_shares(self):
+        """Test splitting with invalid number of shares."""
+        secret = Secret(10)
+        with self.assertRaises(FedbiomedValueError):
+            secret.split(0)
+        with self.assertRaises(FedbiomedValueError):
+            secret.split(-5)
+
+    def test_split_with_bit_length(self):
+        """Test splitting with a specific bit length."""
+        secret = Secret(1024)
+        shares = secret.split(3, bit_length=10)
         
+        # Ensure all shares are within the specified bit length range
+        for share in shares:
+            self.assertTrue(-2**10 <= share.value <=2**10)
+
     def test_reconstruct_secret_int(self):
         """Test reconstructing an integer secret."""
         shares = [Share(10), Share(20), Share(15)]
@@ -32,15 +45,16 @@ class TestSecret(unittest.TestCase):
 
     def test_reconstruct_secret_list(self):
         """Test reconstructing a list secret."""
-        shares = [Share([1, 2, 3]), Share([4, 5, 6]), Share([5, 13, 21])]
-        secret = [10, 20, 30]
+        shares = [Share([1, 2, 3]), Share([4, 5, 6]), Share([5, 13, 21])] 
         reconstructed = Secret.reconstruct(shares)
         self.assertEqual(reconstructed, [10, 20, 30])
 
     def test_reconstruct_invalid_shares(self):
         """Test reconstructing with invalid shares."""
-        with self.assertRaises(FedbiomedSecaggCrypterError):
-            Secret.reconstruct([Share(10), Share([20, 30])])
+        invalid_shares = [Share(10), Share([20, 30])]
+        with self.assertRaises(FedbiomedTypeError):
+            Secret.reconstruct(invalid_shares)
+
 
 class TestShare(unittest.TestCase):
 
@@ -56,30 +70,43 @@ class TestShare(unittest.TestCase):
 
     def test_share_initialization_invalid_type(self):
         """Test initializing Share with an invalid type."""
-        with self.assertRaises(FedbiomedSecaggCrypterError):
+        with self.assertRaises(FedbiomedTypeError):
             Share("invalid_share")
 
     def test_add_shares_int(self):
         """Test adding two integer shares."""
-        share1 = Share(10)
-        share2 = Share(15)
-        result = share1 + share2
-        self.assertEqual(result.value, 25)
+        share1 = Secret(10).split(2)
+        share2 = Secret(15).split(2)
+        result = Share.add(share1, share2)
+        reconstructed = Secret.reconstruct(result)
+        self.assertEqual(reconstructed, 25)
 
     def test_add_shares_list(self):
         """Test adding two list shares."""
-        share1 = Share([1, 2, 3])
-        share2 = Share([4, 5, 6])
-        result = share1 + share2
-        self.assertEqual(result.value, [5, 7, 9])
+        share1 = Secret([1, 2, 3]).split(3)
+        share2 = Secret([4, 5, 6]).split(3)
+        result = Share.add(share1, share2)
+        reconstructed = Secret.reconstruct(result)
+        self.assertEqual(reconstructed, [5, 7, 9])
 
     def test_add_invalid_shares(self):
         """Test adding shares of different types (int and list)."""
-        share1 = Share(10)
-        share2 = Share([1, 2, 3])
-        with self.assertRaises(FedbiomedSecaggCrypterError):
-            share1 + share2
+        share1 = Secret(10).split(2)
+        share2 = Secret([1, 2, 3]).split(2)
+        with self.assertRaises(FedbiomedTypeError):
+            Share.add(share1, share2)
 
+    def test_add_share_and_int(self):
+        """Test adding a Share object and an integer."""
+        share = Share(10)
+        result = share + 5
+        self.assertEqual(result.value, 15)
+
+    def test_add_share_and_list(self):
+        """Test adding a Share object and a list."""
+        share = Share([1, 2, 3])
+        result = share + [4, 5, 6]
+        self.assertEqual(result.value, [5, 7, 9])
 
 
 if __name__ == '__main__':
