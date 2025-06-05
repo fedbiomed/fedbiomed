7549a7d5f69aeba023be2a4963a85989b6142fc6
Marc Vesin <marc.vesin@inria.fr>
2025-06-05 13:57:15 +0200

    WIP skeletons for data classes
    Signed-off-by: Marc Vesin <marc.vesin@inria.fr>

--- fedbiomed/common/data/__init__.py
+++ fedbiomed/common/data/__init__.py
@@ -2,15 +2,19 @@
 # SPDX-License-Identifier: Apache-2.0
 
 from fedbiomed.common.datamanager import DataManager
+from fedbiomed.common.dataset import MedicalFolderDataset
 from fedbiomed.common.logger import logger
 
 logger.warning(
-    "Importing `DataManager` class from the `fedbiomed.common.data` "
+    "Importing data classes from the `fedbiomed.common.data` "
     "package has been deprecated. "
-    "Please use the following import statement instead:\n"
-    "`from fedbiomed.common.datamanager import DataManager`"
+    "Please use the following import statements instead:\n"
+    "`from fedbiomed.common.datamanager import DataManager`\n"
+    "`from fedbiomed.common.dataset.flamby_dataset import FlambyDataset`\n"
+    "`from fedbiomed.common.dataset import MedicalFolderDataset`"
 )
 
 __all__ = [
     "DataManager",
+    "MedicalFolderDataset",
 ]
