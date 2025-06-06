5e613a620ca5cb6edac61d16790dd8d1a06429c9
Marc Vesin <marc.vesin@inria.fr>
2025-06-06 23:55:08 +0200

    WIP skeletons for data classes continued
    Signed-off-by: Marc Vesin <marc.vesin@inria.fr>

--- fedbiomed/common/dataloader/_pytorch_dataloader.py
+++ fedbiomed/common/dataloader/_pytorch_dataloader.py
@@ -5,11 +5,20 @@
 Class for data loader in PyTorch training plans
 """
 
+from typing import Optional, Dict, Tuple
+
 from torch.utils.data import Dataset as TorchDataset
 from torch.utils.data import DataLoader as TorchDataLoader
 from torch.utils.data.dataloader import _BaseDataLoaderIter 
+import torch
 
 from ._dataloader import DataLoader
+
+
+# Base type for data returned by `PytorchDataLoader`
+# a sample is tuple `(PytorchDataItem, PytorchDataItem)` for `(data, target)`
+PytorchDataLoaderItem = Optional[Dict[str, torch.Tensor]] 
+PytorchDataLoaderSample = Tuple[PytorchDataLoaderItem, PytorchDataLoaderItem]
 
 
 class PytorchDataLoader(TorchDataLoader, DataLoader):
