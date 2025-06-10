e73c5288df8a7780947a7fec20b1c8a029f3656a
Marc Vesin <marc.vesin@inria.fr>
2025-06-10 16:57:54 +0200

    WIP sketch classes with Dataset/Controller/Reader for base/MedicalFolder/MNIST
    Signed-off-by: Marc Vesin <marc.vesin@inria.fr>

---
+++ fedbiomed/common/dataset/_native_dataset.py
@@ -0,0 +1,42 @@
+# This file is originally part of Fed-BioMed
+# SPDX-License-Identifier: Apache-2.0
+
+"""
+Dataset implementation for native dataset
+"""
+
+from typing import Any, Tuple
+
+from fedbiomed.common.dataset_types import Transform, DatasetDataItem
+from fedbiomed.common.dataset import Dataset
+
+
+class NativeDataset(Dataset):
+    def __init__(
+            self,
+            data: Any,
+            framework_transform : Transform = None,
+            framework_target_transform : Transform = None,
+            # Optional, per-dataset: implement (or not) generic transform (use same argument name)
+            # generic_transform : Transform = None,
+            # generic_target_transform : Transform = None,
+            # Optional, per dataset: implement native transforms (argument name may vary)
+            *args,
+            **kwargs
+    ) -> None:
+        """Class constructor"""
+        super().__init__(framework_transform, *args, **kwargs)
+
+
+
+    # Implement abstract methods
+
+    def __len__(self) -> int:
+        """Get number of samples"""
+
+    # Nota: use Controller._get_nontransformed_item
+    def __getitem__(self, index: int) -> Tuple[DatasetDataItem, DatasetDataItem]:
+        """Retrieve a data sample"""
+
+
+    # Additional methods for exploring data (folders, modalities, subjects) ?
