46ce8d3b01d23a8a275c294ee1d6e84c33823b0b
Marc Vesin <marc.vesin@inria.fr>
2025-06-11 17:23:41 +0200

    WIP sketch classes : add sketches for data managers
    Signed-off-by: Marc Vesin <marc.vesin@inria.fr>

--- fedbiomed/common/datamanager/__init__.py
+++ fedbiomed/common/datamanager/__init__.py
@@ -8,11 +8,17 @@ Classes that simplify imports from fedbiomed.common.datamanager
 from ._data_manager import DataManager
 from ._torch_data_manager import TorchDataManager
 from ._sklearn_data_manager import SkLearnDataManager
+from ._framework_data_manager import FrameworkDataManager
 from ._new_data_manager import NewDataManager
+from ._new_torch_data_manager import NewTorchDataManager
+from ._new_sklearn_data_manager import NewSkLearnDataManager
 
 __all__ = [
     "DataManager",
     "TorchDataManager",
     "SkLearnDataManager",
+    "FrameworkDataManager",
     "NewDataManager",
+    "NewTorchDataManager",
+    "NewSkLearnDataManager",
 ]
