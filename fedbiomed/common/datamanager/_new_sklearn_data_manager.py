f958271063ed415ee857c497b4cfa46bfd612969
Marc Vesin <marc.vesin@inria.fr>
2025-06-11 17:23:41 +0200

    WIP sketch classes : add sketches for data managers
    Signed-off-by: Marc Vesin <marc.vesin@inria.fr>

---
+++ fedbiomed/common/datamanager/_new_sklearn_data_manager.py
@@ -0,0 +1,38 @@
+# This file is originally part of Fed-BioMed
+# SPDX-License-Identifier: Apache-2.0
+
+"""
+Data manager for scikit-learn training plan
+"""
+
+from typing import Tuple, Optional
+
+from fedbiomed.common.dataset import Dataset
+from fedbiomed.common.dataloader import SkLearnDataLoader
+from ._framework_data_manager import FrameworkDataManager
+
+
+class NewSkLearnDataManager(FrameworkDataManager):
+    """Class for creating data loaders from dataset for scikit-learn training plans"""
+
+    _dataset: Dataset
+
+    def __init__(self, dataset: Dataset, **kwargs: dict):  # noqa : B027 # not yet implemented
+        """Class constructor
+
+        Args:
+            dataset: dataset object
+            **kwargs: arguments for data loader
+        """
+
+    def split(
+        self,
+        test_ratio: float,
+        test_batch_size: Optional[int],
+        is_shuffled_testing_dataset: bool = False,
+    ) -> Tuple[SkLearnDataLoader, Optional[SkLearnDataLoader]]:
+        """Split dataset and return data loaders"""
+
+    # Other methods from current TorchDataManager
+    # - maybe can factor some methods to FrameworkDataManager ?
+    # - make some methods private ?
