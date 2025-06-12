18c11dc6faaeae77904a12d048029efe81962f60
Marc Vesin <marc.vesin@inria.fr>
2025-06-12 08:08:34 +0200

    New dataset implementation - v1 of classes sketch
    - add sketch for DatabaseManager
    
    Signed-off-by: Marc Vesin <marc.vesin@inria.fr>

--- fedbiomed/node/dataset_manager/__init__.py
+++ fedbiomed/node/dataset_manager/__init__.py
@@ -6,7 +6,11 @@ Classes that simplify imports from fedbiomed.node.dataset_manager
 """
 
 from ._dataset_manager import DatasetManager
+from ._dataset_db_manager import DatasetDatabaseManager
+from ._dlp_db_manager import DlpDatabaseManager
 
 __all__ = [
     "DatasetManager",
+    "DatasetDatabaseManager",
+    "DlpDatabaseManager",
 ]
