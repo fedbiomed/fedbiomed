59b44ba23e9ed57bd3a3d117cd4f2735defee823
Marc Vesin <marc.vesin@inria.fr>
2025-06-12 17:49:14 +0200

    Sketch classes update - team feedback SP43 T2
    Signed-off-by: Marc Vesin <marc.vesin@inria.fr>

--- fedbiomed/common/dataset_reader/_nifti_reader.py
+++ fedbiomed/common/dataset_reader/_nifti_reader.py
@@ -9,7 +9,13 @@ from pathlib import Path
 
 import torch
 
-from fedbiomed.common.dataset_types import Transform, ReaderItemShape
+from fedbiomed.common.dataset_types import (
+    DataReturnFormat,
+    ReaderItemShape,
+    Transform,
+    drf_default,
+)
+
 from ._reader import Reader
 
 
@@ -17,6 +23,9 @@ class NiftiReader(Reader):
     def __init__(
         self,
         root: Path,
+        # to_format: support DEFAULT and TORCH (SKLEARN not supported)
+        # both are torch.Tensor
+        to_format: DataReturnFormat = drf_default,
         native_transform: Transform = None,
         native_target_transform: Transform = None,
         # Any other parameter ?
@@ -24,18 +33,10 @@ class NiftiReader(Reader):
     ) -> None:
         """Class constructor"""
 
-    def __len__(self) -> int:
-        """Get number of samples"""
-
     # Nota: does not include filtering of DLP, which is unknown to Reader
-
-    # CAVEAT: with current implementation sketch, a `Reader` is for *one* modality
-    # eg if we have a `MedicalFolderDataset` with 2 modalitites `T1` and `T2`
-    # we will instantiate 2 `Reader`s crawling through the same tree.
-    # - cons: a bit more complexity ?
-    # - pros: clearly separate `Dataset` responsibility for eg handling "incomplete" subjects
-    def __getitem__(self, index: int) -> torch.Tensor:
-        """Retrieve a data sample"""
+    # Nota: may want to support PIL as DEFAULT instead of torch.TENSOR
+    def read(self) -> torch.Tensor:
+        """Retrieve data"""
 
     # Nota: does not include filtering of DLP, which is unknown to Reader
     def validate(self) -> None:
@@ -46,23 +47,11 @@ class NiftiReader(Reader):
 
     # Nota: does not include filtering of DLP, which is unknown to Reader
     def shape(self) -> ReaderItemShape:
-        """Returns shape of a data modality served by a reader"""
+        """Returns shape of a data modality served by a reader
+
+        Computed before applying transforms or convrsion to other format"""
 
     # Optional methods which can be implemented (or not) by some readers
     # Code is specific to each reader
-
-    # This is needed by MedicalFolderDataset (and probably most multimodal datasets)
-    # to coordinate *which* subject they retrieve next. "Next" with `__getitem__` for
-    # a `Reader` for one modality does not address same subject as "next" for another
-    # `Reader` of another modality. Thus only dataset can ensure coherence
-    # of "next" sample retrieval, using a tag.
-    #
-    def getitem_by_tag(self, tag: str) -> torch.Tensor:
-        """Retrieve a data sample identified by an arbitrary string tag"""
-
-        # Nothing to do to support it ?
-
-    def to_torch(self) -> None:
-        """Request reader to return samples for a torch training plan"""
 
     # Additional methods for exploring data, depending on Reader
