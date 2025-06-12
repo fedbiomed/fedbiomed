d8248f5e939f32c2f682c3154970eb22236a82aa
Marc Vesin <marc.vesin@inria.fr>
2025-06-12 17:49:14 +0200

    Sketch classes update - team feedback SP43 T2
    Signed-off-by: Marc Vesin <marc.vesin@inria.fr>

--- fedbiomed/common/dataset_reader/_reader.py
+++ fedbiomed/common/dataset_reader/_reader.py
@@ -9,7 +9,12 @@ from abc import ABC, abstractmethod
 from pathlib import Path
 from typing import Any
 
-from fedbiomed.common.dataset_types import DataReturnFormat, ReaderItemShape, Transform
+from fedbiomed.common.dataset_types import (
+    DataReturnFormat,
+    ReaderItemShape,
+    Transform,
+    drf_default,
+)
 
 
 class Reader(ABC):
@@ -23,11 +28,12 @@ class Reader(ABC):
     #
     # Nota: `_to_format` also kept in `Dataset`. To avoid duplicating state variable,
     # we may prefer to implement passing this value as an argument for each `__getitem__` call
-    _to_format: DataReturnFormat = DataReturnFormat.GENERIC
+    _to_format: DataReturnFormat = DataReturnFormat(DataReturnFormat.DEFAULT)
 
     def __init__(  # noqa : B027  # method empty for now, not yet implemented
         self,
         root: Path,
+        to_format: DataReturnFormat = drf_default,
         # Optional, per-reader: implement (or not) native transform (use same argument name)
         # native_transform : Transform = None,
         # native_target_transform : Transform = None,
@@ -36,14 +42,11 @@ class Reader(ABC):
     ) -> None:
         """Class constructor"""
 
-    @abstractmethod
-    def __len__(self) -> int:
-        """Get number of samples"""
-
     # Nota: does not include filtering of DLP, which is unknown to Reader
+    # Nota: some reader may support index or range when data has multiple entries (eg CSV)
     @abstractmethod
-    def __getitem__(self, index: int) -> Any:
-        """Retrieve a data sample"""
+    def read(self) -> Any:
+        """Retrieve data"""
 
     # Nota: does not include filtering of DLP, which is unknown to Reader
     @abstractmethod
@@ -58,30 +61,13 @@ class Reader(ABC):
     def shape(self) -> ReaderItemShape:
         """Returns shape of a data modality served by a reader
 
-        Computed before applying transforms of to_xxx
+        Computed before applying transforms or convrsion to other format
         """
 
     # Optional methods which can be implemented (or not) by some readers
     # Code is specific to each reader
 
-    # This is needed by MedicalFolderDataset (and probably most multimodal datasets)
-    # to coordinate *which* subject they retrieve next. "Next" with `__getitem__` for
-    # a `Reader` for one modality does not address same subject as "next" for another
-    # `Reader` of another modality. Thus only dataset can ensure coherence
-    # of "next" sample retrieval, using a tag.
-    #
-    # def getitem_by_tag(self, tag: str) -> Any:
-    #    """Retrieve a data sample identified by an arbitrary string tag"""
-
-    # def to_torch(self) -> None:
-    #     """Request reader to return samples for a torch training plan
-    # """
-    #
-    # def to_sklearn(self) -> None:
-    #     """Request reader to return samples for a sklearn training plan
-    # """
-
-    # Nota: we could also implement a `to_native()` method in every dataset/reader,
-    # but do we have a use case for that ?
+    # def len(self) -> int:
+    #    """Get number of samples"""
 
     # Additional methods for exploring data, depending on Reader
