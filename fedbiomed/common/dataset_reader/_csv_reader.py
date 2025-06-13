b462336e41acc93cb0f6b894f2decf22cf29f316
ybouilla <yannick.bouillard@inria.fr>
2025-06-13 18:30:10 +0200

    first implementation of csv reader
    Details:
    
    - provides a way to load huge csv dataset
    - detects int, float, str values and converts them apropriatly

--- fedbiomed/common/dataset_reader/_csv_reader.py
+++ fedbiomed/common/dataset_reader/_csv_reader.py
@@ -5,8 +5,13 @@
 Reader implementation for CSV file
 """
 
+import codecs
+import csv
+import io
+import mmap
+from contextlib import contextmanager
 from pathlib import Path
-from typing import Optional, Union
+from typing import Iterable, Optional, Tuple, Union
 
 import numpy as np
 import pandas as pd
@@ -18,6 +23,8 @@ from fedbiomed.common.dataset_types import (
     Transform,
     drf_default,
 )
+from fedbiomed.common.exceptions import FedbiomedError
+from fedbiomed.common.logger import logger
 
 from ._reader import Reader
 
@@ -30,15 +37,34 @@ class CsvReader(Reader):
         to_format: DataReturnFormat = drf_default,
         reader_transform: Transform = None,
         reader_target_transform: Transform = None,
+        reader_transform: Transform = None,
+        native_target_transform: Transform = None,
+        encoding: str = "utf-8",
         # Any other parameter ?
     ) -> None:
         """Class constructor"""
+        self._path = root
+        self._offsets = []
+        self._is_preparsed = False
+        self._delimiter = ""
+        self._header = None
+        self._header_offset = 0
+        self._encoding = encoding
+        self._quoting = csv.QUOTE_NONE
+
+        self._transform_framework = lambda x: x  # change that dpd on to_format arg
 
     # Nota: does not include filtering of DLP, which is unknown to Reader
     def read(
-        self, index: Optional[int] = None, index_max: Optional[int] = None
+        self, index_col: Optional[int] = None, index_max: Optional[int] = None, **kwargs
     ) -> Union[pd.DataFrame, torch.Tensor, np.ndarray]:
-        """Retrieve data"""
+        """Retrieve data: read all dataset"""
+        import dask.dataframe as dd
+
+        file_content = dd.read_csv(self._path, index_col, **kwargs)
+        # TODO: add header + delimiter into the read_csv
+        # FIXME: use only dask if needed. or use context manager
+        return file_content
 
     # Nota: does not include filtering of DLP, which is unknown to Reader
     def validate(self) -> None:
@@ -53,10 +79,132 @@ class CsvReader(Reader):
 
         Computed before applying transforms or conversion to other format"""
 
-    # Optional methods which can be implemented (or not) by some readers
-    # Code is specific to each reader
+        # Optional methods which can be implemented (or not) by some readers
+        # Code is specific to each reader
+        with open(self._path, "rb") as file:
+            if not self._is_preparsed:
+                self._pre_parse(file)
+
+            if self._header is None:
+                file.seek(0)
+
+            first_line = csv.reader(
+                codecs.iterdecode(file, self._encoding), delimiter=self._delimiter
+            )  # parse first line for control
+            nb_col = len(
+                next(first_line)
+            )  # get column number from first line of the csv
+            nb_row = max(sum(1 for row in file), 1)
+        return nb_row, nb_col
+
+    def read_single_entry(self, indexes: int | Iterable) -> Tuple[np.ndarray]:
+        if not isinstance(indexes, Iterable):
+            indexes = [indexes]
+        entries = []
+
+        # with (open(self._path, 'rb') as f, mmap.mmap(f.fileno(), 0, access=mmap.ACCESS_READ) as mm):
+        #     if not self._is_preparsed:
+        #         self._pre_parse(mm)
+        #     if not self._offsets:
+        #         self._get_offset(mm)
+        with (
+            open(self._path, "rb") as f,
+            self._closer(mmap.mmap(f.fileno(), 0, access=mmap.ACCESS_READ)) as mm,
+        ):
+            for idx in indexes:
+                offset = self._offsets[idx]
+                mm.seek(offset)
+
+                # Read until end of line
+                row_bytes = mm.readline()
+
+                # Decode bytes to string
+                row = row_bytes.decode(self._encoding).strip()
+                if not row:
+                    msg = f"cannot read csv file {self._path} due to inconsistent lines: see line {idx} "
+                    logger.error(msg)
+                    raise FedbiomedError(msg)
+
+                # csv_entry = csv.reader(io.StringIO(row), delimiter=self._delimiter, quotechar='|', quoting=csv.QUOTE_NONE)
+                # entries.append(next(csv_entry))
+
+                csv_entry = pd.read_csv(
+                    io.StringIO(row), delimiter=self._delimiter, header=None
+                )
+
+                entries.append(csv_entry.values[0])
+
+        return tuple(entries)
+
+    def get_index(self):
+        if not self._offsets:
+            self._get_index()
+        return list(range(len(self._offsets)))
+
+    def _get_index(self):
+        with (
+            open(self._path, "rb") as f,
+            self._closer(mmap.mmap(f.fileno(), 0, access=mmap.ACCESS_READ)),
+        ):
+            pass
 
     def len(self) -> int:
         """Get number of samples"""
 
     # Additional methods for exploring data, depending on Reader
+    def _pre_parse(self, f):
+        # get delimiter and header
+        # only rertieve info from the first line of csv
+        sniffer = csv.Sniffer()
+        _first_line = f.readline()
+        _first_line = _first_line.decode(self._encoding).strip()
+        self._delimiter = sniffer.sniff(_first_line).delimiter
+        f.seek(0)
+
+        self._header = 0 if sniffer.has_header(_first_line) else None
+        self._header_offset = len(_first_line) + 1 if self._header is not None else 0
+        self._is_preparsed = True
+
+    @contextmanager
+    def _closer(self, mm):
+        if not self._is_preparsed:
+            self._pre_parse(mm)
+        if not self._offsets:
+            self._get_offset(mm)
+        try:
+            yield mm
+        finally:
+            mm.close()
+
+    def _get_offset(self, mm):
+        # with open(file_path, 'r') as f:
+        # mm = mmap.mmap(f.fileno(), 0, access=mmap.ACCESS_READ)
+        offset = 0
+
+        line = True
+        while line:
+            self._offsets.append(offset)
+            line = mm.readline()
+
+            offset += len(line)
+            # if first_time:
+            #     offset -= 1
+            #     first_time = False
+        if self._header is not None:
+            self._offsets.pop(0)
+
+    # #@staticmethod
+    # def default_auto_cast(self, value) -> str | int | float:
+    #     if not value:
+    #         return value
+    #     if value.isdigit() or (value[0] == '-' and value[1:].isdigit()):
+    #         return int(value)
+
+    # def numerical_cast(self, value):
+    #     return value
+
+    def set_tranform_framework(self, framework: DataReturnFormat):
+        self._transform_framework = framework
+
+    def to_generic(self):
+        self._transform_framework = lambda x: np.array(x)
