7aa23cb42bf09b0a16adb3bf5ef26e38b7c0d216
Jhonatan TORRES <jho@interne-168b.sophia.inria.fr>
2025-06-20 11:35:35 +0200

    Second iteration for MnistDataset

--- fedbiomed/common/dataset_controller/_mnist_controller.py
+++ fedbiomed/common/dataset_controller/_mnist_controller.py
@@ -4,6 +4,7 @@
 """
 Controller implementation for MNIST
 """
+
 from pathlib import Path
 from typing import Tuple, Union
 
@@ -19,25 +20,19 @@ class MnistController(Controller):
     def __init__(self, root: Union[str, Path], **kwargs) -> None:
         try:
             self.root = root
-
             # Download dataset if it does not exist
             dataset = datasets.MNIST(root=self.root, download=True)
             self._data = dataset.data
             self._targets = dataset.targets
-            
-            super().__init__(**kwargs)
-        
         except Exception as e:
             raise FedbiomedError(e) from e
-        
+        super().__init__(**kwargs)
+
     def _get_nontransformed_item(
-        self, index: int,
+        self,
+        index: int,
     ) -> Tuple[DatasetDataItem, DatasetDataItem]:
         # data_item and target_item are torch tensors
         data_item = {"data": self._data[index]}
         target_item = {"target": self._targets[index]}
         return data_item, target_item
-
-    def validate(self) -> None:
-        # TODO
-        pass
