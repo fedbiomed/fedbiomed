ac728e7afb4177685de77f9d5d6708944877dde2
Marc Vesin <marc.vesin@inria.fr>
2025-06-24 16:55:05 +0200

    new implementation of DataManager + TorchDataManager + WIP adapt unit tests
    - fix existing unit tests for DataManager TorchDataManager (not
    SkLearnDataManager)
    - need to add more unit tests for new code
    
    Signed-off-by: Marc Vesin <marc.vesin@inria.fr>

--- tests/test_data_manager.py
+++ tests/test_data_manager.py
@@ -1,20 +1,17 @@
 import unittest
 
-import pandas as pd
-from torch.utils.data import Dataset
-
 from fedbiomed.common.constants import TrainingPlans
 from fedbiomed.common.datamanager import (
     DataManager,
-    SkLearnDataManager,
     TorchDataManager,
 )
-from fedbiomed.common.exceptions import FedbiomedDataManagerError
+from fedbiomed.common.dataset import Dataset
+from fedbiomed.common.exceptions import FedbiomedError
 
 
 class TestDataManager(unittest.TestCase):
     class CustomDataset(Dataset):
-        """Create PyTorch Dataset for test purposes"""
+        """Create Fed-BioMed Dataset for test purposes"""
 
         def __init__(self):
             self.X_train = []
@@ -36,64 +33,57 @@ class TestDataManager(unittest.TestCase):
         """Testing __getattr__ method of DataManager"""
 
         # Test passing invalid argument
-        with self.assertRaises(FedbiomedDataManagerError):
+        with self.assertRaises(FedbiomedError):
             data_manager = DataManager(dataset="invalid-argument")
             data_manager.load(tp_type=TrainingPlans.TorchTrainingPlan)
 
         # Test passing another invalid argument
-        with self.assertRaises(FedbiomedDataManagerError):
+        with self.assertRaises(FedbiomedError):
             DataManager(dataset=12)
             data_manager.load(tp_type=TrainingPlans.TorchTrainingPlan)
 
         # Test passing dataset as list
-        with self.assertRaises(FedbiomedDataManagerError):
+        with self.assertRaises(FedbiomedError):
             data_manager = DataManager(dataset=[12, 12, 12, 12])
             data_manager.load(tp_type=TrainingPlans.TorchTrainingPlan)
-
-        # Test passing PyTorch Dataset while training plan is SkLearn
-        with self.assertRaises(FedbiomedDataManagerError):
-            data_manager = DataManager(dataset=TestDataManager.CustomDataset())
-            data_manager.load(tp_type=TrainingPlans.SkLearnTrainingPlan)
 
         # Test Torch Dataset Scenario
         data_manager = DataManager(dataset=TestDataManager.CustomDataset())
         data_manager.load(tp_type=TrainingPlans.TorchTrainingPlan)
         self.assertIsInstance(data_manager._data_manager_instance, TorchDataManager)
 
-        # Test SkLearn Scenario
-        data_manager = DataManager(
-            dataset=pd.DataFrame([[1, 2, 3], [1, 2, 3]]), target=pd.Series([1, 2])
-        )
-        data_manager.load(tp_type=TrainingPlans.SkLearnTrainingPlan)
-        self.assertIsInstance(data_manager._data_manager_instance, SkLearnDataManager)
-
-        # Test auto PyTorch dataset creation
-        data_manager = DataManager(
-            dataset=pd.DataFrame([[1, 2, 3], [1, 2, 3]]), target=pd.Series([1, 2])
-        )
-        data_manager.load(tp_type=TrainingPlans.TorchTrainingPlan)
-        self.assertIsInstance(data_manager._data_manager_instance, TorchDataManager)
-
-        # Test if inputs are not supported by SkLearnTrainingPlan
-        data_manager = DataManager(
-            dataset=["non-pd-or-numpy"], target=["non-pd-or-numpy"]
-        )
-        with self.assertRaises(FedbiomedDataManagerError):
-            data_manager.load(tp_type=TrainingPlans.SkLearnTrainingPlan)
+        # WIP: deactivate until new implementation for NativeDataset
+        #
+        #         # Test SkLearn Scenario
+        #         data_manager = DataManager(
+        #             dataset=pd.DataFrame([[1, 2, 3], [1, 2, 3]]), target=pd.Series([1, 2])
+        #         )
+        #         data_manager.load(tp_type=TrainingPlans.SkLearnTrainingPlan)
+        #         self.assertIsInstance(data_manager._data_manager_instance, SkLearnDataManager)
+        #
+        #         # Test auto PyTorch dataset creation
+        #         data_manager = DataManager(
+        #             dataset=pd.DataFrame([[1, 2, 3], [1, 2, 3]]), target=pd.Series([1, 2])
+        #         )
+        #         data_manager.load(tp_type=TrainingPlans.TorchTrainingPlan)
+        #         self.assertIsInstance(data_manager._data_manager_instance, TorchDataManager)
+        #
+        #        # Test if inputs are not supported by SkLearnTrainingPlan
+        #        data_manager = DataManager(
+        #            dataset=["non-pd-or-numpy"], target=["non-pd-or-numpy"]
+        #        )
+        #        with self.assertRaises(FedbiomedError):
+        #            data_manager.load(tp_type=TrainingPlans.SkLearnTrainingPlan)
 
         # Test undefined training plan
-        data_manager = DataManager(
-            dataset=pd.DataFrame([[1, 2, 3], [1, 2, 3]]), target=pd.Series([1, 2])
-        )
-        with self.assertRaises(FedbiomedDataManagerError):
+        data_manager = DataManager(dataset=TestDataManager.CustomDataset())
+        with self.assertRaises(FedbiomedError):
             data_manager.load(tp_type="NanaNone")
 
     def test_data_manager_02___getattr___(self):
         """Test __getattr__ magic method of DataManager"""
 
-        data_manager = DataManager(
-            dataset=pd.DataFrame([[1, 2, 3], [1, 2, 3]]), target=pd.Series([1, 2])
-        )
+        data_manager = DataManager(dataset=TestDataManager.CustomDataset())
         data_manager.load(tp_type=TrainingPlans.TorchTrainingPlan)
         try:
             data_manager.__getattr__("load")
@@ -104,15 +94,14 @@ class TestDataManager(unittest.TestCase):
             )
 
         # Test attribute error tyr/catch block
-        with self.assertRaises(FedbiomedDataManagerError):
+        with self.assertRaises(FedbiomedError):
             data_manager.__getattr__("toto")
 
     def test_data_manager_03_extend_loader_args(self):
         """Test that extend loader args respects the precedence rules."""
         dm_keyword_args = {"dm_keyword_argument": "keyword_argument_data_manager"}
         data_manager = DataManager(
-            dataset=pd.DataFrame([[1], [1]]),
-            target=pd.Series([1, 2]),
+            dataset=TestDataManager.CustomDataset(),
             **dm_keyword_args,
         )
         self.assertDictEqual(data_manager._loader_arguments, dm_keyword_args)
@@ -127,22 +116,22 @@ class TestDataManager(unittest.TestCase):
         )
 
     def test_data_manager_04_testing_index_setter_getter(self):
-        # for sklearn
-        data_manager = DataManager(
-            dataset=pd.DataFrame([[1, 2, 3], [1, 2, 3]]), target=pd.Series([1, 1])
-        )
-        data_manager.load(tp_type=TrainingPlans.SkLearnTrainingPlan)
-        data_manager._testing_index = [1]
-        data_manager._training_index = [0]
-
-        train, test = data_manager.split(test_ratio=0.5, test_batch_size=None)
-        self.assertEqual(data_manager._testing_index, [1])
-        # self.assertListEqual(data_manager._testing_index, data_manager._testing_index)
+        # WIP: deactivate until new implementation of SkLearnDataManager and/or NativeDataset
+        #
+        #        # for sklearn
+        #        data_manager = DataManager(
+        #            dataset=pd.DataFrame([[1, 2, 3], [1, 2, 3]]), target=pd.Series([1, 1])
+        #        )
+        #        data_manager.load(tp_type=TrainingPlans.SkLearnTrainingPlan)
+        #        data_manager._testing_index = [1]
+        #        data_manager._training_index = [0]
+        #
+        #        train, test = data_manager.split(test_ratio=0.5, test_batch_size=None)
+        #        self.assertEqual(data_manager._testing_index, [1])
+        #        # self.assertListEqual(data_manager._testing_index, data_manager._testing_index)
 
         # for pytorch
-        data_manager = DataManager(
-            dataset=pd.DataFrame([[1, 2, 3], [1, 2, 3]]), target=pd.Series([1, 1])
-        )
+        data_manager = DataManager(dataset=TestDataManager.CustomDataset())
         data_manager.load(tp_type=TrainingPlans.TorchTrainingPlan)
 
         data_manager._testing_index = [1]
