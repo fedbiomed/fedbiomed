20010ad3e3ab5e79c89904db08f636878356257f
Jhonatan TORRES <jho@interne-168b.sophia.inria.fr>
2025-06-26 16:16:30 +0200

    First iteration ImageFolderDataset

--- fedbiomed/common/dataset/__init__.py
+++ fedbiomed/common/dataset/__init__.py
@@ -6,6 +6,7 @@ Classes that simplify imports from fedbiomed.common.dataset
 """
 
 from ._dataset import Dataset, StructuredDataset
+from ._image_folder_dataset import ImageFolderDataset
 from ._legacy_tabular_dataset import LegacyTabularDataset
 from ._medical_datasets import (
     MedicalFolderBase,
@@ -18,12 +19,13 @@ from ._native_dataset import NativeDataset
 
 __all__ = [
     "Dataset",
-    "StructuredDataset",
-    "NativeDataset",
+    "ImageFolderDataset",
     "LegacyTabularDataset",
     "MedicalFolderBase",
     "MedicalFolderController",
     "MedicalFolderDataset",
     "MedicalFolderLoadingBlockTypes",
+    "NativeDataset",
     "MnistDataset",
+    "StructuredDataset",
 ]
