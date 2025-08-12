cc39bae1b4a0b53a93b38dd567084416639bd061
Jhonatan TORRES <jho@Jhonatans-MacBook-Pro.local>
2025-08-12 10:58:17 +0200

    Legacy files moved to '_dataset' and '_dataset_controller' for future reference

---
+++ fedbiomed/common/_dataset_controller/__init__.py
@@ -0,0 +1,18 @@
+# This file is originally part of Fed-BioMed
+# SPDX-License-Identifier: Apache-2.0
+
+"""
+Classes that simplify imports from fedbiomed.common.dataset_controller
+"""
+
+from ._controller import Controller
+from ._image_folder_controller import ImageFolderController
+from ._medical_folder_controller import NewMedicalFolderController
+from ._mnist_controller import MnistController
+
+__all__ = [
+    "Controller",
+    "ImageFolderController",
+    "NewMedicalFolderController",
+    "MnistController",
+]
