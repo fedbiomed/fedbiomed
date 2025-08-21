798c9659c88806746ccf65e003a391959e97cab0
Marc Vesin <marc.vesin@inria.fr>
2025-08-21 15:29:02 +0200

    Fix, clean, simplify dataset_types in sync with current implementation
    - remove unused types
    - remove unused Reader
    - remove duplicate dataset/_dataset_types.py
    - misc fixes after these updates
    
    Signed-off-by: Marc Vesin <marc.vesin@inria.fr>

--- fedbiomed/common/dataset_reader/__init__.py
+++ fedbiomed/common/dataset_reader/__init__.py
@@ -7,10 +7,8 @@ Classes that simplify imports from fedbiomed.common.dataset_reader
 
 from ._csv_reader import CsvReader
 from ._nifti_reader import NiftiReader
-from ._reader import Reader
 
 __all__ = [
-    "Reader",
     "CsvReader",
     "NiftiReader",
     "MnistReader",
