b6dbf4f476e600e4a934cc2189a408de460b42f3
Sergen CANSIZ <sergen.cansiz@inria.fr>
2025-08-26 17:01:30 +0200

    temporarily fixes CLI issue to avoid errors while executing CLI commands for testing purposes

--- fedbiomed/node/dataset_manager/_dataset_manager.py
+++ fedbiomed/node/dataset_manager/_dataset_manager.py
@@ -21,7 +21,9 @@ from torchvision import datasets, transforms
 
 from fedbiomed.common.constants import DatasetTypes, ErrorNumbers
 from fedbiomed.common.dataloadingplan import DataLoadingBlock, DataLoadingPlan
-from fedbiomed.common.dataset import MedicalFolderController
+
+# TODO: Check usage of MedicalFolderDataset it still assumes that it is old MedicalFolderDataset
+from fedbiomed.common.dataset_controller import MedicalFolderController
 from fedbiomed.common.db import DBTable
 from fedbiomed.common.exceptions import FedbiomedDatasetManagerError, FedbiomedError
 from fedbiomed.common.logger import logger
