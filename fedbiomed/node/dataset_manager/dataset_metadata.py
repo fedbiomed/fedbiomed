4638b24a2b6113dd2db59cf4b6d4b688499e082f
alitolga <alitolga.dincer@gmail.com>
2025-09-10 16:33:27 +0200

    Create dataset metadata dataclasses

---
+++ fedbiomed/node/dataset_manager/dataset_metadata.py
@@ -0,0 +1,44 @@
+import json
+from dataclasses import dataclass
+from typing import Any, List, Dict, Optional, Union
+
+
+@dataclass
+class DatasetMetadata:
+    name: str
+    data_type: str
+    tags: List[str]
+    description: str
+    shape: Union[List[int], Dict[str, List[int]]]
+    path: str
+    dataset_id: str
+    dtypes: List[str]
+    dataset_parameters: Optional[Dict[str, Any]]
+
+
+# Individual dataset classes (for clarity)
+@dataclass
+class CsvMetadata(DatasetMetadata):
+    pass
+
+
+@dataclass
+class MnistMetadata(DatasetMetadata):
+    pass
+
+
+@dataclass
+class MednistMetadata(DatasetMetadata):
+    pass
+
+
+@dataclass
+class ImagesMetadata(DatasetMetadata):
+    pass
+
+
+@dataclass
+class MedicalFolderMetadata(DatasetMetadata):
+    tabular_file: Optional[str]
+    index_col: Optional[int]
+    dlp_id: Optional[str]
