e109b664f0ff3bbf5d57cb36d52c1d3e91648bc5
Sergen CANSIZ <sergen.cansiz@inria.fr>
2025-09-16 11:12:01 +0200

    fix IPython display error

--- fedbiomed/common/logger.py
+++ fedbiomed/common/logger.py
@@ -44,15 +44,7 @@ Contrary to other Fed-BioMed classes, the API of FedLogger is compliant with the
 
 import json
 import logging
-<<<<<<< HEAD
 from typing import Any, Callable, Optional
-=======
-import logging.handlers
-import sys
-from typing import Any, Callable
-
-from IPython.display import display
->>>>>>> 70ed1fe2 (Create docker folder and sub fedbiomed image folders)
 
 from fedbiomed.common.ipython import is_ipython
 from fedbiomed.common.singleton import SingletonMeta
@@ -313,7 +305,7 @@ class FedLogger(metaclass=SingletonMeta):
         if is_ipython():
             handler = _IpythonConsoleHandler()
         else:
-            handler = logging.StreamHandler(sys.stdout)
+            handler = logging.StreamHandler()
 
         handler.setLevel(self._internal_level_translator(level))
 
