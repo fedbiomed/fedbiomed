d282a4c8f8efdc2fe724614f36341c08f7635839
Jhonatan Torres <31116530+leonard2103@users.noreply.github.com>
2025-09-22 14:53:07 +0200

    Feature/1434 dataset manager db (#1435)
    **PR description**
    
    closes #1434
    
    Changes include:
    - A singleton to handle interactions with json file (in
    `fedbiomed/common/db.py`).
    - DB classes for Dataset, Dlp and Dlb that handle their own specific
    functions and use the ones general from TinyTableConnnector.
    - DatasetDatabaseManager and DlpDatabaseManager that use DB classes.
    
    All changes to be validated to continue issue #1363
    
    **Developer Certificate Of Origin (DCO)**
    
    By opening this merge request, you agree to the
    [Developer Certificate of Origin
    (DCO)](https://github.com/fedbiomed/fedbiomed/blob/master/CONTRIBUTING.md#fed-biomed-developer-certificate-of-origin-dco)
    
    This DCO essentially means that:
    
    - you offer the changes under the same license agreement as the project,
    and
    - you have the right to do that,
    - you did not steal somebody else’s work.
    
    **License**
    
    Project code files should begin with these comment lines to help trace
    their origin:
    ```
    ```
    
    Code files can be reused from another project with a compatible
    non-contaminating license.
    They shall retain the original license and copyright mentions.
    The `CREDIT.md` file and `credit/` directory shall be completed and
    updated accordingly.
    
    **Guidelines for PR review**
    
    General:
    
    * give a glance to
    [DoD](https://fedbiomed.org/latest/developer/definition-of-done/)
    * check [coding rules and coding
    style](https://fedbiomed.org/latest/developer/usage_and_tools/#coding-style)
    * check docstrings (eg run `tests/docstrings/check_docstrings`)
    
    Specific to some cases:
    
    * update all conda envs consistently (`development` and `vpn`, Linux and
    MacOS)
    * if modified researcher (eg new attributes in classes) check if
    breakpoint needs update (`breakpoint`/`load_breakpoint` in
    `Experiment()`, `save_state_breakpoint`/`load_state_breakpoint` in
    aggregators, strategies, secagg, etc.)
    * if modified a component with versioning (config files, breakpoint,
    messaging protocol) then update the version following the rules in
    `common/utils/_versions.py`
    
    ---------
    
    Co-authored-by: Jhonatan TORRES <jho@interne-252b.sophia.inria.fr>

---
+++ fedbiomed/node/dataset_manager/_dlp_db_manager.py
@@ -0,0 +1,102 @@
+# This file is originally part of Fed-BioMed
+# SPDX-License-Identifier: Apache-2.0
+
+"""
+Manage the node's database table for handling DLP / DLB
+"""
+
+from typing import Optional, Union
+
+from fedbiomed.common.constants import ErrorNumbers
+from fedbiomed.common.dataloadingplan import (
+    DataLoadingPlan,
+)
+from fedbiomed.common.exceptions import FedbiomedError
+from fedbiomed.common.logger import logger
+
+from ._db_tables import DlbTable, DlpTable
+
+
+class DlpDatabaseManager:
+    """Manage the node's database table for handling DLP / DLB
+
+    Facility for storing data, retrieving data and getting data info
+    for the node. Currently uses TinyDB.
+    """
+
+    _dlp_table: DlpTable
+    _dlb_table: DlbTable
+
+    def __init__(self, path: str):
+        self._dlp_table = DlpTable(path)
+        self._dlb_table = DlbTable(path)
+
+    def list_dlp_by_target_dataset_type(
+        self, target_dataset_type: str = None
+    ) -> list[dict]:
+        """Return all dlps matching the requested target type."""
+        return self._dlp_table.list_by_target_dataset_type(target_dataset_type)
+
+    def get_dlp_by_id(self, dlp_id: str) -> tuple[dict, list[dict]]:
+        """Get DLP information by its ID, along with associated DLBs."""
+        dlp = self._dlp_table.get_by_id(dlp_id)
+        dlb_ids = [] if dlp is None else list(dlp["loading_blocks"].values())
+        dlbs = self._dlb_table.get_all_by_value("dlb_id", dlb_ids) if dlb_ids else []
+        return dlp, dlbs
+
+    def remove_dlp_by_id(self, dlp_id: str):
+        """Removes a data loading plan (DLP) from the database, along with its associated
+        data loading blocks (DLBs).
+
+        Args:
+            dlp_id: the DataLoadingPlan id
+        """
+        dlp, dlbs = self.get_dlp_by_id(dlp_id)
+
+        # TODO: should we raise if dlp is None?
+        if dlp is not None:
+            self._dlp_table.delete_by_id(dlp_id)
+            for dlb in dlbs:
+                self._dlb_table.delete_by_id(dlb["dlb_id"])
+
+    def save_data_loading_plan(
+        self, data_loading_plan: Optional[DataLoadingPlan]
+    ) -> Union[str, None]:
+        """Save a DataLoadingPlan to the database.
+
+        This function saves a DataLoadingPlan to the database, and returns its ID.
+
+        Raises:
+            FedbiomedDatasetManagerError: bad data loading plan name (size, not unique)
+
+        Args:
+            data_loading_plan: the DataLoadingPlan to be saved, or None.
+
+        Returns:
+            The `dlp_id` if a DLP was saved, or None
+        """
+        if data_loading_plan is None:
+            return None
+
+        if len(data_loading_plan.desc) < 4:
+            _msg = (
+                f"{ErrorNumbers.FB316.value}: Cannot save data loading plan, "
+                "DLP name needs to have at least 4 characters."
+            )
+            logger.error(_msg)
+            raise FedbiomedError(_msg)
+
+        dlp_same_name = self._dlp_table.get_all_by_value("name", data_loading_plan.desc)
+        if dlp_same_name:
+            _msg = (
+                f"{ErrorNumbers.FB316.value}: Cannot save data loading plan, "
+                "DLP name needs to be unique."
+            )
+            logger.error(_msg)
+            raise FedbiomedError(_msg)
+
+        dlp_metadata, dlbs_metadata = data_loading_plan.serialize()
+        _ = self._dlp_table.insert(dlp_metadata)
+        for dlb_metadata in dlbs_metadata:
+            _ = self._dlb_table.insert(dlb_metadata)
+        return data_loading_plan.dlp_id
