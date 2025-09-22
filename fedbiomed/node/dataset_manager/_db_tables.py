1fd6df104fb8a016fdac34f21b4b1213e56b0f56
Jhonatan Torres <31116530+leonard2103@users.noreply.github.com>
2025-09-22 14:53:07 +0200

    Feature/1434 dataset manager db (#1435)
    **PR description**
    
    closes #1434
    
    Changes include:
    - A singleton to handle interactions with json file (in
    `fedbiomed/common/db.py`).
    - DB classes for Dataset, Dlp and Dlb that handle their own specific
    functions and use the ones general from TinyTableConnnector.
    - DatasetDatabaseManager and DlpDatabaseManager that use DB classes.
    
    All changes to be validated to continue issue #1363
    
    
    **Developer Certificate Of Origin (DCO)**
    
    By opening this merge request, you agree to the
    [Developer Certificate of Origin
    (DCO)](https://github.com/fedbiomed/fedbiomed/blob/master/CONTRIBUTING.md#fed-biomed-developer-certificate-of-origin-dco)
    
    This DCO essentially means that:
    
    - you offer the changes under the same license agreement as the project,
    and
    - you have the right to do that,
    - you did not steal somebody else’s work.
    
    **License**
    
    Project code files should begin with these comment lines to help trace
    their origin:
    ```
    # This file is originally part of Fed-BioMed
    # SPDX-License-Identifier: Apache-2.0
    ```
    
    Code files can be reused from another project with a compatible
    non-contaminating license.
    They shall retain the original license and copyright mentions.
    The `CREDIT.md` file and `credit/` directory shall be completed and
    updated accordingly.
    
    
    **Guidelines for PR review**
    
    General:
    
    * give a glance to
    [DoD](https://fedbiomed.org/latest/developer/definition-of-done/)
    * check [coding rules and coding
    style](https://fedbiomed.org/latest/developer/usage_and_tools/#coding-style)
    * check docstrings (eg run `tests/docstrings/check_docstrings`)
    
    Specific to some cases:
    
    * update all conda envs consistently (`development` and `vpn`, Linux and
    MacOS)
    * if modified researcher (eg new attributes in classes) check if
    breakpoint needs update (`breakpoint`/`load_breakpoint` in
    `Experiment()`, `save_state_breakpoint`/`load_state_breakpoint` in
    aggregators, strategies, secagg, etc.)
    * if modified a component with versioning (config files, breakpoint,
    messaging protocol) then update the version following the rules in
    `common/utils/_versions.py`
    
    ---------
    
    Co-authored-by: Jhonatan TORRES <jho@interne-252b.sophia.inria.fr>

---
+++ fedbiomed/node/dataset_manager/_db_tables.py
@@ -0,0 +1,116 @@
+from typing import List, Optional, Union
+
+from fedbiomed.common.constants import DatasetTypes, ErrorNumbers
+from fedbiomed.common.db import TinyTableConnector
+from fedbiomed.common.exceptions import FedbiomedError
+from fedbiomed.common.logger import logger
+
+
+class DatasetTable(TinyTableConnector):
+    _table_name = "Datasets"
+    _id_name = "dataset_id"
+
+    def search_by_tags(self, tags: Union[tuple, list]) -> list:
+        """Searches for data with given tags.
+
+        Args:
+            tags:  List of tags
+
+        Returns:
+            The list of matching datasets
+        """
+        return self.get_all_by_condition("tags", lambda x: set(tags).issubset(x))
+
+    def search_conflicting_tags(self, tags: Union[tuple, list]) -> list:
+        """Searches for registered data that have conflicting tags with the given tags
+
+        Args:
+            tags:  List of tags
+
+        Returns:
+            The list of conflicting datasets
+        """
+        return self.get_all_by_condition(
+            "tags",
+            lambda x: set(tags).issubset(x) or set(x).issubset(tags),
+        )
+
+    def remove_database(self, dataset_id: str):
+        """Removes a dataset from database.
+
+        Args:
+            dataset_id: Dataset unique ID.
+
+        Raises:
+            FedbiomedError: If no dataset with given ID exists in the database.
+        """
+        if self.get_by_id(dataset_id) is None:
+            _msg = ErrorNumbers.FB322.value + f": No dataset found with id {dataset_id}"
+            logger.error(_msg)
+            raise FedbiomedError(_msg)
+        self.delete_by_id(dataset_id)
+
+    def modify_database_info(self, dataset_id: str, modified_dataset: dict):
+        """Modifies a dataset in the database.
+
+        Args:
+            dataset_id: ID of the dataset to modify.
+            modified_dataset: key-value pairs to replace in the existing entry.
+
+        Raises:
+            FedbiomedError: conflicting tags with existing dataset
+        """
+        # Check that there are not existing dataset with conflicting tags
+        if "tags" in modified_dataset:
+            # the dataset to modify is ignored (can conflict with its previous tags)
+            conflicting_ids = [
+                _["dataset_id"]
+                for _ in self.search_conflicting_tags(modified_dataset["tags"])
+                if _["dataset_id"] != dataset_id
+            ]
+            if len(conflicting_ids) > 0:
+                msg = (
+                    f"{ErrorNumbers.FB322.value}, one or more registered dataset has conflicting tags: "
+                    f" {' '.join([_['name'] for _ in conflicting_ids])}"
+                )
+                logger.critical(msg)
+                raise FedbiomedError(msg)
+
+        _ = self.update_by_id(dataset_id, modified_dataset)
+
+
+class DlpTable(TinyTableConnector):
+    _table_name = "Dlps"
+    _id_name = "dlp_id"
+
+    def list_by_target_dataset_type(
+        self, target_dataset_type: Optional[str] = None
+    ) -> List[dict]:
+        """Return all existing DataLoadingPlans.
+
+        Args:
+            target_dataset_type: (str or None) if specified, return only dlps matching the requested target type.
+
+        Returns:
+            An array of dict, each dict is a DataLoadingPlan
+        """
+        if target_dataset_type is None:
+            return self.all()
+
+        if not isinstance(target_dataset_type, str):
+            raise FedbiomedError(
+                f"Wrong input type for target_dataset_type. "
+                f"Expected str, got {type(target_dataset_type)} instead."
+            )
+        if target_dataset_type not in [t.value for t in DatasetTypes]:
+            raise FedbiomedError(
+                "target_dataset_type should be of the values defined in "
+                "fedbiomed.common.constants.DatasetTypes"
+            )
+
+        return self.get_all_by_value("target_dataset_type", target_dataset_type)
+
+
+class DlbTable(TinyTableConnector):
+    _table_name = "Dlbs"
+    _id_name = "dlb_id"
