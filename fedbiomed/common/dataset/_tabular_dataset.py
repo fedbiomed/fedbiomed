817cf6de50f71743a187aabfcb6b1ced301fb764
Ali Tolga Dinçer <alitolga.dincer@gmail.com>
2025-11-21 15:10:32 +0100

    Change sklearn native to framework for tabular dataset

--- fedbiomed/common/dataset/_tabular_dataset.py
+++ fedbiomed/common/dataset/_tabular_dataset.py
@@ -16,16 +16,8 @@ from fedbiomed.common.exceptions import FedbiomedError
 class TabularDataset(Dataset):
     _controller_cls: type = TabularController
 
-    @staticmethod
-    def _to_sklearn(x: pl.DataFrame) -> "np.ndarray":
-        arr = x.to_numpy()
-        # if it's a single row [[A1, B1, C1]], make it [A1, B1, C1]
-        if arr.ndim == 2 and arr.shape[0] == 1:
-            return arr[0]
-        return arr
-
     _native_to_framework = {
-        DataReturnFormat.SKLEARN: _to_sklearn.__func__,
+        DataReturnFormat.SKLEARN: lambda x: np.squeeze(np.array(x)),
         DataReturnFormat.TORCH: lambda x: x.to_torch(),
     }
 
