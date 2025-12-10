6e5d8d7cc0e85519aba33b85a415f37c95d698e1
Ali Tolga Dinçer <alitolga.dincer@gmail.com>
2025-12-10 14:46:19 +0100

    Change the implementation according to mixin paradigm.
    - Remove analytics dispatcher.
    - Move mean calculation to base analytics strategy class.
    - Add Tabular Analytics mixin class to Tabular Dataset.
    - Write unit tests.

--- fedbiomed/common/analytics/image_analytics.py
+++ fedbiomed/common/analytics/image_analytics.py
@@ -1,6 +1,10 @@
+from typing import Dict
+
 from .analytics_strategy import AnalyticsStrategy
 
 
 class ImageAnalytics(AnalyticsStrategy):
-    def mean(self, dataset, params=None):
-        raise NotImplementedError
+    """Mixin class for computing analytics on image folder datasets"""
+
+    def mean(self, **kwargs) -> Dict:
+        return super().mean(**kwargs)
