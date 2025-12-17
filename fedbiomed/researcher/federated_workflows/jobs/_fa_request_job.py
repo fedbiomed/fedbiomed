e7854043580e2a193f29957b25cd2f6c6835fbc1
Sergen CANSIZ <sergen.cansiz@inria.fr>
2025-12-17 15:10:27 +0100

    fixes missing implementation for FARequestJob

--- fedbiomed/researcher/federated_workflows/jobs/_fa_request_job.py
+++ fedbiomed/researcher/federated_workflows/jobs/_fa_request_job.py
@@ -1,7 +1,9 @@
 from typing import Dict
 
+from fedbiomed.common.constants import AnalyticsTypes
 from fedbiomed.common.logger import logger
 from fedbiomed.common.message import ErrorMessage, FAReply, FARequest
+from fedbiomed.researcher.requests import MessagesByNode
 
 from ._job import Job
 
@@ -17,35 +19,44 @@ class FARequestJob(Job):
     """
 
     def __init__(
-        self, experiment_id: str, fa_id: str, federated_dataset: dict, fa_args: dict
+        self,
+        experiment_id: str,
+        fa_id: str,
+        federated_dataset: dict,
+        fa_args: dict,
+        **kwargs,
     ) -> None:
         """Initialize the FARequestJob with the given FA request.
 
         Args:
             fa_request: The FA request to be associated with this job.
         """
+        super().__init__(**kwargs)
         self._experiment_id = experiment_id
         self._fa_id = fa_id
         self._federated_dataset = federated_dataset
         self._fa_args = fa_args
 
-        super().__init__()
-
     def execute(self) -> Dict[str, FAReply]:
+        """Executes federated analytics request
+
+        Returns:
+            A dictionary mapping node IDs to their respective FA replies.
+        """
         fa_request = dict(
             researcher_id=self._researcher_id,
             experiment_id=self._experiment_id,
-            analytics_type="mean",
+            analytics_type=AnalyticsTypes.MEAN.value,
             fa_id=self._fa_id,
             fa_args=self._fa_args,
         )
 
-        requests = {}
+        requests = MessagesByNode()
         for node in self._nodes:
             requests[node] = FARequest(
-                {
+                **{
                     **fa_request,
-                    "dataset_id": self._federated_dataset.data()[node].dataset_id,
+                    "dataset_id": self._federated_dataset.data()[node]["dataset_id"],
                 }
             )
 
@@ -56,7 +67,7 @@ class FARequestJob(Job):
         if errors:
             # Handle errors appropriately (logging, raising exceptions, etc.)
             for node_id, error in errors.items():
-                logger.log(
+                logger.error(
                     "Error message received during federated analytics "
                     f"in node_id={node_id}: {error.errnum}. {error.extra_msg}"
                 )
