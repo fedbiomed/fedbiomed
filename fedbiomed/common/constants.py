16d4f0fbdd537058b9615050fe7c097ff38641f2
Marc Vesin <marc.vesin@inria.fr>
2025-12-19 16:54:12 +0100

    Create PreprocRequest PreprocReply messages (#1547)
    Address and completes #1543
    
    ---------
    
    Signed-off-by: Marc Vesin <marc.vesin@inria.fr>

--- fedbiomed/common/constants.py
+++ fedbiomed/common/constants.py
@@ -98,7 +98,7 @@ __node_config_version__ = FBM_Component_Version("2")  # node config file version
 
 __node_state_version__ = FBM_Component_Version("2")  # node state version
 __breakpoints_version__ = FBM_Component_Version("3")  # breakpoints format version
-__messaging_protocol_version__ = FBM_Component_Version("6")  # format of gRPC messages.
+__messaging_protocol_version__ = FBM_Component_Version("7")  # format of gRPC messages.
 __secagg_element_version__ = FBM_Component_Version(
     "2"
 )  # format of secagg database elements
@@ -308,6 +308,27 @@ class SecaggElementTypes(_BaseEnum):
         for element in SecaggElementTypes:
             if element.value == element_value:
                 return element
+
+
+class PreprocType(_BaseEnum):
+    """Enumeration class for dataset preprocessing types"""
+
+    FEDCOMBAT: int = 0
+
+
+class PreprocStep(_BaseEnum):
+    """Abstract base enumeration class for dataset preprocessing steps"""
+
+
+class HarmonizationStep(PreprocStep):
+    """Enumeration class for dataset preprocessing steps"""
+
+    # step1 uses Federated Analytics
+    STEP2: int = 2
+    # step3 uses Federated Learning
+    STEP4: int = 4
+    STEP5: int = 5
+    STEP6: int = 6
 
 
 class SAParameters:
