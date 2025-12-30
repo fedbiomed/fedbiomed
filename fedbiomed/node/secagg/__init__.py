f1445209e8a13cb82086562063ead916ecfc0943
Marc Vesin <marc.vesin@inria.fr>
2025-12-30 14:25:13 +0100

    Fix source file headers when missing license header or not in standard format
    Signed-off-by: Marc Vesin <marc.vesin@inria.fr>

--- fedbiomed/node/secagg/__init__.py
+++ fedbiomed/node/secagg/__init__.py
@@ -1,3 +1,6 @@
+# This file is originally part of Fed-BioMed
+# SPDX-License-Identifier: Apache-2.0
+
 from ._secagg_round import SecaggRound
 from ._secagg_setups import (
     SecaggBaseSetup,
--- fedbiomed/transport/__init__.py
+++ fedbiomed/transport/__init__.py
@@ -1,3 +1,6 @@
+# This file is originally part of Fed-BioMed
+# SPDX-License-Identifier: Apache-2.0
+
 """
 Transport module to manage communication between researcher and
 node components based on gRPC
